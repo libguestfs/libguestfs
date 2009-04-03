ff1f39794b2688f0fd6a0c367164e3d715136837
Richard Jones <rjones@redhat.com>
2009-04-03 19:16:27 +0100

    Make the partition configurable.

--- examples/hello.c
+++ examples/hello.c
@@ -1,4 +1,8 @@
-/* Create a "/hello" file on /dev/sda1. */
+/* Create a "/hello" file on chosen partition.
+ * eg:
+ *   hello guest.img /dev/sda1
+ *   hello guest.img /dev/VolGroup00/LogVol00
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,8 +14,8 @@ main (int argc, char *argv[])
 {
   guestfs_h *g;
 
-  if (argc != 2 || access (argv[1], F_OK) != 0) {
-    fprintf (stderr, "Usage: hello disk-image\n");
+  if (argc != 3 || access (argv[1], F_OK) != 0) {
+    fprintf (stderr, "Usage: hello disk-image partition\n");
     exit (1);
   }
 
@@ -22,7 +26,7 @@ main (int argc, char *argv[])
   if (guestfs_launch (g) == -1) exit (1);
   if (guestfs_wait_ready (g) == -1) exit (1);
 
-  if (guestfs_mount (g, "/dev/sda1", "/") == -1) exit (1);
+  if (guestfs_mount (g, argv[2], "/") == -1) exit (1);
 
   if (guestfs_touch (g, "/hello") == -1) exit (1);
 
