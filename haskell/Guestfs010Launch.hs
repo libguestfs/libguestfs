d43dac69483e8ec62e8356d93f761684ce2f5cc8
Richard Jones <rjones@redhat.com>
2009-05-09 17:19:24 +0100

    Partial Haskell bindings.

---
+++ haskell/Guestfs010Launch.hs
@@ -0,0 +1,32 @@
+{- libguestfs Haskell bindings
+   Copyright (C) 2009 Red Hat Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+-}
+
+module Guestfs010Launch where
+import qualified Guestfs
+import System.IO (openFile, hClose, hSetFileSize, IOMode(WriteMode))
+import System.Posix.Files (removeLink)
+
+main = do
+  g <- Guestfs.create
+  fd <- openFile "test.img" WriteMode
+  hSetFileSize fd (500 * 1024 * 1024)
+  hClose fd
+  Guestfs.add_drive g "test.img"
+  Guestfs.launch g
+  Guestfs.wait_ready g
+  removeLink "test.img"
