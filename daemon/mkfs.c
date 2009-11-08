e0fb48c01f1c25c20e1d5cae0765e65d7eaa4250
Richard Jones <rjones@redhat.com>
2009-11-08 11:57:02 +0000

    appliance: Enhance mkfs to support many more filesystem types.
    This fixes support for NTFS, and adds support for:
     - reiserfs
     - btrfs
     - GFS and GFS2
     - JFS
     - HFS and HFS+
     - NILFS
     - OCFS2 (disabled)
    
    We don't enable OCFS2 by default, because it pulls in about
    140 extra packages into the appliance.
    
    GFS & GFS2 default to single node (no lock manager etc).

--- daemon/mkfs.c
+++ daemon/mkfs.c
@@ -29,15 +29,61 @@
 #include "daemon.h"
 #include "actions.h"
 
-int
-do_mkfs (const char *fstype, const char *device)
-{
-  char *err;
-  int r;
+#define MAX_ARGS 16
 
-  r = command (NULL, &err, "/sbin/mkfs", "-t", fstype, device, NULL);
+static int
+mkfs (const char *fstype, const char *device,
+      const char **extra, size_t nr_extra)
+{
+  const char *argv[MAX_ARGS];
+  size_t i = 0, j;
+  int r;
+  char *err;
+
+  argv[i++] = "/sbin/mkfs";
+  argv[i++] = "-t";
+  argv[i++] = fstype;
+
+  /* mkfs.ntfs requires the -Q argument otherwise it writes zeroes
+   * to every block and does bad block detection, neither of which
+   * are useful behaviour for virtual devices.
+   */
+  if (strcmp (fstype, "ntfs") == 0)
+    argv[i++] = "-Q";
+
+  /* mkfs.reiserfs produces annoying interactive prompts unless you
+   * tell it to be quiet.
+   */
+  if (strcmp (fstype, "reiserfs") == 0)
+    argv[i++] = "-f";
+
+  /* Same for JFS. */
+  if (strcmp (fstype, "jfs") == 0)
+    argv[i++] = "-f";
+
+  /* For GFS, GFS2, assume a single node. */
+  if (strcmp (fstype, "gfs") == 0 || strcmp (fstype, "gfs2") == 0) {
+    argv[i++] = "-p";
+    argv[i++] = "lock_nolock";
+    /* The man page says this is default, but it doesn't seem to be: */
+    argv[i++] = "-j";
+    argv[i++] = "1";
+    /* Don't ask questions: */
+    argv[i++] = "-O";
+  }
+
+  for (j = 0; j < nr_extra; ++j)
+    argv[i++] = extra[j];
+
+  argv[i++] = device;
+  argv[i++] = NULL;
+
+  if (i > MAX_ARGS)
+    abort ();
+
+  r = commandv (NULL, &err, argv);
   if (r == -1) {
-    reply_with_error ("mkfs: %s", err);
+    reply_with_error ("mkfs: %s: %s: %s", fstype, device, err);
     free (err);
     return -1;
   }
@@ -47,22 +93,21 @@ do_mkfs (const char *fstype, const char *device)
 }
 
 int
+do_mkfs (const char *fstype, const char *device)
+{
+  return mkfs (fstype, device, NULL, 0);
+}
+
+int
 do_mkfs_b (const char *fstype, int blocksize, const char *device)
 {
-  char *err;
-  int r;
-
+  const char *extra[2];
   char blocksize_s[32];
+
   snprintf (blocksize_s, sizeof blocksize_s, "%d", blocksize);
 
-  r = command (NULL, &err,
-               "/sbin/mkfs", "-t", fstype, "-b", blocksize_s, device, NULL);
-  if (r == -1) {
-    reply_with_error ("mkfs_b: %s", err);
-    free (err);
-    return -1;
-  }
+  extra[0] = "-b";
+  extra[1] = blocksize_s;
 
-  free (err);
-  return 0;
+  return mkfs (fstype, device, extra, 2);
 }
