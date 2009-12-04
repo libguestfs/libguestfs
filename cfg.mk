9e5fabbe01c0964f7124595beec01dde836d93a6
Jim Meyering <jim@meyering.net>
2009-12-04 15:34:41 +0000

    accommodate leading "+" in git submodule output
    * cfg.mk (_submodule_hash): Also filter out "+".
    * autogen.sh: Likewise.

--- cfg.mk
+++ cfg.mk
@@ -134,7 +134,7 @@ ifeq (0,$(MAKELEVEL))
   #  b653eda3ac4864de205419d9f41eec267cb89eeb .gnulib (v0.0-2286-gb653eda)
   # $ cat .git-module-status
   # b653eda3ac4864de205419d9f41eec267cb89eeb
-  _submodule_hash = sed 's/^[ -]//;s/ .*//'
+  _submodule_hash = sed 's/^[ +-]//;s/ .*//'
   _update_required := $(shell						\
       actual=$$(git submodule status | $(_submodule_hash));		\
       stamp="$$($(_submodule_hash) $(_curr_status) 2>/dev/null)";	\
