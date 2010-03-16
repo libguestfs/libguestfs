d89955fde9806d1125e03cc3caddea12df9d0cfa
Richard Jones <rjones@redhat.com>
2010-03-16 11:50:39 +0000

    guestfs.h: More adjustment to comments in the header file.

--- src/guestfs.h
+++ src/guestfs.h
@@ -16,18 +16,19 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#ifndef GUESTFS_H_
-#define GUESTFS_H_
-
-/* IMPORTANT NOTE
+/* IMPORTANT NOTE:
  *
  * All API documentation is in the manpage, 'guestfs(3)'.
  * To read it, type:
  *   man 3 guestfs
  * Or read it online here:
  *   http://libguestfs.org/guestfs.3.html
+ *
  * Go and read it now, I'll wait for you to come back.
  */
+
+#ifndef GUESTFS_H_
+#define GUESTFS_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,11 +36,11 @@ extern "C" {
 
 typedef struct guestfs_h guestfs_h;
 
-/* Connection management. */
+/*--- Connection management ---*/
 extern guestfs_h *guestfs_create (void);
 extern void guestfs_close (guestfs_h *g);
 
-/* Error handling. */
+/*--- Error handling ---*/
 extern const char *guestfs_last_error (guestfs_h *g);
 
 typedef void (*guestfs_error_handler_cb) (guestfs_h *g, void *data, const char *msg);
@@ -51,7 +52,7 @@ extern guestfs_error_handler_cb guestfs_get_error_handler (guestfs_h *g, void **
 extern void guestfs_set_out_of_memory_handler (guestfs_h *g, guestfs_abort_cb);
 extern guestfs_abort_cb guestfs_get_out_of_memory_handler (guestfs_h *g);
 
-/* Events. */
+/*--- Events ---*/
 typedef void (*guestfs_log_message_cb) (guestfs_h *g, void *data, char *buf, int len);
 typedef void (*guestfs_subprocess_quit_cb) (guestfs_h *g, void *data);
 typedef void (*guestfs_launch_done_cb) (guestfs_h *g, void *data);
@@ -60,13 +61,16 @@ extern void guestfs_set_log_message_callback (guestfs_h *g, guestfs_log_message_
 extern void guestfs_set_subprocess_quit_callback (guestfs_h *g, guestfs_subprocess_quit_cb cb, void *opaque);
 extern void guestfs_set_launch_done_callback (guestfs_h *g, guestfs_launch_done_cb cb, void *opaque);
 
+/*--- Structures and actions ---*/
 #include <rpc/xdr.h>
 #include <guestfs-structs.h>
 #include <guestfs-actions.h>
 
-/* PRIVATE: These are NOT part of the public, stable API, and can
- * change at any time!  We export them because they are used by some
- * of the language bindings.
+/*--- Private ---
+ *
+ * These are NOT part of the public, stable API, and can change at any
+ * time!  We export them because they are used by some of the language
+ * bindings.
  */
 extern void *guestfs_safe_malloc (guestfs_h *g, size_t nbytes);
 extern void *guestfs_safe_calloc (guestfs_h *g, size_t n, size_t s);
