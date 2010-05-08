8e007581c91a5193e6aec0f6f9aeb379a935c9d4
Richard Jones <rjones@redhat.com>
2010-05-08 09:04:28 +0100

    fish: Make '-x' option enable traces instead of using separate echo path.
    Previously we had separate code paths for echoing commands (-x)
    and tracing (guestfs_set_trace).  This just unifies that so that
    the guestfish -x option enables tracing.

--- fish/fish.c
+++ fish/fish.c
@@ -72,7 +72,6 @@ guestfs_h *g;
 int read_only = 0;
 int quit = 0;
 int verbose = 0;
-int echo_commands = 0;
 int remote_control_listen = 0;
 int remote_control = 0;
 int exit_on_error = 1;
@@ -361,7 +360,7 @@ main (int argc, char *argv[])
       exit (EXIT_SUCCESS);
 
     case 'x':
-      echo_commands = 1;
+      guestfs_set_trace (g, 1);
       break;
 
     case HELP_OPTION:
@@ -856,13 +855,6 @@ issue_command (const char *cmd, char *argv[], const char *pipecmd)
 
   /* This counts the commands issued, starting at 1. */
   command_num++;
-
-  if (echo_commands) {
-    printf ("%s", cmd);
-    for (i = 0; argv[i] != NULL; ++i)
-      printf (" %s", argv[i]);
-    printf ("\n");
-  }
 
   /* For | ... commands.  Annoyingly we can't use popen(3) here. */
   if (pipecmd) {
