b76fd51e142494058ca0082b547aa5e747ad4b79
Richard Jones <rjones@redhat.com>
2010-05-13 10:17:48 +0100

    Combine common code in daemon/tar.c.
    This commit shouldn't result in any change in the semantics
    of the code.

--- daemon/tar.c
+++ daemon/tar.c
@@ -35,15 +35,16 @@ write_cb (void *fd_ptr, const void *buf, size_t len)
 }
 
 /* Has one FileIn parameter. */
-int
-do_tar_in (const char *dir)
+static int
+do_tXz_in (const char *dir, const char *filter)
 {
   int err, r;
   FILE *fp;
   char *cmd;
 
   /* "tar -C /sysroot%s -xf -" but we have to quote the dir. */
-  if (asprintf_nowarn (&cmd, "tar -C %R -xf -", dir) == -1) {
+  if (asprintf_nowarn (&cmd, "tar -C %R -%sxf -",
+                       dir, filter) == -1) {
     err = errno;
     r = cancel_receive ();
     errno = err;
@@ -94,138 +95,30 @@ do_tar_in (const char *dir)
   return 0;
 }
 
-/* Has one FileOut parameter. */
-int
-do_tar_out (const char *dir)
-{
-  int r;
-  FILE *fp;
-  char *cmd;
-  char buf[GUESTFS_MAX_CHUNK_SIZE];
-
-  /* "tar -C /sysroot%s -cf - ." but we have to quote the dir. */
-  if (asprintf_nowarn (&cmd, "tar -C %R -cf - .", dir) == -1) {
-    reply_with_perror ("asprintf");
-    return -1;
-  }
-
-  if (verbose)
-    fprintf (stderr, "%s\n", cmd);
-
-  fp = popen (cmd, "r");
-  if (fp == NULL) {
-    reply_with_perror ("%s", cmd);
-    free (cmd);
-    return -1;
-  }
-  free (cmd);
-
-  /* Now we must send the reply message, before the file contents.  After
-   * this there is no opportunity in the protocol to send any error
-   * message back.  Instead we can only cancel the transfer.
-   */
-  reply (NULL, NULL);
-
-  while ((r = fread (buf, 1, sizeof buf, fp)) > 0) {
-    if (send_file_write (buf, r) < 0) {
-      pclose (fp);
-      return -1;
-    }
-  }
-
-  if (ferror (fp)) {
-    perror (dir);
-    send_file_end (1);		/* Cancel. */
-    pclose (fp);
-    return -1;
-  }
-
-  if (pclose (fp) != 0) {
-    perror (dir);
-    send_file_end (1);		/* Cancel. */
-    return -1;
-  }
-
-  if (send_file_end (0))	/* Normal end of file. */
-    return -1;
-
-  return 0;
-}
-
 /* Has one FileIn parameter. */
-static int
-do_tXz_in (const char *dir, char filter)
+int
+do_tar_in (const char *dir)
 {
-  int err, r;
-  FILE *fp;
-  char *cmd;
-
-  /* "tar -C /sysroot%s -zxf -" but we have to quote the dir. */
-  if (asprintf_nowarn (&cmd, "tar -C %R -%cxf -", dir, filter) == -1) {
-    err = errno;
-    r = cancel_receive ();
-    errno = err;
-    if (r != -2) reply_with_perror ("asprintf");
-    return -1;
-  }
-
-  if (verbose)
-    fprintf (stderr, "%s\n", cmd);
-
-  fp = popen (cmd, "w");
-  if (fp == NULL) {
-    err = errno;
-    r = cancel_receive ();
-    errno = err;
-    if (r != -2) reply_with_perror ("%s", cmd);
-    free (cmd);
-    return -1;
-  }
-  free (cmd);
-
-  int fd = fileno (fp);
-
-  r = receive_file (write_cb, &fd);
-  if (r == -1) {		/* write error */
-    r = cancel_receive ();
-    if (r != -2) reply_with_error ("write error on directory: %s", dir);
-    pclose (fp);
-    return -1;
-  }
-  if (r == -2) {		/* cancellation from library */
-    pclose (fp);
-    /* Do NOT send any error. */
-    return -1;
-  }
-
-  if (pclose (fp) != 0) {
-    if (r == -1)                /* if r == 0, file transfer ended already */
-      r = cancel_receive ();
-    if (r != -2)
-      reply_with_error ("tar subcommand failed on directory: %s", dir);
-    return -1;
-  }
-
-  return 0;
+  return do_tXz_in (dir, "");
 }
 
 /* Has one FileIn parameter. */
 int
 do_tgz_in (const char *dir)
 {
-  return do_tXz_in (dir, 'z');
+  return do_tXz_in (dir, "z");
 }
 
 /* Has one FileIn parameter. */
 int
 do_txz_in (const char *dir)
 {
-  return do_tXz_in (dir, 'J');
+  return do_tXz_in (dir, "J");
 }
 
 /* Has one FileOut parameter. */
 static int
-do_tXz_out (const char *dir, char filter)
+do_tXz_out (const char *dir, const char *filter)
 {
   int r;
   FILE *fp;
@@ -233,7 +126,7 @@ do_tXz_out (const char *dir, char filter)
   char buf[GUESTFS_MAX_CHUNK_SIZE];
 
   /* "tar -C /sysroot%s -zcf - ." but we have to quote the dir. */
-  if (asprintf_nowarn (&cmd, "tar -C %R -%ccf - .", dir, filter) == -1) {
+  if (asprintf_nowarn (&cmd, "tar -C %R -%scf - .", dir, filter) == -1) {
     reply_with_perror ("asprintf");
     return -1;
   }
@@ -283,14 +176,21 @@ do_tXz_out (const char *dir, char filter)
 
 /* Has one FileOut parameter. */
 int
+do_tar_out (const char *dir)
+{
+  return do_tXz_out (dir, "");
+}
+
+/* Has one FileOut parameter. */
+int
 do_tgz_out (const char *dir)
 {
-  return do_tXz_out (dir, 'z');
+  return do_tXz_out (dir, "z");
 }
 
 /* Has one FileOut parameter. */
 int
 do_txz_out (const char *dir)
 {
-  return do_tXz_out (dir, 'J');
+  return do_tXz_out (dir, "J");
 }
