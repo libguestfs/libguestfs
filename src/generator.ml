a51433257723984ad313c631ea5b16dc0977fe2b
Richard Jones <rjones@redhat.com>
2010-05-19 12:25:03 +0100

    generator: Some String parameters should be OptString (RHBZ#501894).
    I haven't checked the list of functions exhaustively, but
    these are the obvious ones.
    (cherry picked from commit e715451fae0ba738973af98a4e506b6c5564626a)

--- src/generator.ml
+++ src/generator.ml
@@ -572,7 +572,7 @@ The first character of C<param> string must be a C<-> (dash).
 
 C<value> can be NULL.");
 
-  ("set_qemu", (RErr, [String "qemu"]), -1, [FishAlias "qemu"],
+  ("set_qemu", (RErr, [OptString "qemu"]), -1, [FishAlias "qemu"],
    [],
    "set the qemu binary",
    "\
@@ -604,7 +604,7 @@ Return the current qemu binary.
 This is always non-NULL.  If it wasn't set already, then this will
 return the default qemu binary name.");
 
-  ("set_path", (RErr, [String "searchpath"]), -1, [FishAlias "path"],
+  ("set_path", (RErr, [OptString "searchpath"]), -1, [FishAlias "path"],
    [],
    "set the search path",
    "\
