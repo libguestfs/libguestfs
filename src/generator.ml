f55864b828abd1bbfebc0a5fcf96057d46b1a760
Richard W.M. Jones <rjones@redhat.com>
2010-05-21 18:20:28 +0100

    Build workaround for Python 2.4.x in RHEL 5.
    See:
    http://www.python.org/dev/peps/pep-0353/#conversion-guidelines
    (cherry picked from commit 012b1c71f4eda3ae779da51d7d81c199d90f667d)

--- src/generator.ml
+++ src/generator.ml
@@ -8784,6 +8784,12 @@ and generate_python_c () =
   pr "\
 #include <Python.h>
 
+#if PY_VERSION_HEX < 0x02050000
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
