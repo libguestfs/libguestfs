472722a72df89895bb11a1244eafa7915b1af116
Richard W.M. Jones <rjones@redhat.com>
2010-11-24 18:10:50 +0000

    python: Translate C examples into Python and include documentation.

---
+++ python/examples/create_disk.py
@@ -0,0 +1,61 @@
+# Example showing how to create a disk image.
+
+import os
+import guestfs
+
+output = "disk.img"
+
+g = guestfs.GuestFS ()
+
+# Create a raw-format sparse disk image, 512 MB in size.
+f = open (output, "w")
+f.truncate (512 * 1024 * 1024)
+f.close ()
+
+# Set the trace flag so that we can see each libguestfs call.
+g.set_trace (1)
+
+# Set the autosync flag so that the disk will be synchronized
+# automatically when the libguestfs handle is closed.
+g.set_autosync (1)
+
+# Attach the disk image to libguestfs.
+g.add_drive_opts (output, format = "raw", readonly = 0)
+
+# Run the libguestfs back-end.
+g.launch ()
+
+# Get the list of devices.  Because we only added one drive
+# above, we expect that this list should contain a single
+# element.
+devices = g.list_devices ()
+assert (len (devices) == 1)
+
+# Partition the disk as one single MBR partition.
+g.part_disk (devices[0], "mbr")
+
+# Get the list of partitions.  We expect a single element, which
+# is the partition we have just created.
+partitions = g.list_partitions ()
+assert (len (partitions) == 1)
+
+# Create a filesystem on the partition.
+g.mkfs ("ext4", partitions[0])
+
+# Now mount the filesystem so that we can add files.
+g.mount_options ("", partitions[0], "/")
+
+# Create some files and directories.
+g.touch ("/empty")
+message = "Hello, world\n"
+g.write ("/hello", message)
+g.mkdir ("/foo")
+
+# This one uploads the local file /etc/resolv.conf into
+# the disk image.
+g.upload ("/etc/resolv.conf", "/foo/resolv.conf")
+
+# Because 'autosync' was set (above) we can just close the handle
+# and the disk contents will be synchronized.  You can also do
+# this manually by calling g.umount_all and g.sync.
+del g
---
+++ python/examples/inspect_vm.py
@@ -0,0 +1,57 @@
+# Example showing how to inspect a virtual machine disk.
+
+import sys
+import guestfs
+
+assert (len (sys.argv) == 2)
+disk = sys.argv[1]
+
+g = guestfs.GuestFS ()
+
+# Attach the disk image read-only to libguestfs.
+g.add_drive_opts (disk, readonly=1)
+
+# Run the libguestfs back-end.
+g.launch ()
+
+# Ask libguestfs to inspect for operating systems.
+roots = g.inspect_os ()
+if len (roots) == 0:
+    raise (Error ("inspect_vm: no operating systems found"))
+
+for root in roots:
+    print "Root device: %s" % root
+
+    # Print basic information about the operating system.
+    print "  Product name: %s" % (g.inspect_get_product_name (root))
+    print "  Version:      %d.%d" % \
+        (g.inspect_get_major_version (root),
+         g.inspect_get_minor_version (root))
+    print "  Type:         %s" % (g.inspect_get_type (root))
+    print "  Distro:       %s" % (g.inspect_get_distro (root))
+
+    # Mount up the disks, like guestfish -i.
+    #
+    # Sort keys by length, shortest first, so that we end up
+    # mounting the filesystems in the correct order.
+    mps = g.inspect_get_mountpoints (root)
+    def compare (a, b):
+        if len(a[0]) > len(b[0]):
+            return 1
+        elif len(a[0]) == len(b[0]):
+            return 0
+        else:
+            return -1
+    mps.sort (compare)
+    for mp_dev in mps:
+        g.mount_ro (mp_dev[1], mp_dev[0])
+
+    # If /etc/issue.net file exists, print up to 3 lines.
+    filename = "/etc/issue.net"
+    if g.is_file (filename):
+        print "--- %s ---" % filename
+        lines = g.head_n (3, filename)
+        for line in lines: print line
+
+    # Unmount everything.
+    g.umount_all ()
