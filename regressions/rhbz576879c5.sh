cbd8da6d4dd2e4cbc3b87fbc7cb7d6129eb69172
Richard W.M. Jones <rjones@redhat.com>
2011-03-17 11:57:40 +0000

    regressions: Split the test rhbz576879.sh into two halves.
    We suspect that there are in fact two separate bugs.  In any
    case it makes sense for the two tests to be done separately.
    
    Note that these tests still fail.

--- regressions/rhbz576879c5.sh
+++ regressions/rhbz576879c5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 # libguestfs
-# Copyright (C) 2010 Red Hat Inc.
+# Copyright (C) 2010-2011 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,30 +17,12 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 # Regression test for:
-# https://bugzilla.redhat.com/show_bug.cgi?id=576879
-# upload loses synchronization if the disk is not mounted
+# https://bugzilla.redhat.com/show_bug.cgi?id=576879#c5
+# where both ends send cancel messages simultaneously.
 
 set -e
 
 rm -f test1.img
-
-# Somewhere after 1.7, the first test started to hang.
-#
-# Somewhere between ~ 1.5.23 and ~ 1.5.24 the second test started
-# to hang.
-#
-# We don't understand why.  I have disabled this test and reopened
-# the upstream bug.
-# -- RWMJ 2010-12-06
-
-../fish/guestfish -N disk <<EOF
--upload $srcdir/rhbz576879.sh /test.sh
-# Shouldn't lose synchronization, so next command should work:
-ping-daemon
-EOF
-
-# Second patch tests the problem found in comment 5 where both ends
-# send cancel messages simultaneously.
 
 ../fish/guestfish -N disk <<EOF
 -tar-in /tmp/nosuchfile /blah
