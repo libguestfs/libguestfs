5c3c7e8825341e18c9449976f8a321a04cc78d79
Richard W.M. Jones <rjones@redhat.com>
2011-06-18 13:00:00 +0100

    perl: Don't use qw() as parentheses.
    In Perl 5.14:
    
    Use of qw(...) as parentheses is deprecated at perl/blib/lib/Sys/Guestfs/Lib.pm line 1111.

--- perl/lib/Sys/Guestfs/Lib.pm
+++ perl/lib/Sys/Guestfs/Lib.pm
@@ -1108,7 +1108,7 @@ sub _find_grub_prefix
     die(__"Can't find grub on guest") unless($g->exists('/boot/grub/menu.lst'));
 
     # Look for the most specific mount point in mounts
-    foreach my $path qw(/boot/grub /boot /) {
+    foreach my $path (qw(/boot/grub /boot /)) {
         if(exists($mounts->{$path})) {
             return "" if($path eq '/');
             return $path;
