d025e91f6751505c70b7b5f492ee72c67e274ecf
Richard W.M. Jones <rjones@redhat.com>
2011-07-19 17:54:35 +0100

    java: Add guestfs-java(3) man page.

---
+++ java/examples/CreateDisk.java
@@ -0,0 +1,87 @@
+// Example showing how to create a disk image.
+
+import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
+import com.redhat.et.libguestfs.*;
+
+public class CreateDisk
+{
+    static String output = "disk.img";
+
+    public static void main (String[] argv)
+    {
+        try {
+            GuestFS g = new GuestFS ();
+
+            // Create a raw-format sparse disk image, 512 MB in size.
+            RandomAccessFile f = new RandomAccessFile (output, "rw");
+            f.setLength (512 * 1024 * 1024);
+            f.close ();
+
+            // Set the trace flag so that we can see each libguestfs call.
+            g.set_trace (true);
+
+            // Set the autosync flag so that the disk will be synchronized
+            // automatically when the libguestfs handle is closed.
+            g.set_autosync (true);
+
+            // Attach the disk image to libguestfs.
+            Map<String, Object> optargs = new HashMap<String, Object>() {
+                {
+                    put ("format", "raw");
+                    put ("readonly", Boolean.FALSE);
+                }
+            };
+            g.add_drive_opts (output, optargs);
+
+            // Run the libguestfs back-end.
+            g.launch ();
+
+            // Get the list of devices.  Because we only added one drive
+            // above, we expect that this list should contain a single
+            // element.
+            String[] devices = g.list_devices ();
+            if (devices.length != 1)
+                throw new Error ("expected a single device from list-devices");
+
+            // Partition the disk as one single MBR partition.
+            g.part_disk (devices[0], "mbr");
+
+            // Get the list of partitions.  We expect a single element, which
+            // is the partition we have just created.
+            String[] partitions = g.list_partitions ();
+            if (partitions.length != 1)
+                throw new Error ("expected a single partition from list-partitions");
+
+            // Create a filesystem on the partition.
+            g.mkfs ("ext4", partitions[0]);
+
+            // Now mount the filesystem so that we can add files.
+            g.mount_options ("", partitions[0], "/");
+
+            // Create some files and directories.
+            g.touch ("/empty");
+            String message = "Hello, world\n";
+            g.write ("/hello", message.getBytes());
+            g.mkdir ("/foo");
+
+            // This one uploads the local file /etc/resolv.conf into
+            // the disk image.
+            g.upload ("/etc/resolv.conf", "/foo/resolv.conf");
+
+            // Because 'autosync' was set (above) we can just close the handle
+            // and the disk contents will be synchronized.  You can also do
+            // this manually by calling g#umount_all and g#sync.
+            //
+            // Note also that handles are automatically closed if they are
+            // reaped by the garbage collector.  You only need to call close
+            // if you want to close the handle right away.
+            g.close ();
+        }
+        catch (Exception exn) {
+            System.err.println (exn);
+            System.exit (1);
+        }
+    }
+}
---
+++ java/examples/InspectVM.java
@@ -0,0 +1,99 @@
+// Example showing how to inspect a virtual machine disk.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.redhat.et.libguestfs.*;
+
+public class InspectVM
+{
+    static final Comparator<String> COMPARE_KEYS_LEN =
+        new Comparator<String>() {
+        public int compare (String k1, String k2) {
+            return k1.length() - k2.length();
+        }
+    };
+
+    public static void main (String[] argv)
+    {
+        try {
+            if (argv.length != 1)
+                throw new Error ("usage: InspectVM disk.img");
+
+            String disk = argv[0];
+
+            GuestFS g = new GuestFS ();
+
+            // Attach the disk image read-only to libguestfs.
+            Map<String, Object> optargs = new HashMap<String, Object>() {
+                {
+                    //put ("format", "raw");
+                    put ("readonly", Boolean.TRUE);
+                }
+            };
+
+            g.add_drive_opts (disk, optargs);
+
+            // Run the libguestfs back-end.
+            g.launch ();
+
+            // Ask libguestfs to inspect for operating systems.
+            String roots[] = g.inspect_os ();
+            if (roots.length == 0)
+                throw new Error ("inspect_vm: no operating systems found");
+
+            for (String root : roots) {
+                System.out.println ("Root device: " + root);
+
+                // Print basic information about the operating system.
+                System.out.println ("  Product name: " +
+                                    g.inspect_get_product_name (root));
+                System.out.println ("  Version:      " +
+                                    g.inspect_get_major_version (root) +
+                                    "." +
+                                    g.inspect_get_minor_version (root));
+                System.out.println ("  Type:         " +
+                                    g.inspect_get_type (root));
+                System.out.println ("  Distro:       " +
+                                    g.inspect_get_distro (root));
+
+                // Mount up the disks, like guestfish -i.
+                //
+                // Sort keys by length, shortest first, so that we end up
+                // mounting the filesystems in the correct order.
+                Map<String,String> mps = g.inspect_get_mountpoints (root);
+                List<String> mps_keys = new ArrayList (mps.keySet ());
+                Collections.sort (mps_keys, COMPARE_KEYS_LEN);
+
+                for (String mp : mps_keys) {
+                    String dev = mps.get (mp);
+                    try {
+                        g.mount_ro (dev, mp);
+                    }
+                    catch (Exception exn) {
+                        System.err.println (exn + " (ignored)");
+                    }
+                }
+
+                // If /etc/issue.net file exists, print up to 3 lines.
+                String filename = "/etc/issue.net";
+                if (g.is_file (filename)) {
+                    System.out.println ("--- " + filename + " ---");
+                    String[] lines = g.head_n (3, filename);
+                    for (String line : lines)
+                        System.out.println (line);
+                }
+
+                // Unmount everything.
+                g.umount_all ();
+            }
+        }
+        catch (Exception exn) {
+            System.err.println (exn);
+            System.exit (1);
+        }
+    }
+}
