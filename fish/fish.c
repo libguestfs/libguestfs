c38cb92ec478c4dca83e4d38963720f463d52314
Richard W.M. Jones <rjones@redhat.com>
2011-08-23 19:26:35 +0100

    Coverity: fix memory leak in guestfish.

--- fish/fish.c
+++ fish/fish.c
@@ -1370,9 +1370,8 @@ parse_string_list (const char *str)
     /* We've reached the end of a token. We shouldn't still be in quotes. */
     if (in_quote) {
       fprintf (stderr, _("Runaway quote in string \"%s\"\n"), str);
-
       free_n_strings (argv, argv_len);
-
+      free (tok);
       return NULL;
     }
 
