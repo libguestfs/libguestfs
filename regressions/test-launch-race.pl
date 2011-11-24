db17f3793a6eeed9ab676755c245a53fdff0a23d
Richard W.M. Jones <rjones@redhat.com>
2011-11-24 16:54:17 +0000

    regressions: Allow test-launch-race.pl to be skipped.
    export SKIP_TEST_LAUNCH_RACE_PL=1
    will cause this test to be skipped.
    (cherry picked from commit 3bbcbd5eeadabb1cc5a40b2f8ae586e6aafd39c6)

--- regressions/test-launch-race.pl
+++ regressions/test-launch-race.pl
@@ -25,6 +25,9 @@ use POSIX;
 
 use Sys::Guestfs;
 
+# Allow this test to be skipped.
+exit 0 if $ENV{SKIP_TEST_LAUNCH_RACE_PL};
+
 # Use a temporary TMPDIR to ensure it's clean
 my $tmpdir = tempdir (CLEANUP => 1);
 $ENV{TMPDIR} = $tmpdir;
