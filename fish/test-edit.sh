7c21e49c2c83092e9fd5f40c43a03c764a6d3663
Richard W.M. Jones <rjones@redhat.com>
2012-02-10 10:32:55 +0000

    fish: edit: Preserve permissions, UID, GID, SELinux context when editing files (RHBZ#788641).

--- fish/test-edit.sh
+++ fish/test-edit.sh
@@ -36,13 +36,22 @@ export EDITOR="echo second line of text >>"
 output=$(
 ../fish/guestfish -N fs -m /dev/sda1 <<EOF
 write /file.txt "this is a test\n"
+chmod 0600 /file.txt
+chown 10 11 /file.txt
 edit /file.txt
 cat /file.txt
+stat /file.txt | grep mode:
+stat /file.txt | grep uid:
+stat /file.txt | grep gid:
 EOF
 )
 
 if [ "$output" != "this is a test
-second line of text" ]; then
+second line of text
+
+mode: 33152
+uid: 10
+gid: 11" ]; then
     echo "$0: error: output of guestfish after edit command did not match expected output"
     echo "$output"
     exit 1
