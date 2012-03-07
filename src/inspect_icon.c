4fbb3a313090089e2eb008f58955d7ddad20a67b
Richard W.M. Jones <rjones@redhat.com>
2012-03-07 17:29:38 +0000

    Hide stderr of bmptopng.
    This program is noisy on stderr.  Send that to /dev/null.
    (cherry picked from commit e40f408faeea204417bc46024590a07272eeda92)

--- src/inspect_icon.c
+++ src/inspect_icon.c
@@ -365,7 +365,7 @@ icon_windows_xp (guestfs_h *g, struct inspect_fs *fs, const char *explorer,
 
   cmd = safe_asprintf (g,
                        WRESTOOL " -x --type=2 --name=143 %s | "
-                       BMPTOPNM " | " PNMTOPNG " > %s",
+                       BMPTOPNM " 2>/dev/null | " PNMTOPNG " > %s",
           explorer, pngfile);
   r = system (cmd);
   if (r == -1 || WEXITSTATUS (r) != 0) {
@@ -400,7 +400,8 @@ icon_windows_7 (guestfs_h *g, struct inspect_fs *fs, const char *explorer,
 
   cmd = safe_asprintf (g,
                        WRESTOOL " -x --type=2 --name=6801 %s | "
-                       BMPTOPNM " | " PAMCUT " -bottom 54 | " PNMTOPNG " > %s",
+                       BMPTOPNM " 2>/dev/null | " PAMCUT " -bottom 54 | "
+                       PNMTOPNG " > %s",
           explorer, pngfile);
   r = system (cmd);
   if (r == -1 || WEXITSTATUS (r) != 0) {
