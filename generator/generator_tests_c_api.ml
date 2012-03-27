074f5cbe6f4c90992eeef14f0a34775aa5e97758
Richard W.M. Jones <rjones@redhat.com>
2012-03-27 17:39:59 +0100

    tests: Don't include internal guestfs header in tests. (cherry picked from commit f35aadcd86a081756b35c1eb4326a9d23e82f21e)

--- generator/generator_tests_c_api.ml
+++ generator/generator_tests_c_api.ml
@@ -41,7 +41,16 @@ let rec generate_tests () =
 #include <fcntl.h>
 
 #include \"guestfs.h\"
-#include \"guestfs-internal.h\"
+
+#define STREQ(a,b) (strcmp((a),(b)) == 0)
+//#define STRCASEEQ(a,b) (strcasecmp((a),(b)) == 0)
+#define STRNEQ(a,b) (strcmp((a),(b)) != 0)
+//#define STRCASENEQ(a,b) (strcasecmp((a),(b)) != 0)
+//#define STREQLEN(a,b,n) (strncmp((a),(b),(n)) == 0)
+//#define STRCASEEQLEN(a,b,n) (strncasecmp((a),(b),(n)) == 0)
+#define STRNEQLEN(a,b,n) (strncmp((a),(b),(n)) != 0)
+//#define STRCASENEQLEN(a,b,n) (strncasecmp((a),(b),(n)) != 0)
+//#define STRPREFIX(a,b) (strncmp((a),(b),strlen((b))) == 0)
 
 static guestfs_h *g;
 static int suppress_error = 0;
