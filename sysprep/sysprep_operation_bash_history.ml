6c276606d7cd124b2a0fcf50526bfa89142144d5
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-04-12 16:27:16 +0800

    sysprep: remove the bash history of users
    Remove the bash history of users in home directory,
    and history of root.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

---
+++ sysprep/sysprep_operation_bash_history.ml
@@ -0,0 +1,46 @@
+(* virt-sysprep
+ * Copyright (C) 2012 Fujitsu Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Sysprep_operation
+
+module G = Guestfs
+
+let bash_history_perform g root =
+  let typ = g#inspect_get_type root in
+  if typ <> "windows" then (
+    let files = g#glob_expand "/home/*/.bash_history" in
+    Array.iter (
+      fun file -> try g#rm file with G.Error _ -> ();
+    ) files;
+    (try g#rm "/root/.bash_history" with G.Error _ -> ());
+    []
+  )
+  else []
+
+let bash_history_op = {
+  name = "bash-history";
+  pod_description = "\
+Remove the bash history in the guest.
+
+Remove the bash history of user \"root\" and any other users
+who have a C<.bash_history> file in their home directory.";
+  extra_args = [];
+  perform = bash_history_perform;
+}
+
+let () = register_operation bash_history_op
