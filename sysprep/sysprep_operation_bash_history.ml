1f373aa24667fef62378d58ac546ddd2adaf37e9
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-04-13 15:36:28 +0800

    sysprep: bash_history: fix copyright
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- sysprep/sysprep_operation_bash_history.ml
+++ sysprep/sysprep_operation_bash_history.ml
@@ -1,5 +1,5 @@
 (* virt-sysprep
- * Copyright (C) 2012 Fujitsu Inc.
+ * Copyright (C) 2012 FUJITSU LIMITED
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
