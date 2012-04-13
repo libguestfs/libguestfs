ecdfd2c8cccbf8aa27b5a90d66437132ebb501b7
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-04-13 15:36:27 +0800

    sysprep: remove .ssh directory in users' directory
    Remove the .ssh directory of user "root" and any other
    users who have a .ssh directory in their home directory.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

---
+++ sysprep/sysprep_operation_ssh_userdir.ml
@@ -0,0 +1,46 @@
+(* virt-sysprep
+ * Copyright (C) 2012 FUJITSU LIMITED
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Sysprep_operation
+
+module G = Guestfs
+
+let ssh_userdir_perform g root =
+  let typ = g#inspect_get_type root in
+  if typ <> "windows" then (
+    let dirs = g#glob_expand "/home/*/.ssh" in
+    Array.iter (
+      fun dir -> g#rm_rf dir;
+    ) dirs;
+    g#rm_rf "/root/.ssh";
+    []
+  )
+  else []
+
+let ssh_userdir_op = {
+  name = "ssh-userdir";
+  pod_description = "\
+Remove C<.ssh> directories in the guest.
+
+Remove the C<.ssh> directory of user \"root\" and any other
+users who have a C<.ssh> directory in their home directory.";
+  extra_args = [];
+  perform = ssh_userdir_perform;
+}
+
+let () = register_operation ssh_userdir_op
