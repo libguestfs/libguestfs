580ae3297a04a1f531eeb5c42e060260626d410f
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-04-25 10:25:09 +0800

    sysprep: remove the db and log of sssd
    Remove the db and log of sssd.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

---
+++ sysprep/sysprep_operation_sssd_db_log.ml
@@ -0,0 +1,50 @@
+(* virt-sysprep
+ * Copyright (C) 2012 FUJITSU LIMITED
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Sysprep_operation
+
+module G = Guestfs
+
+let sssd_db_log_perform g root =
+  let typ = g#inspect_get_type root in
+  if typ <> "windows" then (
+    let paths = [ "/var/log/sssd/*";
+                  "/var/lib/sss/db/*" ] in
+    List.iter (
+      fun path ->
+        let files = g#glob_expand path in
+        Array.iter (
+          fun file ->
+            try g#rm file with G.Error _ -> ()
+        ) files;
+    ) paths;
+
+    []
+  )
+  else []
+
+let sssd_db_log_op = {
+  name = "sssd-db-log";
+  enabled_by_default = true;
+  heading = "Remove the database and log files of sssd";
+  pod_description = None;
+  extra_args = [];
+  perform = sssd_db_log_perform;
+}
+
+let () = register_operation sssd_db_log_op
