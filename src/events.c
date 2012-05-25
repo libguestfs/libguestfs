93ba687128efbe113ae37f0a63c70dc890772d44
Richard W.M. Jones <rjones@redhat.com>
2012-05-25 15:08:27 +0100

    trace: Fix escaping of non-printable signed characters. (cherry picked from commit fba81add94cde015afe8a65245cb4e60a1d3e6bf)

--- src/events.c
+++ src/events.c
@@ -163,7 +163,7 @@ guestfs___call_callbacks_message (guestfs_h *g, uint64_t event,
         case '\t': fputs ("\\t", stderr); break;
         case '\v': fputs ("\\v", stderr); break;
         default:
-          fprintf (stderr, "\\x%x", (unsigned) c);
+          fprintf (stderr, "\\x%x", (unsigned char) c);
         }
       }
     }
