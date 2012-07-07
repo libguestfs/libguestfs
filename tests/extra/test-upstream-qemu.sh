e6dec159488f589df62c9f59477281f8264a19c8
Richard W.M. Jones <rjones@redhat.com>
2012-07-07 16:19:55 +0100

    extra-tests: Add a test using upstream qemu.

---
+++ tests/extra/test-upstream-qemu.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -
+# Copyright (C) 2012 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+set -e
+#set -x
+
+if [ ! -d "$QEMUDIR" ]; then
+    echo "$0: \$QEMUDIR not a directory, tests against upstream qemu skipped"
+    exit 0
+fi
+
+QEMU="$QEMUDIR/x86_64-softmmu/qemu-system-x86_64"
+if ! "$QEMU" --help >/dev/null 2>&1; then
+    echo "$0: $QEMU not executable, tests against upstream qemu skipped"
+    exit 0
+fi
+
+"$QEMU" --version
+
+LIBGUESTFS_QEMU=$abs_srcdir/test-upstream-qemu-wrapper.sh
+export LIBGUESTFS_QEMU
+export QEMU
+
+$MAKE -C ../.. quickcheck
+$MAKE -C ../c-api check
