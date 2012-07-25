e68336d72a7d71a44d80f0d13c40ff44653e5c01
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-07-25 13:56:16 +0800

    sysprep: remove the local machine ID
    remove the local machine ID, leave it empty, let it be generated
    during next booting.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>
    
    RWMJ:
     - Whitespace change.
     - Remove completed items from TODO.

---
+++ sysprep/sysprep_operation_machine_id.ml
@@ -0,0 +1,47 @@
+(* virt-sysprep
+ * Copyright (C) 2012 Fujitsu Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Sysprep_operation
+open Sysprep_gettext.Gettext
+
+module G = Guestfs
+
+let machine_id_perform g root =
+  let typ = g#inspect_get_type root in
+  if typ <> "windows" then (
+    let path = "/etc/machine-id" in
+    (try g#rm path with G.Error _ -> ());
+    (try g#touch path with G.Error _ -> ());
+    []
+  )
+  else []
+
+let machine_id_op = {
+  name = "machine-id";
+  enabled_by_default = true;
+  heading = s_"Remove the local machine ID";
+  pod_description = Some (s_"\
+The machine ID is usually generated from a random source during system
+installation and stays constant for all subsequent boots.  Optionally,
+for stateless systems it is generated during runtime at boot if it is
+found to be empty.");
+  extra_args = [];
+  perform = machine_id_perform;
+}
+
+let () = register_operation machine_id_op
