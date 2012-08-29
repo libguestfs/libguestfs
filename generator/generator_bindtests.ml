e900884d7e764c21ca90b19f3620bf7ce78d71d0
Richard W.M. Jones <rjones@redhat.com>
2012-08-29 10:31:09 +0100

    perl: bindtests: Fix 64 bit integers on input on 32 bit Perl interpreter. (cherry picked from commit 15cd9dde5e0e4a7d510de71ef8991750971cf6d1)

--- generator/generator_bindtests.ml
+++ generator/generator_bindtests.ml
@@ -307,7 +307,7 @@ my $g = Sys::Guestfs->new ();
         | CallStringList xs ->
             "[" ^ String.concat "," (List.map (sprintf "\"%s\"") xs) ^ "]"
         | CallInt i -> string_of_int i
-        | CallInt64 i -> Int64.to_string i
+        | CallInt64 i -> "\"" ^ Int64.to_string i ^ "\""
         | CallBool b -> if b then "1" else "0"
         | CallBuffer s -> "\"" ^ c_quote s ^ "\""
       ) args
@@ -316,7 +316,7 @@ my $g = Sys::Guestfs->new ();
         function
         | CallOBool (n, v)    -> "'" ^ n ^ "' => " ^ if v then "1" else "0"
         | CallOInt (n, v)     -> "'" ^ n ^ "' => " ^ string_of_int v
-        | CallOInt64 (n, v)   -> "'" ^ n ^ "' => " ^ Int64.to_string v
+        | CallOInt64 (n, v)   -> "'" ^ n ^ "' => \"" ^ Int64.to_string v ^ "\""
         | CallOString (n, v)  -> "'" ^ n ^ "' => '" ^ v ^ "'"
       ) optargs
     )
