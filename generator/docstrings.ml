a95d4261b8cedc31bfc3c5358be7b346b27d457b
Richard W.M. Jones <rjones@redhat.com>
2012-09-24 09:24:33 +0100

    generator: Allow an optional emacs-mode at the top of generated files.

--- generator/docstrings.ml
+++ generator/docstrings.ml
@@ -61,7 +61,7 @@ type comment_style =
   | ErlangStyle
 type license = GPLv2plus | LGPLv2plus
 
-let generate_header ?(extra_inputs = []) comment license =
+let generate_header ?(extra_inputs = []) ?emacs_mode comment license =
   let inputs = "generator/ *.ml" :: extra_inputs in
   let c = match comment with
     | CStyle ->         pr "/* "; " *"
@@ -70,7 +70,12 @@ let generate_header ?(extra_inputs = []) comment license =
     | OCamlStyle ->     pr "(* "; " *"
     | HaskellStyle ->   pr "{- "; "  "
     | ErlangStyle ->    pr "%% "; "% " in
-  pr "libguestfs generated file\n";
+  pr "libguestfs generated file";
+  (match emacs_mode with
+  | None -> ()
+  | Some mode -> pr " -*- %s -*-" mode
+  );
+  pr "\n";
   pr "%s WARNING: THIS FILE IS GENERATED FROM:\n" c;
   List.iter (pr "%s   %s\n" c) inputs;
   pr "%s ANY CHANGES YOU MAKE TO THIS FILE WILL BE LOST.\n" c;
