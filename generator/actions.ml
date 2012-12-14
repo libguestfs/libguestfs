956e30effa25eee00fb9e8f5303a6a1fe7409037
Matthew Booth <mbooth@redhat.com>
2012-12-14 15:53:27 +0000

    Add support for getting and setting GPT partition type GUIDs
    New APIs:
      part_set_gpt_type
      part_get_gpt_type

--- generator/actions.ml
+++ generator/actions.ml
@@ -10692,6 +10692,37 @@ not always, the name of a Windows drive, eg. C<E:>." };
 Return the list of partitions in the volume named C<volume> in the disk
 group with GUID <diskgroup>." };
 
+  { defaults with
+    name = "part_set_gpt_type";
+    style = RErr, [Device "device"; Int "partnum"; String "guid"], [];
+    proc_nr = Some 392;
+    tests = [];
+    shortdesc = "set the type GUID of a GPT partition";
+    longdesc = "\
+Set the type GUID of numbered GPT partition C<partnum> to C<guid>. Return an
+error if the partition table of C<device> isn't GPT, or if C<guid> is not a
+valid GUID.
+
+See L<http://en.wikipedia.org/wiki/GUID_Partition_Table#Partition_type_GUIDs>
+for a useful list of type GUIDs." };
+
+  { defaults with
+    name = "part_get_gpt_type";
+    style = RString "guid", [Device "device"; Int "partnum"], [];
+    proc_nr = Some 393;
+    tests = [
+      InitGPT, Always, TestOutput (
+        [["part_set_gpt_type"; "/dev/sda"; "1";
+          "01234567-89AB-CDEF-0123-456789ABCDEF"];
+         ["part_get_gpt_type"; "/dev/sda"; "1"]],
+         "01234567-89AB-CDEF-0123-456789ABCDEF");
+    ];
+    shortdesc = "get the type GUID of a GPT partition";
+    longdesc = "\
+Return the type GUID of numbered GPT partition C<partnum>. For MBR partitions,
+return an appropriate GUID corresponding to the MBR type. Behaviour is undefined
+for other partition types." };
+
 ]
 
 (* Non-API meta-commands available only in guestfish.
