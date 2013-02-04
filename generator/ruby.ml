d012b729faaa3db73bf198f82ae28d567005b137
Richard W.M. Jones <rjones@redhat.com>
2013-02-04 12:17:31 +0000

    ruby: Include "guestfs-internal-frontend.h" in extension.
    Also undefine '_' symbol defined by Ruby's internal headers.
    
    This fixes commit ec3b75e5ffd1abea1400f6609ae9c61eab0dfa71.

--- generator/ruby.ml
+++ generator/ruby.ml
@@ -46,7 +46,13 @@ let rec generate_ruby_c () =
 #include <ruby.h>
 #pragma GCC diagnostic pop
 
+/* ruby/defines.h defines '_'. */
+#ifdef _
+#undef _
+#endif
+
 #include \"guestfs.h\"
+#include \"guestfs-internal-frontend.h\"
 
 #include \"extconf.h\"
 
