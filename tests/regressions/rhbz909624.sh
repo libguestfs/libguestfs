635da25ff75e52705f1b781809fd27d15124ea0c
Richard W.M. Jones <rjones@redhat.com>
2013-02-11 17:35:11 +0000

    tests: Add a regression test for stack overflow in events (RHBZ#909624).
    Note this is a check-slow test since generating 1,000,000 progress
    events take a few minutes.
    (cherry picked from commit 05444da98351c9269fc8c6278f8bb5c5cfd4bb83)

---
+++ tests/regressions/rhbz909624.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -
+# libguestfs
+# Copyright (C) 2013 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Regression test for:
+# https://bugzilla.redhat.com/show_bug.cgi?id=909624
+#
+# Ensure that progress messages don't cause a stack overflow.  Note if
+# this fails, it fails by causing guestfish to segfault (inside
+# libguestfs).
+
+set -e
+export LANG=C
+
+../../fish/guestfish <<EOF
+
+add-ro /dev/null
+run
+
+# Generate a million progress messages.  Typically around 14000 is
+# enough to trigger the stack overflow.
+debug progress "1 1"
+
+EOF
