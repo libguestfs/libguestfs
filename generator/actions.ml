8571e2eb3120e57ed632c520ba107ac40a20304a
Richard W.M. Jones <rjones@redhat.com>
2013-02-18 13:02:59 +0000

    generator: Whitespace changes. (cherry picked from commit 0e0eec76d9dca032e3eb45c0bbe024edc7bb16f8)

--- generator/actions.ml
+++ generator/actions.ml
@@ -4426,8 +4426,8 @@ to return the existing UUID of a filesystem." };
            ["set_e2uuid"; "/dev/sdc"; uuid];
            ["get_e2uuid"; "/dev/sdc"]], uuid)
       ]);
-      shortdesc = "get the ext2/3/4 filesystem UUID";
-      longdesc = "\
+    shortdesc = "get the ext2/3/4 filesystem UUID";
+    longdesc = "\
 This returns the ext2/3/4 filesystem UUID of the filesystem on
 C<device>." };
 
@@ -5430,7 +5430,7 @@ a limitation of the kernel or swap tools." };
         InitEmpty, Always, TestRun (
           [["part_disk"; "/dev/sda"; "mbr"];
            ["mkswap_U"; uuid; "/dev/sda1"]])
-       ]);
+      ]);
     shortdesc = "create a swap partition with an explicit UUID";
     longdesc = "\
 Create a swap partition on C<device> with UUID C<uuid>." };
@@ -6235,7 +6235,7 @@ labeled swap partition." };
           [["mkswap"; "/dev/sdc"; "NOARG"; uuid];
            ["swapon_uuid"; uuid];
            ["swapoff_uuid"; uuid]])
-       ]);
+      ]);
     shortdesc = "enable swap on swap partition by UUID";
     longdesc = "\
 This command enables swap to a swap partition with the given UUID.
@@ -6508,7 +6508,7 @@ This creates an ext2 external journal on C<device> with label C<label>." };
            ["mount"; "/dev/sda2"; "/"];
            ["write"; "/new"; "new file contents"];
            ["cat"; "/new"]], "new file contents")
-       ]);
+      ]);
     shortdesc = "make ext2/3/4 external journal with UUID";
     longdesc = "\
 This creates an ext2 external journal on C<device> with UUID C<uuid>." };
@@ -7842,7 +7842,7 @@ To find a filesystem from the label, use C<guestfs_findfs_label>." };
         InitBasicFS, Always, TestOutput (
           [["set_e2uuid"; "/dev/sda1"; uuid];
            ["vfs_uuid"; "/dev/sda1"]], uuid)
-       ]);
+      ]);
     shortdesc = "get the filesystem UUID";
     longdesc = "\
 This returns the filesystem UUID of the filesystem on
@@ -8187,7 +8187,7 @@ See also C<guestfs_upload>, C<guestfs_pwrite>." };
        let size = string_of_int ((Unix.stat "COPYING.LIB").Unix.st_size - 100) in
        [
          InitScratchFS, Always, TestOutput (
-          (* Pick a file from cwd which isn't likely to change. *)
+           (* Pick a file from cwd which isn't likely to change. *)
            [["mkdir"; "/download_offset"];
             ["upload"; "../../COPYING.LIB"; "/download_offset/COPYING.LIB"];
             ["download_offset"; "/download_offset/COPYING.LIB"; "testdownload.tmp"; offset; size];
@@ -10153,7 +10153,7 @@ This is a wrapper around the L<hivex(3)> call of the same name." };
         [["part_disk"; "/dev/sda"; "mbr"];
          ["mkfs"; "xfs"; "/dev/sda1"; ""; "NOARG"; ""; ""];
          ["xfs_repair"; "/dev/sda1"; ""; "true"; ""; ""; ""; ""; ""; ""; "NOARG"; "NOARG"]
-      ])
+        ])
     ];
     shortdesc = "repair an XFS filesystem";
     longdesc = "\
