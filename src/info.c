5e5026b730dfea1e940e961a5b7e516a9c9bda72
Richard W.M. Jones <rjones@redhat.com>
2013-03-11 18:33:17 +0000

    qemu-img info: Use --output json and YAJL (JSON parser) to safely parse output (RHBZ#920225).
    The old parser is still used if either YAJL is not available or
    qemu-img info doesn't support the --output json option.

--- src/info.c
+++ src/info.c
@@ -27,12 +27,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <assert.h>
+
+#if HAVE_YAJL
+#include <yajl/yajl_tree.h>
+#endif
 
 #include "guestfs.h"
 #include "guestfs-internal.h"
 #include "guestfs-internal-actions.h"
 #include "guestfs_protocol.h"
 
+static int which_parser (guestfs_h *g);
+static char *get_disk_format (guestfs_h *g, const char *filename);
+static int64_t get_disk_virtual_size (guestfs_h *g, const char *filename);
+static int get_disk_has_backing_file (guestfs_h *g, const char *filename);
+#if HAVE_YAJL
+static yajl_val get_json_output (guestfs_h *g, const char *filename);
+#endif
 static char *old_parser_disk_format (guestfs_h *g, const char *filename);
 static int64_t old_parser_disk_virtual_size (guestfs_h *g, const char *filename);
 static int old_parser_disk_has_backing_file (guestfs_h *g, const char *filename);
@@ -40,20 +52,284 @@ static int old_parser_disk_has_backing_file (guestfs_h *g, const char *filename)
 char *
 guestfs__disk_format (guestfs_h *g, const char *filename)
 {
-  return old_parser_disk_format (g, filename);
+  switch (which_parser (g)) {
+  case QEMU_IMG_INFO_NEW_PARSER:
+    return get_disk_format (g, filename);
+  case QEMU_IMG_INFO_OLD_PARSER:
+    return old_parser_disk_format (g, filename);
+  case QEMU_IMG_INFO_UNKNOWN_PARSER:
+  default:
+    abort ();
+  }
 }
 
 int64_t
 guestfs__disk_virtual_size (guestfs_h *g, const char *filename)
 {
-  return old_parser_disk_virtual_size (g, filename);
+  switch (which_parser (g)) {
+  case QEMU_IMG_INFO_NEW_PARSER:
+    return get_disk_virtual_size (g, filename);
+  case QEMU_IMG_INFO_OLD_PARSER:
+    return old_parser_disk_virtual_size (g, filename);
+  case QEMU_IMG_INFO_UNKNOWN_PARSER:
+  default:
+    abort ();
+  }
 }
 
 int
 guestfs__disk_has_backing_file (guestfs_h *g, const char *filename)
 {
-  return old_parser_disk_has_backing_file (g, filename);
+  switch (which_parser (g)) {
+  case QEMU_IMG_INFO_NEW_PARSER:
+    return get_disk_has_backing_file (g, filename);
+  case QEMU_IMG_INFO_OLD_PARSER:
+    return old_parser_disk_has_backing_file (g, filename);
+  case QEMU_IMG_INFO_UNKNOWN_PARSER:
+  default:
+    abort ();
+  }
 }
+
+#if HAVE_YAJL
+
+# ifdef HAVE_ATTRIBUTE_CLEANUP
+# define CLEANUP_YAJL_TREE_FREE __attribute__((cleanup(cleanup_yajl_tree_free)))
+
+static void
+cleanup_yajl_tree_free (void *ptr)
+{
+  yajl_tree_free (* (yajl_val *) ptr);
+}
+
+# else
+# define CLEANUP_YAJL_TREE_FREE
+# endif
+#endif /* HAVE_ATTRIBUTE_CLEANUP */
+
+static char *
+get_disk_format (guestfs_h *g, const char *filename)
+{
+#if HAVE_YAJL
+
+  size_t i, len;
+  CLEANUP_YAJL_TREE_FREE yajl_val tree = get_json_output (g, filename);
+
+  if (tree == NULL)
+    return NULL;
+
+  if (! YAJL_IS_OBJECT (tree))
+    goto bad_type;
+
+  len = YAJL_GET_OBJECT(tree)->len;
+  for (i = 0; i < len; ++i) {
+    if (STREQ (YAJL_GET_OBJECT(tree)->keys[i], "format")) {
+      const char *str;
+      yajl_val node = YAJL_GET_OBJECT(tree)->values[i];
+      if (YAJL_IS_NULL (node))
+        goto bad_type;
+      str = YAJL_GET_STRING (node);
+      if (str == NULL)
+        goto bad_type;
+      return safe_strdup (g, str); /* caller frees */
+    }
+  }
+
+ bad_type:
+  error (g, _("qemu-img info: JSON output did not contain 'format' key"));
+  return NULL;
+
+#else /* !HAVE_YAJL */
+  abort ();
+#endif /* !HAVE_YAJL */
+}
+
+static int64_t
+get_disk_virtual_size (guestfs_h *g, const char *filename)
+{
+#if HAVE_YAJL
+
+  size_t i, len;
+  CLEANUP_YAJL_TREE_FREE yajl_val tree = get_json_output (g, filename);
+
+  if (tree == NULL)
+    return -1;
+
+  if (! YAJL_IS_OBJECT (tree))
+    goto bad_type;
+
+  len = YAJL_GET_OBJECT(tree)->len;
+  for (i = 0; i < len; ++i) {
+    if (STREQ (YAJL_GET_OBJECT(tree)->keys[i], "virtual-size")) {
+      yajl_val node = YAJL_GET_OBJECT(tree)->values[i];
+      if (YAJL_IS_NULL (node))
+        goto bad_type;
+      if (! YAJL_IS_NUMBER (node))
+        goto bad_type;
+      if (! YAJL_IS_INTEGER (node)) {
+        error (g, _("qemu-img info: 'virtual-size' is not representable as a 64 bit integer"));
+        return -1;
+      }
+      return YAJL_GET_INTEGER (node);
+    }
+  }
+
+ bad_type:
+  error (g, _("qemu-img info: JSON output did not contain 'virtual-size' key"));
+  return -1;
+
+#else /* !HAVE_YAJL */
+  abort ();
+#endif /* !HAVE_YAJL */
+}
+
+static int
+get_disk_has_backing_file (guestfs_h *g, const char *filename)
+{
+#if HAVE_YAJL
+
+  size_t i, len;
+  CLEANUP_YAJL_TREE_FREE yajl_val tree = get_json_output (g, filename);
+
+  if (tree == NULL)
+    return -1;
+
+  if (! YAJL_IS_OBJECT (tree))
+    goto bad_type;
+
+  len = YAJL_GET_OBJECT(tree)->len;
+  for (i = 0; i < len; ++i) {
+    if (STREQ (YAJL_GET_OBJECT(tree)->keys[i], "backing-filename")) {
+      yajl_val node = YAJL_GET_OBJECT(tree)->values[i];
+      /* Work on the assumption that if this field is null, it means
+       * no backing file, rather than being an error.
+       */
+      if (YAJL_IS_NULL (node))
+        return 0;
+      return 1;
+    }
+  }
+
+  return 0; /* no backing-filename key means no backing file */
+
+ bad_type:
+  error (g, _("qemu-img info: JSON output was not an object"));
+  return -1;
+
+#else /* !HAVE_YAJL */
+  abort ();
+#endif /* !HAVE_YAJL */
+}
+
+#if HAVE_YAJL
+
+/* Run 'qemu-img info --output json filename', and parse the output
+ * as JSON, returning a JSON tree and handling errors.
+ */
+static void parse_json (guestfs_h *g, void *treevp, const char *input, size_t len);
+
+static yajl_val
+get_json_output (guestfs_h *g, const char *filename)
+{
+  CLEANUP_CMD_CLOSE struct command *cmd = guestfs___new_command (g);
+  int r;
+  yajl_val tree = NULL;
+
+  guestfs___cmd_add_arg (cmd, "qemu-img");
+  guestfs___cmd_add_arg (cmd, "info");
+  guestfs___cmd_add_arg (cmd, "--output");
+  guestfs___cmd_add_arg (cmd, "json");
+  guestfs___cmd_add_arg (cmd, filename);
+  guestfs___cmd_set_stdout_callback (cmd, parse_json, &tree,
+                                     CMD_STDOUT_FLAG_WHOLE_BUFFER);
+  r = guestfs___cmd_run (cmd);
+  if (r == -1)
+    return NULL;
+  if (!WIFEXITED (r) || WEXITSTATUS (r) != 0) {
+    error (g, _("qemu-img info: %s: child process failed"), filename);
+    return NULL;
+  }
+
+  if (tree == NULL)
+    return NULL;        /* parse_json callback already set an error */
+
+  return tree;          /* caller must call yajl_tree_free (tree) */
+}
+
+/* Parse the JSON document printed by qemu-img info --output json. */
+static void
+parse_json (guestfs_h *g, void *treevp, const char *input, size_t len)
+{
+  yajl_val *tree_ret = treevp;
+  CLEANUP_FREE char *input_copy = NULL;
+  char parse_error[256];
+
+  assert (*tree_ret == NULL);
+
+  /* 'input' is not \0-terminated; we have to make it so. */
+  input_copy = safe_strndup (g, input, len);
+
+  debug (g, "%s: qemu-img info JSON output:\n%s\n", __func__, input_copy);
+
+  *tree_ret = yajl_tree_parse (input_copy, parse_error, sizeof parse_error);
+  if (*tree_ret == NULL) {
+    if (strlen (parse_error) > 0)
+      error (g, _("qemu-img info: JSON parse error: %s"), parse_error);
+    else
+      error (g, _("qemu-img info: unknown JSON parse error"));
+  }
+}
+
+static void help_contains_output_json (guestfs_h *g, void *datav, const char *help_line, size_t len);
+
+/* Choose new (JSON) or old (human) parser? */
+static int
+which_parser (guestfs_h *g)
+{
+  if (g->qemu_img_info_parser == QEMU_IMG_INFO_UNKNOWN_PARSER) {
+    int qemu_img_supports_json = 0;
+    CLEANUP_CMD_CLOSE struct command *cmd = guestfs___new_command (g);
+
+    guestfs___cmd_add_arg (cmd, "qemu-img");
+    guestfs___cmd_add_arg (cmd, "--help");
+    guestfs___cmd_set_stdout_callback (cmd,
+                                       help_contains_output_json,
+                                       &qemu_img_supports_json, 0);
+    guestfs___cmd_run (cmd);
+    /* ignore return code, which would usually be 1 */
+
+    if (qemu_img_supports_json)
+      g->qemu_img_info_parser = QEMU_IMG_INFO_NEW_PARSER;
+    else
+      g->qemu_img_info_parser = QEMU_IMG_INFO_OLD_PARSER;
+  }
+
+  debug (g, "%s: g->qemu_img_info_parser = %d",
+         __func__, g->qemu_img_info_parser);
+
+  return g->qemu_img_info_parser;
+}
+
+static void
+help_contains_output_json (guestfs_h *g, void *datav,
+                           const char *help_line, size_t len)
+{
+  if (strstr (help_line, "--output") != NULL &&
+      strstr (help_line, "json") != NULL) {
+    * (int *) datav = 1;
+  }
+}
+
+#else /* !HAVE_YAJL */
+
+/* With no YAJL, only the old parser is available. */
+static int
+which_parser (guestfs_h *g)
+{
+  return g->qemu_img_info_parser = QEMU_IMG_INFO_OLD_PARSER;
+}
+
+#endif /* !HAVE_YAJL */
 
 /*----------------------------------------------------------------------
  * This is the old parser for the old / human-readable output of
