cc93840d31e440e64e27ee13df3afd134ef2e68f
Richard W.M. Jones <rjones@redhat.com>
2013-04-29 15:22:34 +0100

    tests: Add a regression test for RHBZ#957772.

---
+++ tests/regressions/rhbz957772.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -
+# libguestfs
+# Copyright (C) 2013 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Regression test for:
+# https://bugzilla.redhat.com/show_bug.cgi?id=957772
+# Ensure that tar-out and base64-out commands properly quote arguments.
+
+set -e
+export LANG=C
+
+../../fish/guestfish -N fs -m /dev/sda1 <<EOF
+mkdir "/test 1"
+touch "/test 2"
+tar-out "/test 1" /dev/null
+base64-out "/test 2" /dev/null
+EOF
+
+rm test1.img
