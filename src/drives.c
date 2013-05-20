f3bbd1f4ddc26b266d38666c1526549833ceeab7
Richard W.M. Jones <rjones@redhat.com>
2013-05-20 15:27:36 +0100

    gluster: Don't force gluster port number to be specified.
    Gluster can choose its own port number.

--- src/drives.c
+++ src/drives.c
@@ -200,16 +200,8 @@ create_drive_gluster (guestfs_h *g,
     return NULL;
   }
 
-
   if (nr_servers != 1) {
     error (g, _("gluster: you must specify exactly one server"));
-    return NULL;
-  }
-
-  if ((servers[0].transport == drive_transport_none ||
-       servers[0].transport == drive_transport_tcp) &&
-      servers[0].port == 0) {
-    error (g, _("gluster: port number must be specified"));
     return NULL;
   }
 
