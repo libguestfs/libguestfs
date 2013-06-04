efb83c4ab3e3a8bfca60596a778965948d6c2e40
Richard W.M. Jones <rjones@redhat.com>
2013-06-04 11:52:18 +0100

    build: Add common-rules.mk, common rules for all Makefiles.
    This file is mainly a central place to:
    
     - include localenv if it exists, and
    
     - define the RHEL 5 backwards compatibility macros, instead of
       spreading them over every other file.
    
    (cherry picked from commit 49bdaabc7d011e7fb55e457d6fb865c175d1c757)
    (cherry picked from commit 7bc67024f0200abf3f267ae582aab5186f4f1b70)

--- subdir-rules.mk
+++ subdir-rules.mk
@@ -15,6 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+include $(top_srcdir)/common-rules.mk
+
 # Define a force dependency which will always be rebuilt
 .PHONY: force
 
