d9e9cc39c92288b713320b4d7f6f66d1b1e4ff42
Richard W.M. Jones <rjones@redhat.com>
2013-07-29 14:47:56 +0100

    RHEL: Disable unsupported remote drive protocols (RHBZ#962113).
    This disables support for unsupported remote drive protocols:
    
     * ftp
     * ftps
     * http
     * https
     * iscsi
     * ssh
    
    Note 'nbd' is not disabled, and of course 'file' works.

--- tests/disks/test-qemu-drive-libvirt.sh
+++ tests/disks/test-qemu-drive-libvirt.sh
@@ -65,13 +65,6 @@ check_output
 grep -sq -- '-drive file=rbd:abc-def/ghi-jkl:auth_supported=none,' "$DEBUG_QEMU_FILE" || fail ceph2
 rm "$DEBUG_QEMU_FILE"
 
-# iSCSI.
-
-$guestfish -d iscsi run ||:
-check_output
-grep -sq -- '-drive file=iscsi://1.2.3.4:1234/iqn.2003-01.org.linux-iscsi.fedora' "$DEBUG_QEMU_FILE" || fail iscsi
-rm "$DEBUG_QEMU_FILE"
-
 # NBD.
 
 $guestfish -d nbd run ||:
