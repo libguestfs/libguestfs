0a22c69b7f7b7fda8dbfdf06db3fbdf9c2994c8f
Richard W.M. Jones <rjones@redhat.com>
2013-08-11 19:45:12 +0100

    lib: Turn 'random_chars' function used by libvirt backend into utility function.
    (cherry picked from commit e5285cc4210683887a5a4a853fe4e6a72c23e74d)

--- src/guestfs-internal-frontend.h
+++ src/guestfs-internal-frontend.h
@@ -97,6 +97,7 @@ extern size_t guestfs___count_strings (char *const *);
 extern char *guestfs___concat_strings (char *const *);
 extern char *guestfs___join_strings (const char *sep, char *const *);
 extern char *guestfs___exit_status_to_string (int status, const char *cmd_name, char *buffer, size_t buflen);
+extern int guestfs___random_string (char *ret, size_t len);
 
 /* These functions are used internally by the CLEANUP_* macros.
  * Don't call them directly.
