libguestfs/libguestfs

commit d241876bd14fcb0284a793ac48c4627e96398c45

Remove use of gnulib progname module.
It's simpler to use the glibc 'program_invocation_short_name(3)'
feature, and fall back to a generic solution.  Also remove risky
assignments to argv[0].

(cherry picked from commit 142b874ce8ddc09a06e765df66445a3611d61c29)

align/domains.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libintl.h>
+#include <errno.h>
 
 #ifdef HAVE_LIBVIRT
 #include <libvirt/libvirt.h>

df/domains.c
@@ -21,14 +21,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <libintl.h>
 
 #ifdef HAVE_LIBVIRT
 #include <libvirt/libvirt.h>
 #include <libvirt/virterror.h>
 #endif
-
-#include "progname.h"
 
 #include "guestfs.h"
 #include "options.h"

align/scan.c
@@ -25,6 +25,7 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
 #include <locale.h>
 #include <assert.h>
 #include <libintl.h>
@@ -33,8 +34,6 @@
 #include <libvirt/libvirt.h>
 #include <libvirt/virterror.h>
 #endif
-
-#include "progname.h"
 
 #include "guestfs.h"
 #include "options.h"
@@ -86,9 +85,6 @@ usage (int status)
 int
 main (int argc, char *argv[])
 {
-  /* Set global program name that is not polluted with libtool artifacts.  */
-  set_program_name (argv[0]);
-
   setlocale (LC_ALL, "");
   bindtextdomain (PACKAGE, LOCALEBASEDIR);
   textdomain (PACKAGE);
@@ -123,8 +119,6 @@ main (int argc, char *argv[])
     fprintf (stderr, _("guestfs_create: failed to create handle\n"));
     exit (EXIT_FAILURE);
   }
-
-  argv[0] = (char *) program_name;
 
   for (;;) {
     c = getopt_long (argc, argv, options, long_options, &option_index);

bootstrap
@@ -75,7 +75,6 @@ openat
 perror
 pipe2
 pread
-progname
 read-file
 readlink
 select

cat/virt-cat.c
@@ -24,11 +24,11 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
 #include <locale.h>
 #include <assert.h>
 #include <libintl.h>
 
-#include "progname.h"
 #include "c-ctype.h"
 
 #include "guestfs.h"
@@ -83,9 +83,6 @@ usage (int status)
 int
 main (int argc, char *argv[])
 {
-  /* Set global program name that is not polluted with libtool artifacts.  */
-  set_program_name (argv[0]);
-
   setlocale (LC_ALL, "");
   bindtextdomain (PACKAGE, LOCALEBASEDIR);
   textdomain (PACKAGE);
@@ -117,8 +114,6 @@ main (int argc, char *argv[])
     fprintf (stderr, _("guestfs_create: failed to create handle\n"));
     exit (EXIT_FAILURE);
   }
-
-  argv[0] = (char *) program_name;
 
   for (;;) {
     c = getopt_long (argc, argv, options, long_options, &option_index);

cat/virt-filesystems.c
@@ -24,6 +24,7 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
 #include <locale.h>
 #include <assert.h>
 #include <string.h>
@@ -31,7 +32,6 @@
 
 #include "c-ctype.h"
 #include "human.h"
-#include "progname.h"
 
 #include "guestfs.h"
 #include "options.h"
@@ -131,9 +131,6 @@ usage (int status)
 int
 main (int argc, char *argv[])
 {
-  /* Set global program name that is not polluted with libtool artifacts.  */
-  set_program_name (argv[0]);
-
   setlocale (LC_ALL, "");
   bindtextdomain (PACKAGE, LOCALEBASEDIR);
   textdomain (PACKAGE);
@@ -190,8 +187,6 @@ main (int argc, char *argv[])
     fprintf (stderr, _("guestfs_create: failed to create handle\n"));
     exit (EXIT_FAILURE);
   }
-
-  argv[0] = (char *) program_name;
 
   for (;;) {
     c = getopt_long (argc, argv, options, long_options, &option_index);

cat/virt-ls.c
@@ -25,13 +25,13 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <locale.h>
 #include <assert.h>
 #include <time.h>
 #include <libintl.h>
 
 #include "human.h"
-#include "progname.h"
 
 #include "guestfs.h"
 #include "options.h"
@@ -130,9 +130,6 @@ main (int argc, char *argv[])
   /* Current time for --time-days, --time-relative output. */
   time (&now);
 
-  /* Set global program name that is not polluted with libtool artifacts.  */
-  set_program_name (argv[0]);
-
   setlocale (LC_ALL, "");
   bindtextdomain (PACKAGE, LOCALEBASEDIR);
   textdomain (PACKAGE);
@@ -182,8 +179,6 @@ main (int argc, char *argv[])
     fprintf (stderr, _("guestfs_create: failed to create handle\n"));
     exit (EXIT_FAILURE);
   }
-
-  argv[0] = (char *) program_name;
 
   for (;;) {
     c = getopt_long (argc, argv, options, long_options, &option_index);

df/df.c
@@ -29,7 +29,6 @@
 #include <libvirt/virterror.h>
 #endif
 
-#include "progname.h"
 #include "c-ctype.h"
 
 #include "guestfs.h"

commit 6d763b86d6e9631c2b1d00724fd224b53ecf0129

align/domains.c: Remove include of progname.h.
This was inadvertently broken by
commit d241876bd14fcb0284a793ac48c4627e96398c45.  This wasn't
noticed until I updated gnulib.

align/domains.c
@@ -29,8 +29,6 @@
 #include <libvirt/virterror.h>
 #endif
 
-#include "progname.h"
-
 #include "guestfs.h"
 #include "options.h"
 #include "scan.h"