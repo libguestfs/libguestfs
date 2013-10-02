6203c50479c0c693c3fbdca222d28e98f4e094e5
Richard W.M. Jones <rjones@redhat.com>
2013-10-02 17:57:08 +0100

    sysprep: Refactor setting random seed code into a common library.
    This is just code motion.

--- mllib/random_seed.ml
+++ mllib/random_seed.ml
@@ -1,5 +1,5 @@
 (* virt-sysprep
- * Copyright (C) 2012 Red Hat Inc.
+ * Copyright (C) 2012-2013 Red Hat Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,27 +16,27 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *)
 
-open Sysprep_operation
-open Common_gettext.Gettext
+let set_random_seed (g : Guestfs.guestfs) root =
+  let typ = g#inspect_get_type root in
+  if typ = "linux" then (
+    let files = [
+      "/var/lib/random-seed"; (* Fedora *)
+      "/var/lib/urandom/random-seed"; (* Debian *)
+      "/var/lib/misc/random-seed"; (* SuSE *)
+    ] in
+    List.iter (
+      fun file ->
+        if g#is_file file then (
+          (* Get 8 bytes of randomness from the host. *)
+          let chan = open_in "/dev/urandom" in
+          let buf = String.create 8 in
+          really_input chan buf 0 8;
+          close_in chan;
 
-open Random_seed
-
-module G = Guestfs
-
-let random_seed_perform (g : Guestfs.guestfs) root =
-  if set_random_seed g root then [ `Created_files ] else []
-
-let op = {
-  defaults with
-    name = "random-seed";
-    enabled_by_default = true;
-    heading = s_"Generate random seed for guest";
-    pod_description = Some (s_"\
-Write some random bytes from the host into the random seed file of the
-guest.
-
-See L</RANDOM SEED> below.");
-    perform_on_filesystems = Some random_seed_perform;
-}
-
-let () = register_operation op
+          g#write file buf
+        )
+    ) files;
+    true
+  )
+  else
+    false
--- mllib/random_seed.mli
+++ mllib/random_seed.mli
@@ -1,5 +1,5 @@
 (* virt-sysprep
- * Copyright (C) 2012 Red Hat Inc.
+ * Copyright (C) 2012-2013 Red Hat Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,27 +16,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *)
 
-open Sysprep_operation
-open Common_gettext.Gettext
-
-open Random_seed
-
-module G = Guestfs
-
-let random_seed_perform (g : Guestfs.guestfs) root =
-  if set_random_seed g root then [ `Created_files ] else []
-
-let op = {
-  defaults with
-    name = "random-seed";
-    enabled_by_default = true;
-    heading = s_"Generate random seed for guest";
-    pod_description = Some (s_"\
-Write some random bytes from the host into the random seed file of the
-guest.
-
-See L</RANDOM SEED> below.");
-    perform_on_filesystems = Some random_seed_perform;
-}
-
-let () = register_operation op
+val set_random_seed : Guestfs.guestfs -> string -> bool
+(** Set the random seed in the guest.  Returns true if it was able to
+    do set it, false if not. *)
