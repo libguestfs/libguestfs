1caf8a6ca300f09bf7ffa631c51f6ee0940e24f6
Richard W.M. Jones <rjones@redhat.com>
2013-12-13 13:18:47 +0000

    builder: Use a planner to work out how to convert the template to the final image.
    The original template might be, say, xz-compressed raw of a certain
    size.  We need to work out how to convert it to, say, qcow2 with a
    larger size, in as few operations as possible.
    
    Instead of using a nasty aggregation of special cases to do this, use
    a breadth-first search over all possible trees to try to find an
    optimal plan.
    
    (Actually the current implementation isn't optimal, but it's fine for
    virt-builder.)
    
    (cherry picked from commit 62cc7d3361127b4e007f8e23028213852be09124)

---
+++ mllib/planner.mli
@@ -0,0 +1,78 @@
+(* virt-builder
+ * Copyright (C) 2012-2013 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+(** The Planner can plan how to reach a goal by carrying out a series
+    of operations.  You tag the input state and the output state, and
+    give it a list of permitted transitions, and it will return a
+    multi-step plan (list of transitions) from the input state to the
+    output state.
+
+    For example:
+
+    Input tags: xz cached size=4G format=raw
+
+    Output tags: -xz -cached +size=8G +format=raw
+
+    (In this case the "-" before an output tag means the tag MUST NOT
+    appear, and the "+" before an output tag means the tag MUST
+    appear).
+
+    The plan produced might be:
+
+    (1) Run xzcat (removes xz and cached tags).
+
+    (2) Run virt-resize (changes size=4G to size=8G)
+
+    Tags are described as OCaml association lists.  See the OCaml
+    {!List} module.
+
+    Transitions are defined by a function (that the caller supplies)
+    which returns the possible transitions for a given set of tags,
+    and for each possible transition, the weight (higher number =
+    higher cost), and the tag state after that transition.
+
+    The returned plan is a list of transitions.
+
+    The implementation is a simple breadth-first search of the tree of
+    states (each edge in the tree is a transition).  It doesn't work
+    very hard to optimize the weights, so the returned plan is
+    possible, but might not be optimal. *)
+
+type ('name, 'value) tag = 'name * 'value
+
+type ('name, 'value) tags = ('name, 'value) tag list
+  (** An assoc-list of tags. *)
+
+type ('name, 'value, 'task) plan =
+  (('name, 'value) tags * 'task * ('name, 'value) tags) list
+
+type ('name, 'value, 'task) transitions_function = ('name, 'value) tags -> ('task * int * ('name, 'value) tags) list
+
+val plan : ?max_depth:int -> ('name, 'value, 'task) transitions_function -> ('name, 'value) tags -> ('name, 'value) tags * ('name, 'value) tags -> ('name, 'value, 'task) plan
+(** Make a plan.
+
+    [plan transitions itags (goal_must, goal_must_not)] works out a
+    plan, which is a list of tasks that have to be carried out in
+    order to go from the input tags to the goal.  The goal is passed
+    in as a pair of lists: tags that MUST appear and tags that MUST
+    NOT appear.
+
+    The returned value is a {!plan}.
+
+    Raises [Failure "plan"] if no plan was found within [max_depth]
+    transitions. *)
