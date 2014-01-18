4da779139d179e74f3519ae390251f405083f46b
Richard W.M. Jones <rjones@redhat.com>
2014-01-18 15:25:07 +0000

    utils: Add utility function to copy string lists.
    (cherry picked from commit 693a21513b8bf4c61aad76f1860d68f44d5e5e73)

--- src/guestfs-internal-frontend.h
+++ src/guestfs-internal-frontend.h
@@ -95,6 +95,7 @@ extern char *guestfs___safe_asprintf (guestfs_h *g, const char *fs, ...)
 extern void guestfs___free_string_list (char **);
 extern size_t guestfs___count_strings (char *const *);
 extern char *guestfs___concat_strings (char *const *);
+extern char **guestfs___copy_string_list (char *const *);
 extern char *guestfs___join_strings (const char *sep, char *const *);
 extern char *guestfs___exit_status_to_string (int status, const char *cmd_name, char *buffer, size_t buflen);
 extern int guestfs___random_string (char *ret, size_t len);
