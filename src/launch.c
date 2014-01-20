libguestfs/libguestfs

commit 0f26218905816ca86b6ec57070eed802515f4853

Refactor backends so they are modular.
Instead of hard-coding the list of backends into the core of
libguestfs in many places, make each backend into a true module which
registers itself with the core when the library is loaded.

This is a step towards making backends into loadable modules, but it
doesn't actually do that.

This is just code motion.

src/launch.c
@@ -30,25 +30,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <assert.h>
 
 #include "guestfs.h"
 #include "guestfs-internal.h"
 #include "guestfs-internal-actions.h"
 #include "guestfs_protocol.h"
 
-static mode_t get_umask (guestfs_h *g);
+static struct backend {
+  struct backend *next;
+  const char *name;
+  const struct backend_ops *ops;
+} *backends = NULL;
 
-static const struct backend_ops *
-get_backend_ops (guestfs_h *g)
-{
-  switch (g->backend) {
-  case BACKEND_DIRECT:    return &backend_ops_direct;
-  case BACKEND_LIBVIRT:   return &backend_ops_libvirt;
-  case BACKEND_UML:       return &backend_ops_uml;
-  case BACKEND_UNIX:      return &backend_ops_unix;
-  default: abort ();
-  }
-}
+static mode_t get_umask (guestfs_h *g);
 
 int
 guestfs__launch (guestfs_h *g)
@@ -76,8 +71,11 @@ guestfs__launch (guestfs_h *g)
 
   /* Some common debugging information. */
   if (g->verbose) {
+    struct backend *b;
     CLEANUP_FREE char *backend = guestfs_get_backend (g);
 
+    for (b = backends; b != NULL; b = b->next)
+      debug (g, "launch: backend registered: %s", b->name);
     debug (g, "launch: backend=%s", backend);
     debug (g, "launch: tmpdir=%s", g->tmpdir);
     debug (g, "launch: umask=0%03o", get_umask (g));
@@ -85,8 +83,7 @@ guestfs__launch (guestfs_h *g)
   }
 
   /* Launch the appliance. */
-  g->backend_ops = get_backend_ops (g);
-  return g->backend_ops->launch (g, g->backend_arg);
+  return g->backend_ops->launch (g, g->backend_data, g->backend_arg);
 }
 
 /* launch (of the appliance) generates approximate progress
@@ -170,45 +167,42 @@ guestfs__get_pid (guestfs_h *g)
     NOT_SUPPORTED (g, -1,
                    _("the current backend does not support 'get-pid'"));
 
-  return g->backend_ops->get_pid (g);
+  return g->backend_ops->get_pid (g, g->backend_data);
 }
 
 /* Maximum number of disks. */
 int
 guestfs__max_disks (guestfs_h *g)
 {
-  const struct backend_ops *backend_ops = get_backend_ops (g);
-
-  if (backend_ops->max_disks == NULL)
+  if (g->backend_ops->max_disks == NULL)
     NOT_SUPPORTED (g, -1,
                    _("the current backend does not allow max disks to be queried"));
 
-  return backend_ops->max_disks (g);
+  return g->backend_ops->max_disks (g, g->backend_data);
 }
 
 int
 guestfs__internal_set_libvirt_selinux_label (guestfs_h *g, const char *label,
                                              const char *imagelabel)
 {
-  const struct backend_ops *backend_ops = get_backend_ops (g);
-
-  if (backend_ops->set_libvirt_selinux_label == NULL)
+  if (g->backend_ops->set_libvirt_selinux_label == NULL)
     /* Not an error, just ignore it. */
     return 0;
 
-  return backend_ops->set_libvirt_selinux_label (g, label, imagelabel);
+  return g->backend_ops->set_libvirt_selinux_label (g, g->backend_data,
+                                                    label, imagelabel);
 }
 
 int
 guestfs__internal_set_libvirt_selinux_norelabel_disks (guestfs_h *g, int flag)
 {
-  const struct backend_ops *backend_ops = get_backend_ops (g);
-
-  if (backend_ops->set_libvirt_selinux_norelabel_disks == NULL)
+  if (g->backend_ops->set_libvirt_selinux_norelabel_disks == NULL)
     /* Not an error, just ignore it. */
     return 0;
 
-  return backend_ops->set_libvirt_selinux_norelabel_disks (g, flag);
+  return g->backend_ops->set_libvirt_selinux_norelabel_disks (g,
+                                                              g->backend_data,
+                                                              flag);
 }
 
 /* You had to call this function after launch in versions <= 1.0.70,
@@ -406,4 +400,65 @@ get_umask (guestfs_h *g)
   ret = ret ^ 0777;
 
   return ret;
+}
+
+/* Register backends in a global list when the library is loaded. */
+void
+guestfs___register_backend (const char *name, const struct backend_ops *ops)
+{
+  struct backend *b;
+
+  b = malloc (sizeof *b);
+  if (!b) abort ();
+
+  b->name = name;
+  b->ops = ops;
+
+  b->next = backends;
+  backends = b;
+}
+
+/* Set the current backend.  Notes:
+ * (1) Callers must ensure this is only called in the config state.
+ * (2) This shouldn't call 'error' since it may be called early in
+ * handle initialization.  It can return an error code however.
+ */
+int
+guestfs___set_backend (guestfs_h *g, const char *method)
+{
+  struct backend *b;
+  size_t len, arg_offs = 0;
+
+  assert (g->state == CONFIG);
+
+  for (b = backends; b != NULL; b = b->next) {
+    if (STREQ (method, b->name))
+      break;
+    len = strlen (b->name);
+    if (STRPREFIX (method, b->name) && method[len] == ':') {
+      arg_offs = len+1;
+      break;
+    }
+  }
+
+  if (b == NULL)
+    return -1;                  /* Not found. */
+
+  /* At this point, we know it's a valid method. */
+  free (g->backend);
+  g->backend = safe_strdup (g, method);
+  if (arg_offs > 0)
+    g->backend_arg = &g->backend[arg_offs];
+  else
+    g->backend_arg = NULL;
+
+  g->backend_ops = b->ops;
+
+  free (g->backend_data);
+  if (b->ops->data_size > 0)
+    g->backend_data = safe_calloc (g, 1, b->ops->data_size);
+  else
+    g->backend_data = NULL;
+
+  return 0;
 }

src/appliance.c
@@ -666,7 +666,7 @@ hard_link_to_cached_appliance (guestfs_h *g,
    *
    * XXX
    */
-  if (g->backend != BACKEND_UML)
+  if (STRNEQ (g->backend, "uml"))
     (void) utime (filename, NULL);
 
   return 0;

src/drives.c
@@ -1001,7 +1001,7 @@ guestfs__add_drive_opts (guestfs_h *g, const char *filename,
   }
 
   /* ... else, hotplugging case. */
-  if (!g->backend || !g->backend_ops->hot_add_drive) {
+  if (!g->backend_ops->hot_add_drive) {
     error (g, _("the current backend does not support hotplugging drives"));
     free_drive_struct (drv);
     return -1;
@@ -1020,7 +1020,8 @@ guestfs__add_drive_opts (guestfs_h *g, const char *filename,
       drv_index = i;
 
   /* Hot-add the drive. */
-  if (g->backend_ops->hot_add_drive (g, drv, drv_index) == -1) {
+  if (g->backend_ops->hot_add_drive (g, g->backend_data,
+                                     drv, drv_index) == -1) {
     free_drive_struct (drv);
     return -1;
   }
@@ -1172,7 +1173,7 @@ guestfs__remove_drive (guestfs_h *g, const char *label)
     return 0;
   }
   else {                        /* Hotplugging. */
-    if (!g->backend_ops || !g->backend_ops->hot_remove_drive) {
+    if (!g->backend_ops->hot_remove_drive) {
       error (g, _("the current backend does not support hotplugging drives"));
       return -1;
     }
@@ -1180,7 +1181,7 @@ guestfs__remove_drive (guestfs_h *g, const char *label)
     if (guestfs_internal_hot_remove_drive_precheck (g, label) == -1)
       return -1;
 
-    if (g->backend_ops->hot_remove_drive (g, drv, i) == -1)
+    if (g->backend_ops->hot_remove_drive (g, g->backend_data, drv, i) == -1)
       return -1;
 
     free_drive_struct (drv);

src/guestfs-internal.h
@@ -93,14 +93,6 @@
 enum state { CONFIG = 0, LAUNCHING = 1, READY = 2,
              NO_HANDLE = 0xebadebad };
 
-/* Backend. */
-enum backend {
-  BACKEND_DIRECT,
-  BACKEND_LIBVIRT,
-  BACKEND_UML,
-  BACKEND_UNIX,
-};
-
 /* Event. */
 struct event {
   uint64_t event_bitmask;
@@ -197,29 +189,35 @@ struct hv_param {
 
 /* Backend operations. */
 struct backend_ops {
-  int (*launch) (guestfs_h *g, const char *arg); /* Initialize and launch. */
-                                /* Shutdown and cleanup. */
-  int (*shutdown) (guestfs_h *g, int check_for_errors);
+  /* Size (in bytes) of the per-handle data structure needed by this
+   * backend.  The data pointer is allocated and freed by libguestfs
+   * and passed to the functions in the 'void *data' parameter.
+   * Inside the data structure is opaque to libguestfs.  Any strings
+   * etc pointed to by it must be freed by the backend during
+   * shutdown.
+   */
+  size_t data_size;
 
-  int (*get_pid) (guestfs_h *g);         /* get-pid API. */
-  int (*max_disks) (guestfs_h *g);       /* max-disks API. */
+  /* Launch and shut down. */
+  int (*launch) (guestfs_h *g, void *data, const char *arg);
+  int (*shutdown) (guestfs_h *g, void *data, int check_for_errors);
+
+  /* Miscellaneous. */
+  int (*get_pid) (guestfs_h *g, void *data);
+  int (*max_disks) (guestfs_h *g, void *data);
 
   /* Hotplugging drives. */
-  int (*hot_add_drive) (guestfs_h *g, struct drive *drv, size_t drv_index);
-  int (*hot_remove_drive) (guestfs_h *g, struct drive *drv, size_t drv_index);
+  int (*hot_add_drive) (guestfs_h *g, void *data, struct drive *drv, size_t drv_index);
+  int (*hot_remove_drive) (guestfs_h *g, void *data, struct drive *drv, size_t drv_index);
 
   /* These are a hack used to communicate between guestfs_add_domain and
    * the libvirt backend.  We will probably remove these in a future
    * version once we can find a better way to pass this information
    * around.
    */
-  int (*set_libvirt_selinux_label) (guestfs_h *g, const char *label, const char *imagelabel);
-  int (*set_libvirt_selinux_norelabel_disks) (guestfs_h *g, int flag);
+  int (*set_libvirt_selinux_label) (guestfs_h *g, void *data, const char *label, const char *imagelabel);
+  int (*set_libvirt_selinux_norelabel_disks) (guestfs_h *g, void *data, int flag);
 };
-extern struct backend_ops backend_ops_direct;
-extern struct backend_ops backend_ops_libvirt;
-extern struct backend_ops backend_ops_uml;
-extern struct backend_ops backend_ops_unix;
 
 /* Connection module.  A 'connection' represents the appliance console
  * connection plus the daemon connection.  It hides the underlying
@@ -322,10 +320,11 @@ struct guestfs_h
   for (i = 0; i < (g)->nr_drives; ++i)    \
     if (((drv) = (g)->drives[i]) != NULL)
 
-  /* Backend, and associated backend operations. */
-  enum backend backend;
-  char *backend_arg;
+  /* Backend.  NB: Use guestfs___set_backend to change the backend. */
+  char *backend;                /* The full string, always non-NULL. */
+  char *backend_arg;            /* Pointer to the argument part. */
   const struct backend_ops *backend_ops;
+  void *backend_data;           /* Per-handle data. */
 
   /**** Runtime information. ****/
   char *last_error;             /* Last error on handle. */
@@ -408,51 +407,6 @@ struct guestfs_h
   unsigned int nr_requested_credentials;
   virConnectCredentialPtr requested_credentials;
 #endif
-
-  /**** Private data for backends. ****/
-  /* NB: This cannot be a union because of a pathological case where
-   * the user changes backend while reusing the handle to launch
-   * multiple times (not a recommended thing to do).  Some fields here
-   * cache things across launches so that would break if we used a
-   * union.
-   */
-  struct {                      /* Used only by src/launch-appliance.c. */
-    pid_t pid;                  /* Qemu PID. */
-    pid_t recoverypid;          /* Recovery process PID. */
-
-    char *qemu_help;            /* Output of qemu -help. */
-    char *qemu_version;         /* Output of qemu -version. */
-    char *qemu_devices;         /* Output of qemu -device ? */
-
-    /* qemu version (0, 0 if unable to parse). */
-    int qemu_version_major, qemu_version_minor;
-
-    char **cmdline;   /* Only used in child, does not need freeing. */
-    size_t cmdline_size;
-
-    int virtio_scsi;      /* See function qemu_supports_virtio_scsi */
-  } direct;
-
-#ifdef HAVE_LIBVIRT
-  struct {                      /* Used only by src/launch-libvirt.c. */
-    virConnectPtr conn;         /* libvirt connection */
-    virDomainPtr dom;           /* libvirt domain */
-    char *selinux_label;
-    char *selinux_imagelabel;
-    bool selinux_norelabel_disks;
-  } virt;
-#endif
-
-  struct {                      /* Used only by src/launch-uml.c. */
-    pid_t pid;                  /* vmlinux PID. */
-    pid_t recoverypid;          /* Recovery process PID. */
-
-#define UML_UMID_LEN 16
-    char umid[UML_UMID_LEN+1];  /* umid=<...> unique ID. */
-
-    char **cmdline;   /* Only used in child, does not need freeing. */
-    size_t cmdline_size;
-  } uml;
 };
 
 /* Per-filesystem data stored for inspect_os. */
@@ -685,6 +639,8 @@ extern void guestfs___print_timestamped_message (guestfs_h *g, const char *fs, .
 extern void guestfs___launch_send_progress (guestfs_h *g, int perdozen);
 extern char *guestfs___appliance_command_line (guestfs_h *g, const char *appliance_dev, int flags);
 #define APPLIANCE_COMMAND_LINE_IS_TCG 1
+extern void guestfs___register_backend (const char *name, const struct backend_ops *);
+extern int guestfs___set_backend (guestfs_h *g, const char *method);
 
 /* inspect.c */
 extern void guestfs___free_inspect_info (guestfs_h *g);

src/handle.c
@@ -39,7 +39,6 @@
 #include "guestfs-internal-actions.h"
 #include "guestfs_protocol.h"
 
-static int parse_backend (guestfs_h *g, const char *method);
 static int shutdown_backend (guestfs_h *g, int check_for_errors);
 static void close_handles (void);
 
@@ -123,9 +122,12 @@ guestfs_create_flags (unsigned flags, ...)
 #endif
   if (!g->program) goto error;
 
-  if (parse_backend (g, DEFAULT_BACKEND) == -1) {
+  if (guestfs___set_backend (g, DEFAULT_BACKEND) == -1) {
     warning (g, _("libguestfs was built with an invalid default backend, using 'direct' instead"));
-    g->backend = BACKEND_DIRECT;
+    if (guestfs___set_backend (g, "direct") == -1) {
+      warning (g, _("'direct' backend does not work"));
+      goto error;
+    }
   }
 
   if (!(flags & GUESTFS_CREATE_NO_ENVIRONMENT))
@@ -151,7 +153,7 @@ guestfs_create_flags (unsigned flags, ...)
   return g;
 
  error:
-  free (g->backend_arg);
+  free (g->backend);
   free (g->program);
   free (g->path);
   free (g->hv);
@@ -346,6 +348,8 @@ guestfs_close (guestfs_h *g)
   free (g->program);
   free (g->path);
   free (g->hv);
+  free (g->backend);
+  free (g->backend_data);
   free (g->append);
   free (g);
 }
@@ -378,7 +382,7 @@ shutdown_backend (guestfs_h *g, int check_for_errors)
   }
 
   /* Shut down the backend. */
-  if (g->backend_ops->shutdown (g, check_for_errors) == -1)
+  if (g->backend_ops->shutdown (g, g->backend_data, check_for_errors) == -1)
     ret = -1;
 
   /* Close sockets. */
@@ -595,52 +599,10 @@ guestfs__get_program (guestfs_h *g)
   return g->program;
 }
 
-static int
-parse_backend (guestfs_h *g, const char *method)
-{
-  if (STREQ (method, "direct") || STREQ (method, "appliance")) {
-    g->backend = BACKEND_DIRECT;
-    free (g->backend_arg);
-    g->backend_arg = NULL;
-    return 0;
-  }
-
-  if (STREQ (method, "libvirt")) {
-    g->backend = BACKEND_LIBVIRT;
-    free (g->backend_arg);
-    g->backend_arg = NULL;
-    return 0;
-  }
-
-  if (STRPREFIX (method, "libvirt:") && strlen (method) > 8) {
-    g->backend = BACKEND_LIBVIRT;
-    free (g->backend_arg);
-    g->backend_arg = safe_strdup (g, method + 8);
-    return 0;
-  }
-
-  if (STREQ (method, "uml")) {
-    g->backend = BACKEND_UML;
-    free (g->backend_arg);
-    g->backend_arg = NULL;
-    return 0;
-  }
-
-  if (STRPREFIX (method, "unix:") && strlen (method) > 5) {
-    g->backend = BACKEND_UNIX;
-    free (g->backend_arg);
-    g->backend_arg = safe_strdup (g, method + 5);
-    /* Note that we don't check the path exists until launch is called. */
-    return 0;
-  }
-
-  return -1;
-}
-
 int
 guestfs__set_backend (guestfs_h *g, const char *method)
 {
-  if (parse_backend (g, method) == -1) {
+  if (guestfs___set_backend (g, method) == -1) {
     error (g, "invalid backend: %s", method);
     return -1;
   }
@@ -657,46 +619,17 @@ guestfs__set_attach_method (guestfs_h *g, const char *method)
 char *
 guestfs__get_backend (guestfs_h *g)
 {
-  char *ret = NULL;
-
-  switch (g->backend) {
-  case BACKEND_DIRECT:
-    ret = safe_strdup (g, "direct");
-    break;
-
-  case BACKEND_LIBVIRT:
-    if (g->backend_arg == NULL)
-      ret = safe_strdup (g, "libvirt");
-    else
-      ret = safe_asprintf (g, "libvirt:%s", g->backend_arg);
-    break;
-
-  case BACKEND_UML:
-    ret = safe_strdup (g, "uml");
-    break;
-
-  case BACKEND_UNIX:
-    ret = safe_asprintf (g, "unix:%s", g->backend_arg);
-    break;
-  }
-
-  if (ret == NULL)
-    abort ();
-
-  return ret;
+  return safe_strdup (g, g->backend);
 }
 
 char *
 guestfs__get_attach_method (guestfs_h *g)
 {
-  switch (g->backend) {
-  case BACKEND_DIRECT:
+  if (STREQ (g->backend, "direct"))
     /* Return 'appliance' here for backwards compatibility. */
     return safe_strdup (g, "appliance");
 
-  default:
-    return guestfs_get_backend (g);
-  }
+  return guestfs_get_backend (g);
 }
 
 int

commit 69267d8762b586deab6b930131e1d677ba85df8d

launch: Allow backend = "appliance" for backwards compatibility (RHBZ#1055452).
This fixes a regression which was introduced in
commit 0f26218905816ca86b6ec57070eed802515f4853.

src/launch.c
@@ -450,6 +450,10 @@ guestfs___set_backend (guestfs_h *g, const char *method)
 
   assert (g->state == CONFIG);
 
+  /* For backwards compatibility with old code (RHBZ#1055452). */
+  if (STREQ (method, "appliance"))
+    method = "direct";
+
   for (b = backends; b != NULL; b = b->next) {
     if (STREQ (method, b->name))
       break;