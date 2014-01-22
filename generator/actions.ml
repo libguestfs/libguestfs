c7f48b44e150b99817ae941c0f0c4e2af984e743
Colin Walters <walters@verbum.org>
2014-01-22 15:45:08 +0000

    generator/set-uuid: Fix two typos in documentation
    (cherry picked from commit 49c3d819c184f71ac98cd436fb9435f5b3bbddda)

--- generator/actions.ml
+++ generator/actions.ml
@@ -11477,7 +11477,7 @@ parameter.  In future we may allow other flags to be adjusted." };
       ]);
     shortdesc = "set the filesystem UUID";
     longdesc = "\
-Set the filesystem UIUD on C<device> to C<label>.
+Set the filesystem UUID on C<device> to C<uuid>.
 
 Only some filesystem types support setting UUIDs.
 
