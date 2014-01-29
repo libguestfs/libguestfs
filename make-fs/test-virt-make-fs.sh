c86fcc0fd4dde908bce41f9a5ca90b5fcd89f7e9
Richard W.M. Jones <rjones@redhat.com>
2014-01-29 08:00:55 +0000

    make-fs: Make check-valgrind do something.

--- make-fs/test-virt-make-fs.sh
+++ make-fs/test-virt-make-fs.sh
@@ -94,6 +94,6 @@ dd if=/dev/zero of=test.file bs=1024 count=$tarsize
 tar -c -f test.tar test.file
 rm test.file
 
-./virt-make-fs $params -- test.tar output.img
+$VG ./virt-make-fs $params -- test.tar output.img
 
 rm test.tar output.img
