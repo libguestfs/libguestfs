5db1a28461cc42f09532e8b98150e716040d3aa0
Richard W.M. Jones <rjones@redhat.com>
2014-01-29 14:38:50 +0000

    sysprep: Add --scrub option.
    This works like the option in virt-builder with the same name.

--- sysprep/sysprep_operation_delete.ml
+++ sysprep/sysprep_operation_delete.ml
@@ -23,33 +23,48 @@ open Common_gettext.Gettext
 module G = Guestfs
 
 let paths = ref []
-let add_paths path = paths := path :: !paths
+let add_paths ?(scrub = false) path = paths := (scrub, path) :: !paths
 
 let path_perform g root side_effects =
   let paths = List.rev !paths in
   if paths <> [] then (
-    List.iter (fun glob -> Array.iter g#rm_rf (g#glob_expand glob)) paths
+    List.iter (
+      function
+      | false, glob -> Array.iter g#rm_rf (g#glob_expand glob)
+      | true, path -> g#scrub_file path
+    ) paths
   )
 
 let op = {
   defaults with
     name = "delete";
     enabled_by_default = true;
-    heading = s_"Delete specified files or directories";
+    heading = s_"Delete or scrub specified files or directories";
     pod_description = Some (s_"\
-Delete specified files or directories.
-
 Use the I<--delete> option to specify a path to remove.
 
 You can use shell glob characters in the specified path; note that such
 metacharacters might require proper escape.  For example:
 
- virt-sysprep --delete '/var/log/*.log'.");
+ virt-sysprep --delete '/var/log/*.log'
+
+An alternative option, I<--scrub>, can be used to scrub files.  This
+only works for files (not directories) and cannot use globs.
+
+You can use both options as many times as you want.");
     extra_args = [
       { extra_argspec = ("--delete", Arg.String add_paths, s_"path" ^ " " ^ s_"File or directory to be removed on guest");
         extra_pod_argval = Some "PATHNAME";
         extra_pod_description = s_"\
 Delete (recursively) the specified C<PATHNAME> in the guest.";
+      };
+
+      { extra_argspec = ("--scrub", Arg.String (add_paths ~scrub:true), s_"path" ^ " " ^ s_"File to be scrubbed");
+        extra_pod_argval = Some "PATHNAME";
+        extra_pod_description = s_"\
+Scrub (aggressively overwrite then remove) the specified
+file called C<PATHNAME> in the guest.  Only single files can
+be specified using this argument.";
       }
     ];
 
