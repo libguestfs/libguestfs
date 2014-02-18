bb35e725035d3d8e73604a95a8b055cc1073416a
Pino Toscano <ptoscano@redhat.com>
2014-02-18 12:54:23 +0100

    builder: move the XDG path handling in an own file
    Just code motion and renaming, no actual behaviour changes.

---
+++ builder/paths.ml
@@ -0,0 +1,26 @@
+(* virt-builder
+ * Copyright (C) 2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Common_utils
+
+let xdg_cache_home =
+  try Some (Sys.getenv "XDG_CACHE_HOME" // "virt-builder")
+  with Not_found ->
+    try Some (Sys.getenv "HOME" // ".cache" // "virt-builder")
+    with Not_found ->
+      None (* no cache directory *)
