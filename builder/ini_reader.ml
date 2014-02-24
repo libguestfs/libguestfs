623c36262943f6f1e3c3df56d5c56e34ed658e48
Pino Toscano <ptoscano@redhat.com>
2014-02-24 15:08:03 +0100

    builder: split INI C <-> OCaml glue code in own module
    Move in an own module the code which calls the C
    virt_builder_parse_index and does the array -> list conversion of the
    result. This way this code can be easily called also in places different
    than Index_parser without the need to copy the types mapping, etc.
    
    Just code motion, no actual behaviour changes.
    
    (cherry picked from commit 8ed905c7e6a837887a758417bdfd1822b86a214f)

---
+++ builder/ini_reader.ml
@@ -0,0 +1,38 @@
+(* virt-builder
+ * Copyright (C) 2013-2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+type sections = section list
+and section = string * fields                (* [name] + fields *)
+and fields = field list
+and field = string * string option * string  (* key + subkey + value *)
+
+(* Types returned by the C index parser. *)
+type c_sections = c_section array
+and c_section = string * c_fields             (* [name] + fields *)
+and c_fields = field array
+
+(* Calls yyparse in the C code. *)
+external parse_index : string -> c_sections = "virt_builder_parse_index"
+
+let read_ini file =
+  let sections = parse_index file in
+  let sections = Array.to_list sections in
+  List.map (
+    fun (n, fields) ->
+      n, Array.to_list fields
+  ) sections
