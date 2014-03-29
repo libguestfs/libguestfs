8d868255b50be802fa84b173d17358a3269f2a9b
Richard W.M. Jones <rjones@redhat.com>
2014-03-29 16:57:59 +0000

    utils: Move guestfs___validate_guid out of utils.c.
    guestfs___validate_guid was a new function added to utils.c in
    commit beef77403cd9d634b6ff6daa9f33d292e2d320a7.
    
    However utils.c should not include <guestfs-internal.h> since the
    other functions in this file can be used by all front-end code, not
    just libguestfs.so.
    
    This function is only needed in libguestfs.so, so move it to another
    source file, and remove include of <guestfs-internal.h> from utils.c.
    
    Also: use 'size_t' for counting, not 'int'.
    
    This fixes commit beef77403cd9d634b6ff6daa9f33d292e2d320a7.
    
    (cherry picked from commit 768ab2e01dab5a8e7e751c33b72c1341fcaf9dc3)

---
+++ src/guid.c
@@ -0,0 +1,70 @@
+/* libguestfs
+ * Copyright (C) 2014 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "c-ctype.h"
+
+#include "guestfs.h"
+#include "guestfs-internal.h"
+
+/* Check whether a string supposed to contain a GUID actually contains it.
+ * It can recognize strings either as '{21EC2020-3AEA-1069-A2DD-08002B30309D}'
+ * or '21EC2020-3AEA-1069-A2DD-08002B30309D'.
+ */
+int
+guestfs___validate_guid (const char *str)
+{
+  size_t i, len = strlen (str);
+
+  switch (len) {
+  case 36:
+    break;
+  case 38:
+    if (str[0] == '{' && str[len -1] == '}') {
+      ++str;
+      len -= 2;
+      break;
+    }
+    return 0;
+  default:
+    return 0;
+  }
+
+  for (i = 0; i < len; ++i) {
+    switch (i) {
+    case 8:
+    case 13:
+    case 18:
+    case 23:
+      if (str[i] != '-')
+        return 0;
+      break;
+    default:
+      if (!c_isalnum (str[i]))
+        return 0;
+      break;
+    }
+  }
+
+  return 1;
+}
