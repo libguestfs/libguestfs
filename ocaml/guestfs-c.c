5d4e4e7ecaafee6ad8c445f069b3b4c606ba013c
Richard W.M. Jones <rjones@redhat.com>
2014-03-31 14:20:28 +0100

    ocaml: Add ESRCH to Guestfs.Errno module.

--- ocaml/guestfs-c.c
+++ ocaml/guestfs-c.c
@@ -64,6 +64,7 @@ value ocaml_guestfs_delete_event_callback (value gv, value eh);
 value ocaml_guestfs_event_to_string (value events);
 value ocaml_guestfs_last_errno (value gv);
 value ocaml_guestfs_get_ENOTSUP (value unitv);
+value ocaml_guestfs_get_ESRCH (value unitv);
 
 /* Allocate handles and deal with finalization. */
 static void
@@ -445,4 +446,11 @@ value
 ocaml_guestfs_get_ENOTSUP (value unitv)
 {
   return Val_int (ENOTSUP);
+}
+
+/* NB: "noalloc" function. */
+value
+ocaml_guestfs_get_ESRCH (value unitv)
+{
+  return Val_int (ESRCH);
 }
