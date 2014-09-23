06b04e5676eff7af7d0d667eab2f84288cb28755
Richard W.M. Jones <rjones@redhat.com>
2014-09-23 15:28:28 +0100

    v2v: -o rhev: Use a random name when testing UID:GID mapping.
    Using a fixed name is risky if there are multiple parallel virt-v2v
    processes running.

--- v2v/output_rhev.ml
+++ v2v/output_rhev.ml
@@ -173,7 +173,7 @@ object
 
     (* See if we can write files as UID:GID 36:36. *)
     let () =
-      let testfile = esd_mp // esd_uuid // "v2v-uid-test" in
+      let testfile = esd_mp // esd_uuid // string_random8 () in
       Kvmuid.make_file kvmuid_t testfile "";
       let stat = stat testfile in
       Kvmuid.unlink kvmuid_t testfile;
