e99af52aed0209b21cc743157b70b97a40e47c56
Richard W.M. Jones <rjones@redhat.com>
2014-10-20 20:41:29 +0100

    v2v: Inline and simplify Xen and vCenter input methods.
    Take the opportunity presented by the refactoring in the previous
    commit to inline and simplify functions in these input subclasses.
    
    This finally removes the map_source* functions.
    
    (cherry picked from commit b8f826b7ac1e7f90f670f474c3582b56063cdef6)

--- v2v/input_libvirt_xen_ssh.ml
+++ v2v/input_libvirt_xen_ssh.ml
@@ -28,57 +28,15 @@ open Input_libvirt_other
 
 open Printf
 
-(* Map a Xen <source/> to a qemu URI using the SSH driver in qemu.
- * This code assumes (and the caller checks) that the Xen URI is
- * remotely accessible over ssh, so we can map the path to the qemu
- * URI:
- *
- * json: {
- *   "file.driver": "ssh",
- *   "file.user": "username",
- *   "file.host": "xen-host",
- *   "file.port": 123,
- *   "file.path": "path",
- *   "file.host_key_check": "no"
- * }
- *)
-let map_path_to_uri verbose uri scheme server path format =
-  (* Construct the JSON parameters. *)
-  let json_params = [
-    "file.driver", JSON.String "ssh";
-    "file.path", JSON.String path;
-    "file.host", JSON.String server;
-    "file.host_key_check", JSON.String "no";
-  ] in
-
-  let json_params =
-    match uri.uri_port with
-    | 0 | 22 -> json_params
-    (* qemu will actually assert-fail if you send the port number as a
-     * string ...
-     *)
-    | i -> ("file.port", JSON.Int i) :: json_params in
-
-  let json_params =
-    match uri.uri_user with
-    | None -> json_params
-    | Some user -> ("file.user", JSON.String user) :: json_params in
-
-  if verbose then
-    printf "ssh: json parameters: %s\n" (JSON.string_of_doc json_params);
-
-  (* Turn the JSON parameters into a 'json:' protocol string. *)
-  let qemu_uri = "json: " ^ JSON.string_of_doc json_params in
-
-  qemu_uri, format
-
 (* Subclass specialized for handling Xen over SSH. *)
 class input_libvirt_xen_ssh verbose libvirt_uri parsed_uri scheme server guest =
 object
   inherit input_libvirt verbose libvirt_uri guest
 
   method source () =
-    if verbose then printf "input_libvirt_xen_ssh: source()\n%!";
+    if verbose then
+      printf "input_libvirt_xen_ssh: source: scheme %s server %s\n%!"
+        scheme server;
 
     error_if_libvirt_backend ();
     error_if_no_ssh_agent ();
@@ -90,11 +48,48 @@ object
     let { s_disks = disks } as source =
       Input_libvirtxml.parse_libvirt_xml ~verbose xml in
 
-    let mapf = map_path_to_uri verbose parsed_uri scheme server in
+    (* Map the <source/> filename (which is relative to the remote
+     * Xen server) to an ssh URI.  This is a JSON URI looking something
+     * like this:
+     *
+     * json: {
+     *   "file.driver": "ssh",
+     *   "file.user": "username",
+     *   "file.host": "xen-host",
+     *   "file.port": 1022,
+     *   "file.path": "path",
+     *   "file.host_key_check": "no"
+     *)
     let disks = List.map (
-      fun ({ s_qemu_uri = uri; s_format = format } as disk) ->
-        let uri, format = mapf uri format in
-        { disk with s_qemu_uri = uri; s_format = format }
+      fun ({ s_qemu_uri = path } as disk) ->
+        (* Construct the JSON parameters. *)
+        let json_params = [
+          "file.driver", JSON.String "ssh";
+          "file.path", JSON.String path;
+          "file.host", JSON.String server;
+          "file.host_key_check", JSON.String "no";
+        ] in
+
+        let json_params =
+          match parsed_uri.uri_port with
+          | 0 | 22 -> json_params
+          (* qemu will actually assert-fail if you send the port
+           * number as a string ...
+           *)
+          | i -> ("file.port", JSON.Int i) :: json_params in
+
+        let json_params =
+          match parsed_uri.uri_user with
+          | None -> json_params
+          | Some user -> ("file.user", JSON.String user) :: json_params in
+
+        if verbose then
+          printf "ssh: json parameters: %s\n" (JSON.string_of_doc json_params);
+
+        (* Turn the JSON parameters into a 'json:' protocol string. *)
+        let qemu_uri = "json: " ^ JSON.string_of_doc json_params in
+
+        { disk with s_qemu_uri = qemu_uri }
     ) disks in
 
     { source with s_disks = disks }
