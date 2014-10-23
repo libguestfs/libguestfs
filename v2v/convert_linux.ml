5af7ba1b2e7e833fc0cb1f0cbb6cbde0f4bb89fe
Richard W.M. Jones <rjones@redhat.com>
2014-10-23 08:41:56 +0100

    RHEL 7: v2v: Disable unconfiguration of VMware drivers on Linux (RHBZ#1155610).
    This is currently broken and breaks conversions.  For details see:
    https://bugzilla.redhat.com/show_bug.cgi?id=1155610

--- v2v/convert_linux.ml
+++ v2v/convert_linux.ml
@@ -540,6 +540,7 @@ let rec convert ~keep_serial_console (g : G.guestfs) inspect source =
               msg
     )
 
+(* Disabled in RHEL 7.1: see https://bugzilla.redhat.com/show_bug.cgi?id=1155610
   and unconfigure_vmware () =
     (* Look for any configured VMware yum repos and disable them. *)
     let repos =
@@ -630,6 +631,7 @@ let rec convert ~keep_serial_console (g : G.guestfs) inspect source =
           warning (f_"VMware tools was detected, but uninstallation failed.  The error message was: %s (ignored)")
             msg
     )
+*)
 
   and unconfigure_citrix () =
     let pkgs =
@@ -1385,7 +1387,7 @@ let rec convert ~keep_serial_console (g : G.guestfs) inspect source =
 
   unconfigure_xen ();
   unconfigure_vbox ();
-  unconfigure_vmware ();
+  (*unconfigure_vmware ();*)
   unconfigure_citrix ();
   unconfigure_kudzu ();
 
