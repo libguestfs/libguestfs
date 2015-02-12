bbc8c20a2daf7c1febf970ab8c4a66fff51727c5
Margaret Lewicka <margaret@meaningless.name>
2015-02-12 17:28:47 +0000

    builder: Check HAVE_POSIX_FADVISE before using it

--- builder/pxzcat-c.c
+++ builder/pxzcat-c.c
@@ -214,8 +214,10 @@ pxzcat (value filenamev, value outputfilev, unsigned nr_threads)
     unix_error (err, (char *) "ftruncate", outputfilev);
   }
 
+#if defined HAVE_POSIX_FADVISE
   /* Tell the kernel we won't read the output file. */
   ignore_value (posix_fadvise (fd, 0, 0, POSIX_FADV_RANDOM|POSIX_FADV_DONTNEED));
+#endif
 
   /* Iterate over blocks. */
   iter_blocks (idx, nr_threads, filenamev, fd, outputfilev, ofd);
