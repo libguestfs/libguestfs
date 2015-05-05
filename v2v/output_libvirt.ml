badfc2e9b2a56fd5bac849a6edca4fec823abeea
Richard W.M. Jones <rjones@redhat.com>
2015-05-05 16:11:46 +0100

    v2v: -o libvirt: Check if the domain exists on the target (RHBZ#889082).

--- v2v/output_libvirt.ml
+++ v2v/output_libvirt.ml
@@ -333,6 +333,16 @@ class output_libvirt verbose oc output_pool = object
      *)
     capabilities_doc <- Some doc;
 
+    (* Does the domain already exist on the target?  (RHBZ#889082) *)
+    if Domainxml.domain_exists ?conn:oc source.s_name then (
+      if source.s_hypervisor = Physical then (* virt-p2v user *)
+        error (f_"a libvirt domain called '%s' already exists on the target.\n\nIf using virt-p2v, select a different 'Name' in the 'Target properties'. Or delete the existing domain on the target using the 'virsh undefine' command.")
+              source.s_name
+      else                      (* !virt-p2v *)
+        error (f_"a libvirt domain called '%s' already exists on the target.\n\nIf using virt-v2v directly, use the '-on' option to select a different name. Or delete the existing domain on the target using the 'virsh undefine' command.")
+              source.s_name
+    );
+
     (* Connect to output libvirt instance and check that the pool exists
      * and dump out its XML.
      *)
