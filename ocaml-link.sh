13dd6323015c44a6b98a72661363d53118962ccd
Pino Toscano <ptoscano@redhat.com>
2015-05-13 15:30:39 +0200

    build: factor out the OCaml link.sh scripts
    Create a single ocaml-link.sh script, which supports a -cclib parameter
    so it can be used instead of the per-project link.sh scripts.
    As result, the libraries for each OCaml application can be moved back to
    each Makefile.am.

--- ocaml-link.sh
+++ ocaml-link.sh
@@ -1,6 +1,6 @@
-# libguestfs Makefile.am
-# @configure_input@
-# (C) Copyright 2014 Red Hat Inc.
+#!/bin/bash
+
+# (C) Copyright 2015 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,28 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-# Hack automake to link binary properly.  There is no other way to add
-# the -cclib parameter to the end of the command line.
+# Hack automake to link OCaml-based binaries properly.
+# There is no other way to add the -cclib parameter to the end of
+# the command line.
 
-exec "$@" -linkpkg -cclib '-lutils @LIBTINFO_LIBS@ @LIBCRYPT_LIBS@ @LIBVIRT_LIBS@ @LIBXML2_LIBS@ @LIBINTL@ -lgnu'
+# Usage:
+#   ./ocaml-link.sh -cclib '...' -- ARGS
+# Pass the cclib argument separately, and the rest as separated
+# arguments.
+
+TEMP=`getopt -a -o '' --long 'cclib:' \
+  -n "$(basename $0)" -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+
+cclib=
+
+while true ; do
+  case "$1" in
+    -cclib|--cclib) cclib="$2" ; shift 2 ;;
+    --) shift ; break ;;
+    *) echo "Internal error!" ; exit 1 ;;
+  esac
+done
+
+exec "$@" -linkpkg -cclib "${cclib}"
