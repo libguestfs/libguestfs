9f85477cbac1427b8d4d314c8b7de7f703821891
Richard W.M. Jones <rjones@redhat.com>
2015-06-23 18:21:50 +0100

    tests: Add tests using a captive daemon process.
    This allows us to test the daemon running as a host process, allowing
    us to meaningfully test it using valgrind.
    
    This commit only adds a single test that check that the daemon starts
    up, can be pinged, and exits.

---
+++ tests/daemon/test-daemon-start.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+# libguestfs
+# Copyright (C) 2015 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test that the daemon starts and stops.
+
+use strict;
+use warnings;
+
+require 'captive-daemon.pm';
+
+sub tests {
+    my $g = shift;
+
+    $g->ping_daemon;
+
+    # Return true to indicate the test succeeded.
+    1;
+}
+
+CaptiveDaemon::run_tests ()
