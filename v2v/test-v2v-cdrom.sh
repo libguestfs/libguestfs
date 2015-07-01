b7166e1a9e092b5c2e16ce42a6ee025283d6b520
Richard W.M. Jones <rjones@redhat.com>
2015-07-01 13:53:50 +0100

    v2v: Add a regression test for RHBZ#1238053.

---
+++ v2v/test-v2v-cdrom.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -
+# libguestfs virt-v2v test script
+# Copyright (C) 2015 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test cdrom dev assignment.
+# https://bugzilla.redhat.com/show_bug.cgi?id=1238053
+
+unset CDPATH
+export LANG=C
+set -e
+
+if [ -n "$SKIP_TEST_V2V_CDROM_SH" ]; then
+    echo "$0: test skipped because environment variable is set"
+    exit 77
+fi
+
+if [ "$(guestfish get-backend)" = "uml" ]; then
+    echo "$0: test skipped because UML backend does not support network"
+    exit 77
+fi
+
+abs_builddir="$(pwd)"
+libvirt_uri="test://$abs_builddir/test-v2v-cdrom.xml"
+
+f=../tests/guests/windows.img
+if ! test -f $f || ! test -s $f; then
+    echo "$0: test skipped because phony Windows image was not created"
+    exit 77
+fi
+
+f=../tests/guests/blank-disk.img
+if ! test -f $f || ! test -s $f; then
+    echo "$0: test skipped because blank-disk.img was not created"
+    exit 77
+fi
+
+virt_tools_data_dir=${VIRT_TOOLS_DATA_DIR:-/usr/share/virt-tools}
+if ! test -r $virt_tools_data_dir/rhsrvany.exe; then
+    echo "$0: test skipped because rhsrvany.exe is not installed"
+    exit 77
+fi
+
+d=test-v2v-cdrom.d
+rm -rf $d
+mkdir $d
+
+$VG virt-v2v --debug-gc \
+    -i libvirt -ic "$libvirt_uri" windows \
+    -o local -os $d --no-copy
+
+# Test the libvirt XML metadata was created.
+test -f $d/windows.xml
+
+# Grab just the <disk>..</disk> output and compare it to what we
+# expect.  https://stackoverflow.com/questions/16587218
+awk '/<disk /{p=1;print;next} p&&/<\/disk>/{p=0;print;next} ;p' \
+    $d/windows.xml |
+    grep -v '<source file' > $d/disks
+
+if ! diff -u test-v2v-cdrom.expected $d/disks; then
+    echo "$0: unexpected disk assignments"
+    cat $d/disks
+    exit 1
+fi
+
+rm -r $d
