libguestfs/virt-p2v

lib: Change 'program_name' macro to avoid conflict with gnulib.
The gnulib 'error' module uses 'program_name'.  On some platforms --
but not Linux / glibc -- it references it as:

  extern char *program_name;

This means when you compile libguestfs on non-glibc (eg. Mac OS X)
gnulib requires 'program_name' as an external string reference, which
we don't provide.

This change doesn't define this string reference for gnulib, but it
does change the name of the macro we use to avoid conflicts if we
eventually need to export 'program_name' as a string.

Thanks: Margaret Lewicka

conversion.c
@@ -159,7 +159,7 @@ start_conversion (struct config *config,
 #if DEBUG_STDERR
     fprintf (stderr,
              "%s: data connection for %s: SSH remote port %d, local port %d\n",
-             program_name, device,
+             guestfs___program_name, device,
              data_conns[i].nbd_remote_port, data_conns[i].nbd_local_port);
 #endif
 
@@ -202,7 +202,7 @@ start_conversion (struct config *config,
     goto out;
 
 #if DEBUG_STDERR
-  fprintf (stderr, "%s: libvirt XML:\n%s", program_name, libvirt_xml);
+  fprintf (stderr, "%s: libvirt XML:\n%s", guestfs___program_name, libvirt_xml);
 #endif
 
   /* Open the control connection and start conversion */

libguestfs/libguestfs

(cherry picked from commit 5cafedaa45d5993c54bf1827426171d9e23d77ae)

p2v/conversion.c
@@ -171,7 +171,7 @@ start_conversion (struct config *config,
 #if DEBUG_STDERR
     fprintf (stderr,
              "%s: data connection for %s: SSH remote port %d, local port %d\n",
-             program_name, device,
+             guestfs_int_program_name, device,
              data_conns[i].nbd_remote_port, data_conns[i].nbd_local_port);
 #endif
   }
@@ -203,7 +203,7 @@ start_conversion (struct config *config,
     goto out;
 
 #if DEBUG_STDERR
-  fprintf (stderr, "%s: libvirt XML:\n%s", program_name, libvirt_xml);
+  fprintf (stderr, "%s: libvirt XML:\n%s", guestfs___program_name, libvirt_xml);
 #endif
 
   /* Open the control connection and start conversion */