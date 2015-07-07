b33e943827b2decc2a01f198f1b300a3c3f6710e
Richard W.M. Jones <rjones@redhat.com>
2015-07-07 09:28:03 -0400

    RHEL: Reject use of libguestfs-winsupport features except for virt-* tools (RHBZ#1240276).
    Fix the tests: it doesn't let us use guestfish for arbitrary Windows
    edits.

--- generator/c.ml
+++ generator/c.ml
@@ -1846,6 +1846,22 @@ and generate_client_actions actions () =
     check_args_validity c_name style;
     trace_call name c_name style;
 
+    (* RHEL 8 *)
+    if name = "mount" || name = "mount_ro" || name = "mount_options" ||
+       name = "mount_vfs" then (
+      pr "  if (g->program && !STRPREFIX (g->program, \"virt-\")) {\n";
+      pr "    CLEANUP_FREE char *vfs_type = guestfs_vfs_type (g, mountable);\n";
+      pr "    if (vfs_type && STREQ (vfs_type, \"ntfs\")) {\n";
+      pr "      error (g, \"mount: unsupported filesystem type\");\n";
+      pr "      if (trace_flag)\n";
+      pr "        guestfs_int_trace (g, \"%%s = %%s (error)\",\n";
+      pr "                              \"%s\", \"-1\");\n" name;
+      pr "      return %s;\n" (string_of_errcode errcode);
+      pr "    }\n";
+      pr "  }\n";
+      pr "\n";
+    );
+
     (* Calculate the total size of all FileIn arguments to pass
      * as a progress bar hint.
      *)
