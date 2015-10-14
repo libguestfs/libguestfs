169bc884a5f115ee6e10140eaf2d45f3faf2b9d2
Roman Kagan <rkagan@virtuozzo.com>
2015-10-14 19:55:44 +0300

    v2v: drop useless forced gc
    Now that virtio driver copying is localized in a single function and the
    auxiliary guestfs handle doesn't leak outside of it (and thus an extra
    qemu instance doesn't survive beyond that function's runtime) there's no
    need in the forced GC which used to trigger closing of that handle and
    termination of that qemu instance.
    
    Signed-off-by: Roman Kagan <rkagan@virtuozzo.com>

--- v2v/v2v.ml
+++ v2v/v2v.ml
@@ -319,14 +319,6 @@ let rec main () =
   if verbose () then
     printf "%s%!" (string_of_target_buses target_buses);
 
-  (* Force a GC here, to ensure that we're using the minimum resources
-   * as we go into the copy stage.  The particular reason is that
-   * Windows conversion may have opened a second libguestfs handle
-   * pointing to the virtio-win ISO, which is only closed when the
-   * handle is GC'd.
-   *)
-  Gc.compact ();
-
   let delete_target_on_exit = ref true in
 
   let targets =
