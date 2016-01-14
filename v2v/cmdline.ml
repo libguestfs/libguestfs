a5c71228ad0a2015b0e556a3fbf131e001b0d59a
Richard W.M. Jones <rjones@redhat.com>
2016-01-14 11:53:42 -0500

    RHEL 8: v2v: Disable the virt-v2v --in-place option.
    This disables the virt-v2v --in-place option which we do not
    wish to support in RHEL.
    (See commit d0069559a939e47e5f29973ed9a69a13f0b58301).

--- v2v/cmdline.ml
+++ v2v/cmdline.ml
@@ -198,8 +198,7 @@ let parse_cmdline () =
                                     s_"Set option for input mode";
     [ M"it" ],       Getopt.String ("transport", set_string_option_once "-it" input_transport),
                                     s_"Input transport";
-    [ L"in-place" ], Getopt.Set in_place,
-                                    s_"Only tune the guest in the input VM";
+    [ L"in-place" ], Getopt.Set in_place, Getopt.hidden_option_description;
     [ L"machine-readable" ], Getopt.Set machine_readable,
                                     s_"Make output machine readable";
     [ S 'n'; L"network" ], Getopt.String ("in:out", add_network),
@@ -336,7 +335,6 @@ read the man page virt-v2v(1).
     printf "vddk\n";
     printf "colours-option\n";
     printf "vdsm-compat-option\n";
-    printf "in-place\n";
     printf "io/oo\n";
     List.iter (printf "input:%s\n") (Modules_list.input_modules ());
     List.iter (printf "output:%s\n") (Modules_list.output_modules ());
@@ -484,6 +482,10 @@ read the man page virt-v2v(1).
         | Some (`VDDK _) ->
            error (f_"only ‘-it ssh’ can be used here") in
       Input_vmx.input_vmx input_transport arg in
+
+  (* Prevent use of --in-place option in RHEL. *)
+  if in_place then
+    error (f_"--in-place cannot be used in RHEL");
 
   (* Common error message. *)
   let error_option_cannot_be_used_in_output_mode mode opt =
