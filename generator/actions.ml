00069ae81298656dcf698f005b2769f75d234b31
Richard W.M. Jones <rjones@redhat.com>
2016-02-03 16:53:17 +0000

    tests: Increase the size of the /dev/sda and /dev/sdb test devices.
    Previously these were rather small - just 500 MB.  This is too small
    to create a btrfs device on aarch64 (where page size may be 64K), and
    barely enough even on x86-64.  This change makes both these devices
    10 GB, and adjusts a few tests so they continue to pass.
    
    (cherry picked from commit 8ffad75e5b610274a664a00f1f1186070b602e18)

--- generator/actions.ml
+++ generator/actions.ml
@@ -4763,7 +4763,8 @@ C<blocksize> option of C<guestfs_mkfs>." };
     proc_nr = Some 62;
     tests = [
       InitEmpty, Always, TestResult (
-        [["blockdev_getsz"; "/dev/sda"]], "ret == 1024000"), []
+        [["blockdev_getsz"; "/dev/sda"]],
+          "ret == INT64_C(10)*1024*1024*1024/512"), []
     ];
     shortdesc = "get total size of device in 512-byte sectors";
     longdesc = "\
@@ -4782,7 +4783,8 @@ This uses the L<blockdev(8)> command." };
     proc_nr = Some 63;
     tests = [
       InitEmpty, Always, TestResult (
-        [["blockdev_getsize64"; "/dev/sda"]], "ret == UINT64_C (524288000)"), []
+        [["blockdev_getsize64"; "/dev/sda"]],
+          "ret == INT64_C(10)*1024*1024*1024"), []
     ];
     shortdesc = "get total size of device in bytes";
     longdesc = "\
@@ -7379,13 +7381,16 @@ and C<guestfs_setcon>" };
          ["write"; "/new"; "new file contents"];
          ["cat"; "/new"]], "new file contents"), [];
       InitEmpty, Always, TestRun (
-        [["part_disk"; "/dev/sda"; "mbr"];
+        [["part_init"; "/dev/sda"; "mbr"];
+         ["part_add"; "/dev/sda"; "p"; "64"; "204799"];
          ["mkfs_b"; "vfat"; "32768"; "/dev/sda1"]]), [];
       InitEmpty, Always, TestLastFail (
-        [["part_disk"; "/dev/sda"; "mbr"];
+        [["part_init"; "/dev/sda"; "mbr"];
+         ["part_add"; "/dev/sda"; "p"; "64"; "204799"];
          ["mkfs_b"; "vfat"; "32769"; "/dev/sda1"]]), [];
       InitEmpty, Always, TestLastFail (
-        [["part_disk"; "/dev/sda"; "mbr"];
+        [["part_init"; "/dev/sda"; "mbr"];
+         ["part_add"; "/dev/sda"; "p"; "64"; "204799"];
          ["mkfs_b"; "vfat"; "33280"; "/dev/sda1"]]), [];
       InitEmpty, IfAvailable "ntfsprogs", TestRun (
         [["part_disk"; "/dev/sda"; "mbr"];
@@ -9852,7 +9857,7 @@ device is stopped, but it is not destroyed or zeroed." };
           "check_hash (ret, \"PART_ENTRY_NUMBER\", \"1\") == 0 && "^
           "check_hash (ret, \"PART_ENTRY_TYPE\", \"0x83\") == 0 && "^
           "check_hash (ret, \"PART_ENTRY_OFFSET\", \"128\") == 0 && "^
-          "check_hash (ret, \"PART_ENTRY_SIZE\", \"1023745\") == 0"), [];
+          "check_hash (ret, \"PART_ENTRY_SIZE\", \"20971265\") == 0"), [];
     ];
     shortdesc = "print block device attributes";
     longdesc = "\
