246e7a4689975440735cf226076aac2c22a13095
Pino Toscano <ptoscano@redhat.com>
2016-02-03 13:24:37 +0100

    generator: simplify generated code for always-available features
    Just refer to the dummy function directly, instead of using #define's.
    
    (cherry picked from commit d3502bdc32629c63e6d8b9e313a0c968d7b1e5b7)

--- generator/daemon.ml
+++ generator/daemon.ml
@@ -732,19 +732,15 @@ and generate_daemon_optgroups_c () =
     pr "  return 1;\n";
     pr "}\n";
     pr "\n";
-
-    List.iter (
-      fun group ->
-        pr "#define optgroup_%s_available dummy_available\n" group;
-    ) optgroups_retired;
-
-    pr "\n";
   );
 
   pr "struct optgroup optgroups[] = {\n";
   List.iter (
     fun group ->
-      pr "  { \"%s\", optgroup_%s_available },\n" group group
+      if List.mem group optgroups_retired then
+        pr "  { \"%s\", dummy_available },\n" group
+      else
+        pr "  { \"%s\", optgroup_%s_available },\n" group group
   ) optgroups_names_all;
   pr "  { NULL, NULL }\n";
   pr "};\n"
