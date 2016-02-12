85b1815e7be07d27b21692b6677b66817b3ea88b
Richard W.M. Jones <rjones@redhat.com>
2016-02-12 14:10:07 +0000

    tests: Make '080' be an official test of the guestfs_version API.
    Useful because it tests returning a single structure.

---
+++ ocaml/t/guestfs_080_version.ml
@@ -0,0 +1,27 @@
+(* libguestfs OCaml tests
+ * Copyright (C) 2009-2016 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+let () =
+  let g = new Guestfs.guestfs () in
+  let version = g#version () in
+  assert (version.Guestfs.major = 1_L)
+  (* In other bindings, we may do additional tests on dynamic types
+   * here, but those are not necessary in OCaml.
+   *)
+
+let () = Gc.compact ()
