8d98ce37036480aad0628c560c30df403b4a63b4
Richard W.M. Jones <rjones@redhat.com>
2016-02-18 14:41:43 +0000

    v2v: RHEV: Treat Windows 8.1 client as Windows 8 (RHBZ#1309580).
    (cherry picked from commit 7bf820b0d9cc94c6291525c45b359177a11170d7)

--- v2v/OVF.ml
+++ v2v/OVF.ml
@@ -178,6 +178,13 @@ and get_ostype = function
     "windows_2012x64"
 
   | { i_type = "windows"; i_major_version = 6; i_minor_version = 3;
+      i_arch = "x86_64"; i_product_variant = "Client" } ->
+    (* Treat Windows 8.1 client like Windows 8.  See:
+     * https://bugzilla.redhat.com/show_bug.cgi?id=1309580#c4
+     *)
+    "windows_8x64"
+
+  | { i_type = "windows"; i_major_version = 6; i_minor_version = 3;
       i_arch = "x86_64" } ->
     "windows_2012R2x64"
 
