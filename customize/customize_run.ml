b1a79bde2d85d751e96fedff44329b01f54ae1fc
Richard W.M. Jones <rjones@redhat.com>
2016-05-04 13:57:53 +0100

    customize: Improve the error messages when package manager is unknown or unsupported.
    For Windows, we now print:
    
    $ virt-customize -a ./test-data/phony-guests/windows.img --install MSSQL
    [   0.0] Examining the guest ...
    [  14.2] Setting a random seed
    virt-customize: warning: random seed could not be set for this type of
    guest
    [  14.2] Installing packages: MSSQL
    virt-customize: error: cannot use '--install' because no package manager
    has been detected for this guest OS.
    
    If this guest OS is a common one with ordinary package management then this
    may have been caused by a failure of libguestfs inspection.
    
    For OSes such as Windows that lack package management, this is not
    possible.  Try using one of the '--firstboot*' flags instead (described in
    the manual).
    
    (cherry picked from commit 547662d9f6cea869d5edefd7e4612f070fa73143)

--- customize/customize_run.ml
+++ customize/customize_run.ml
@@ -94,7 +94,7 @@ exec >>%s 2>&1
   in
 
   (* http://distrowatch.com/dwres.php?resource=package-management *)
-  let guest_install_command packages =
+  let rec guest_install_command packages =
     let quoted_args = String.concat " " (List.map quote packages) in
     match g#inspect_get_package_management root with
     | "apt" ->
@@ -118,9 +118,9 @@ exec >>%s 2>&1
     | "zypper" ->
       sprintf "zypper -n in -l %s" quoted_args
     | "unknown" ->
-      error (f_"--install is not supported for this guest operating system")
+      error_unknown_package_manager (s_"--install")
     | pm ->
-      error (f_"sorry, don't know how to use --install with the '%s' package manager") pm
+      error_unimplemented_package_manager (s_"--install") pm
 
   and guest_update_command () =
     match g#inspect_get_package_management root with
@@ -145,9 +145,16 @@ exec >>%s 2>&1
     | "zypper" ->
       sprintf "zypper -n update -l"
     | "unknown" ->
-      error (f_"--update is not supported for this guest operating system")
+      error_unknown_package_manager (s_"--update")
     | pm ->
-      error (f_"sorry, don't know how to use --update with the '%s' package manager") pm
+      error_unimplemented_package_manager (s_"--update") pm
+
+  (* Windows has package_management == "unknown". *)
+  and error_unknown_package_manager flag =
+    error (f_"cannot use '%s' because no package manager has been detected for this guest OS.\n\nIf this guest OS is a common one with ordinary package management then this may have been caused by a failure of libguestfs inspection.\n\nFor OSes such as Windows that lack package management, this is not possible.  Try using one of the '--firstboot*' flags instead (described in the manual).") flag
+
+  and error_unimplemented_package_manager flag pm =
+      error (f_"sorry, '%s' with the '%s' package manager has not been implemented yet.\n\nYou can work around this by using one of the '--run*' or '--firstboot*' options instead (described in the manual).") flag pm
   in
 
   (* Set the random seed. *)
