cc38d62fdfff9dd72ecb379038e99727077f639d
Richard W.M. Jones <rjones@redhat.com>
2016-06-28 12:29:29 +0100

    p2v: Ensure messages are flushed through to the journal immediately (RHBZ#1229386).
    In kernel conversion mode, we must ensure messages are flushed to the
    journal as soon as they are received from virt-v2v, otherwise (now
    that debugging is not the default) we won't see any messages at all
    until the last moment.
    
    Thanks: Ming Xie

--- p2v/kernel.c
+++ p2v/kernel.c
@@ -257,7 +257,8 @@ notify_ui_callback (int type, const char *data)
 {
   switch (type) {
   case NOTIFY_LOG_DIR:
-    printf ("%s: remote log directory location: %s\n", guestfs_int_program_name, data);
+    printf ("%s: remote log directory location: %s\n",
+            guestfs_int_program_name, data);
     break;
 
   case NOTIFY_REMOTE_MESSAGE:
@@ -272,6 +273,8 @@ notify_ui_callback (int type, const char *data)
     printf ("%s: unknown message during conversion: type=%d data=%s\n",
             guestfs_int_program_name, type, data);
   }
+
+  fflush (stdout);
 }
 
 static void
