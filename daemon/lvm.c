7367945647e774aec4b93c3bf13181212d3c6430
Pino Toscano <ptoscano@redhat.com>
2016-07-26 17:23:34 +0200

    daemon: lvm: improve filter for LVs with activationskip flag set
    Commit 2e16e3e99324112845446c82b6a6e8b3e652e10d added lv_active=active
    as additional condition when listing LVs, to ignore those with the
    activationskip flag set.  OTOH, this check is too broad, and matches
    also other kind of LVs.
    
    Change the condition to lv_skip_activation!=1, so matching precisely
    what was meant, and only that.
    
    Related to: RHBZ#1306666

--- daemon/lvm.c
+++ daemon/lvm.c
@@ -264,7 +264,7 @@ do_lvs (void)
     r = command (&out, &err,
                  str_lvm, "lvs",
                  "-o", "vg_name,lv_name",
-                 "-S", "lv_role=public && lv_active=active",
+                 "-S", "lv_role=public && lv_skip_activation!=1",
                  "--noheadings",
                  "--separator", "/", NULL);
     if (r == -1) {
