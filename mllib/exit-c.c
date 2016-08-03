42c6c4c7130e4cc88458428edb0fb4654c52c2ab
Pino Toscano <ptoscano@redhat.com>
2016-08-03 15:01:59 +0200

    mllib: move _exit from v2v as Exit module
    Move the OCaml binding to C _exit to an own module.
    
    Just code motion, adapting v2v in the process.
    
    (cherry picked from commit 5077c020469827e104995cb2143d6d4eda981b9f)

---
+++ mllib/exit-c.c
@@ -0,0 +1,33 @@
+/* libguestfs OCaml tools common code
+ * Copyright (C) 2009-2016 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <caml/mlvalues.h>
+
+extern int guestfs_int_mllib_exit (value rv) __attribute__((noreturn));
+
+int
+guestfs_int_mllib_exit (value rv)
+{
+  _exit (Int_val (rv));
+}
