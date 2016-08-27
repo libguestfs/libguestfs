9e650f06cd70fdbf342b7c1ef4a03bf02ceba085
Richard W.M. Jones <rjones@redhat.com>
2016-08-27 07:32:49 +0100

    tests: Add a regression test for RHBZ#1370424.
    Regression test for previous commit which just tests that
    guestfs_add_domain doesn't assert-fail on guests with no port
    attribute.
    
    (cherry picked from commit 98aa78f288e44868bcccf56b777d57d9fd4ec6a6)

---
+++ tests/regressions/rhbz1370424.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -
+# libguestfs
+# Copyright (C) 2016 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Regression test for:
+# https://bugzilla.redhat.com/show_bug.cgi?id=1370424
+# Handle a network disk without a port attribute.
+
+set -e
+export LANG=C
+
+if [ -n "$SKIP_TEST_RHBZ1370424_SH" ]; then
+    echo "$0: test skipped because environment variable is set"
+    exit 77
+fi
+
+if [ "$(guestfish get-backend)" = "uml" ]; then
+    echo "$0: test skipped because UML backend does not support network"
+    exit 77
+fi
+
+guestfish <<EOF
+-add-domain rhbz1370424 \
+            libvirturi:test://$(pwd)/rhbz1370424.xml \
+            readonly:true
+# The test is just that the above command does not segfault.
+# We don't need to do anything else here.
+EOF
