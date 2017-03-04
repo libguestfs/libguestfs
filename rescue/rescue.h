3637c42f4e521eb647d7dfae7f48eb1689d0af54
Richard W.M. Jones <rjones@redhat.com>
2017-03-04 11:47:59 +0000

    rescue: Implement escape sequences.
    This implements a few useful escape sequences:
    
    ><rescue> ^]?
    virt-rescue escape sequences:
     ^] ? - print this message
     ^] h - print this message
     ^] q - quit virt-rescue
     ^] s - sync the filesystems
     ^] u - unmount filesystems
     ^] x - quit virt-rescue
     ^] z - suspend virt-rescue
    to pass the escape key through to the rescue shell, type it twice
    
    ^]i
    
    root device: /dev/sda3
      product name: Fedora 25 (Twenty Five)
      type: linux
      distro: fedora
    
    ^]z
    [3]+  Stopped                 ./run virt-rescue --scratch
    $ fg
    
    ><rescue> ^]u
    
    unmounting filesystems ...
    [   21.158558] XFS (sda3): Unmounting Filesystem

---
+++ rescue/rescue.h
@@ -0,0 +1,47 @@
+/* virt-rescue
+ * Copyright (C) 2010-2017 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef RESCUE_H
+#define RESCUE_H
+
+#include <stdbool.h>
+
+#include "guestfs.h"
+
+extern guestfs_h *g;
+extern int read_only;
+extern int live;
+extern int verbose;
+extern int keys_from_stdin;
+extern int echo_keys;
+extern const char *libvirt_uri;
+extern int inspector;
+extern int in_guestfish;
+extern int in_virt_rescue;
+extern int escape_key;
+
+/* escape.c */
+struct escape_state {
+  bool in_escape;
+};
+extern void init_escape_state (struct escape_state *state);
+extern bool process_escapes (struct escape_state *state, char *buf, size_t *len);
+extern int parse_escape_key (const char *);
+extern void print_escape_key_help (void);
+
+#endif /* RESCUE_H */
