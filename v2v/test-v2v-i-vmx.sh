e43fe85f28ba6fb184e6894db019668f0515c38d
Richard W.M. Jones <rjones@redhat.com>
2017-04-10 15:24:26 +0100

    v2v: Implement -i vmx to read VMware vmx files directly (RHBZ#1441197).
    This is a mostly complete implementation of a VMX parser and input
    class for virt-v2v.  It parses the name, memory size, CPU topology,
    firmware, video, sound, hard disks, removable disks and network
    interfaces from the VMX file.  It only omits support for floppies and
    SCSI CD-ROMs.
    
    The input class is split into two major parts: a generic VMX file
    parser (Parse_vmx), and the Input_vmx module which translates the VMX
    tree into the source device model.
    
    This also contains tests.  There are simple unit tests of the
    Parse_vmx module, and also some more complete parsing tests taken from
    real guests.
    
    (cherry picked from commit ca40078cdda9167d4658ddfe24c828c7ee76be37)

---
+++ v2v/test-v2v-i-vmx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -
+# libguestfs virt-v2v test script
+# Copyright (C) 2017 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test -i ova option.
+
+set -e
+
+$TEST_FUNCTIONS
+skip_if_skipped
+skip_if_backend uml
+
+export VIRT_TOOLS_DATA_DIR="$top_srcdir/test-data/fake-virt-tools"
+export VIRTIO_WIN="$top_srcdir/test-data/fake-virtio-win"
+
+rm -f test-v2v-i-vmx-*.actual
+
+for i in 1 2 3 4; do
+    $VG virt-v2v --debug-gc \
+        -i vmx test-v2v-i-vmx-$i.vmx \
+        --print-source > test-v2v-i-vmx-$i.actual
+
+    # Normalize the print-source output.
+    mv test-v2v-i-vmx-$i.actual test-v2v-i-vmx-$i.actual.old
+    sed \
+        -e "s,$(pwd),," \
+        < test-v2v-i-vmx-$i.actual.old > test-v2v-i-vmx-$i.actual
+    rm test-v2v-i-vmx-$i.actual.old
+
+    # Check the output.
+    diff -u test-v2v-i-vmx-$i.expected test-v2v-i-vmx-$i.actual
+done
+
+rm test-v2v-i-vmx-*.actual
