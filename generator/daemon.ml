e5d497f4936fc51fe161ef1d2fe277edc2260fb1
Richard W.M. Jones <rjones@redhat.com>
2017-04-20 15:07:23 +0100

    generator: daemon: Move *RESOLVE* macros to an ordinary header file.
    It's clumsy to define these in the generator, as they are essentially
    just static text.
    
    (cherry picked from commit ca12aaa90c7780a56bddd30c2199ca7b8d664979)

--- generator/daemon.ml
+++ generator/daemon.ml
@@ -80,109 +80,6 @@ let generate_daemon_stubs_h () =
 #ifndef GUESTFSD_STUBS_H
 #define GUESTFSD_STUBS_H
 
-/* Some macros to make resolving devices easier.  These used to
- * be available in daemon.h but now they are only used by stubs.
- */
-
-/* All functions that need an argument that is a device or partition name
- * must call this macro.  It checks that the device exists and does
- * device name translation (described in the guestfs(3) manpage).
- * Note that the \"path\" argument may be modified.
- */
-#define RESOLVE_DEVICE(path,path_out,is_filein)                         \\
-  do {									\\
-    if (STRNEQLEN ((path), \"/dev/\", 5)) {				\\
-      if (is_filein) cancel_receive ();                                 \\
-      reply_with_error (\"%%s: %%s: expecting a device name\", __func__, (path)); \\
-      return;							        \\
-    }									\\
-    if (is_root_device (path)) {                                        \\
-      if (is_filein) cancel_receive ();                                 \\
-      reply_with_error (\"%%s: %%s: device not found\", __func__, path);    \\
-      return;                                                           \\
-    }                                                                   \\
-    (path_out) = device_name_translation ((path));                      \\
-    if ((path_out) == NULL) {                                           \\
-      const int err = errno;                                            \\
-      if (is_filein) cancel_receive ();                                 \\
-      errno = err;                                                      \\
-      reply_with_perror (\"%%s: %%s\", __func__, path);                     \\
-      return;							        \\
-    }                                                                   \\
-  } while (0)
-
-/* All functions that take a mountable argument must call this macro.
- * It parses the mountable into a mountable_t, ensures any
- * underlying device exists, and does device name translation
- * (described in the guestfs(3) manpage).
- *
- * Note that the \"string\" argument may be modified.
- */
-#define RESOLVE_MOUNTABLE(string,mountable,is_filein)                   \\
-  do {                                                                  \\
-    if (STRPREFIX ((string), \"btrfsvol:\")) {                            \\
-      if (parse_btrfsvol ((string) + strlen (\"btrfsvol:\"), &(mountable)) == -1)\\
-      {                                                                 \\
-        if (is_filein) cancel_receive ();                               \\
-        reply_with_error (\"%%s: %%s: expecting a btrfs volume\",           \\
-                          __func__, (string));                          \\
-        return;                                                         \\
-      }                                                                 \\
-    }                                                                   \\
-    else {                                                              \\
-      (mountable).type = MOUNTABLE_DEVICE;                              \\
-      (mountable).device = NULL;                                        \\
-      (mountable).volume = NULL;                                        \\
-      RESOLVE_DEVICE ((string), (mountable).device, (is_filein));       \\
-    }                                                                   \\
-  } while (0)
-
-/* Helper for functions which need either an absolute path in the
- * mounted filesystem, OR a /dev/ device which exists.
- *
- * NB: Functions which mix filenames and device paths should be
- * avoided, and existing functions should be deprecated.  This is
- * because we intend in future to make device parameters a distinct
- * type from filenames.
- */
-#define REQUIRE_ROOT_OR_RESOLVE_DEVICE(path,path_out,is_filein)         \\
-  do {									\\
-    if (STREQLEN ((path), \"/dev/\", 5))                                  \\
-      RESOLVE_DEVICE ((path), (path_out), (is_filein));                 \\
-    else {								\\
-      NEED_ROOT ((is_filein), return);                                  \\
-      ABS_PATH ((path), (is_filein), return);                           \\
-      (path_out) = strdup ((path));                                     \\
-      if ((path_out) == NULL) {                                         \\
-        if (is_filein) cancel_receive ();                               \\
-        reply_with_perror (\"strdup\");                                   \\
-        return;                                                         \\
-      }                                                                 \\
-    }									\\
-  } while (0)
-
-/* Helper for functions which need either an absolute path in the
- * mounted filesystem, OR a valid mountable description.
- */
-#define REQUIRE_ROOT_OR_RESOLVE_MOUNTABLE(string, mountable, is_filein) \\
-  do {                                                                  \\
-    if (STRPREFIX ((string), \"/dev/\") || (string)[0] != '/') {          \\
-      RESOLVE_MOUNTABLE ((string), (mountable), (is_filein));           \\
-    }                                                                   \\
-    else {                                                              \\
-      NEED_ROOT ((is_filein), return);                                  \\
-      /* NB: It's a path, not a device. */                              \\
-      (mountable).type = MOUNTABLE_PATH;                                \\
-      (mountable).device = strdup ((string));                           \\
-      (mountable).volume = NULL;                                        \\
-      if ((mountable).device == NULL) {                                 \\
-        if (is_filein) cancel_receive ();                               \\
-        reply_with_perror (\"strdup\");                                   \\
-        return;                                                         \\
-      }                                                                 \\
-    }                                                                   \\
-  } while (0)                                                           \\
-
 ";
 
   List.iter (
@@ -215,6 +112,7 @@ let generate_daemon_stubs actions () =
 #include \"actions.h\"
 #include \"optgroups.h\"
 #include \"stubs.h\"
+#include \"stubs-macros.h\"
 
 ";
 
