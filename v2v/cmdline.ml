7e6c16f1e7698317991b875f2d7ab3ce9e94c8bb
Pino Toscano <ptoscano@redhat.com>
2017-05-22 11:31:45 +0200

    v2v: add crypto support (RHBZ#1451665)
    Make use of the additional command line arguments, and API needed to
    decrypt LUKS partitions.  This extends to v2v the work done in other
    OCaml tools with commit 6b26a0cce4f1d6264bee88902b8931e39288c901,
    since it seems to be working fine after a basic testing.
    
    Related to: RHBZ#1362649

--- v2v/cmdline.ml
+++ v2v/cmdline.ml
@@ -236,7 +236,7 @@ A short summary of the options is given below.  For detailed help please
 read the man page virt-v2v(1).
 ")
       prog in
-  let opthandle = create_standard_options argspec ~anon_fun usage_msg in
+  let opthandle = create_standard_options argspec ~anon_fun ~key_opts:true usage_msg in
   Getopt.parse opthandle;
 
   (* Dereference the arguments. *)
