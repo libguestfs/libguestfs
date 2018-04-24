libguestfs/virt-v2v

v2v: Abstract the target file so it could be a QEMU URI.
This complicated bit of refactoring abstracts the target file so it is
allowed to be either a filename or a QEMU URI.

v2v/output_qemu.ml
@@ -35,8 +35,9 @@ object
   method prepare_targets source targets =
     List.map (
       fun t ->
-        let target_file = dir // source.s_name ^ "-" ^ t.target_overlay.ov_sd in
-        { t with target_file = target_file }
+        let target_file =
+          TargetFile (dir // source.s_name ^ "-" ^ t.target_overlay.ov_sd) in
+        { t with target_file }
     ) targets
 
   method supported_firmware = [ TargetBIOS; TargetUEFI ]
@@ -122,7 +123,11 @@ object
     | BusSlotEmpty -> ()
 
     | BusSlotTarget t ->
-       arg_list "-drive" ["file=" ^ t.target_file; "format=" ^ t.target_format;
+       let target_file =
+         match t.target_file with
+         | TargetFile s -> s
+         | TargetURI _ -> assert false in
+       arg_list "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
                           "if=" ^ if_name; "index=" ^ string_of_int i;
                           "media=disk"]
 
@@ -141,7 +146,11 @@ object
     | BusSlotEmpty -> ()
 
     | BusSlotTarget t ->
-       arg_list "-drive" ["file=" ^ t.target_file; "format=" ^ t.target_format;
+       let target_file =
+         match t.target_file with
+         | TargetFile s -> s
+         | TargetURI _ -> assert false in
+       arg_list "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
                           "if=scsi"; "bus=0"; "unit=" ^ string_of_int i;
                           "media=disk"]
 

libguestfs/libguestfs

(cherry picked from commit e29296cfa20dd691995832940a30fe2e6b98149a)

v2v/output_qemu.ml
@@ -34,8 +34,9 @@ object
   method prepare_targets source targets =
     List.map (
       fun t ->
-        let target_file = dir // source.s_name ^ "-" ^ t.target_overlay.ov_sd in
-        { t with target_file = target_file }
+        let target_file =
+          TargetFile (dir // source.s_name ^ "-" ^ t.target_overlay.ov_sd) in
+        { t with target_file }
     ) targets
 
   method supported_firmware = [ TargetBIOS; TargetUEFI ]
@@ -99,7 +100,11 @@ object
     | BusSlotEmpty -> ()
 
     | BusSlotTarget t ->
-       commas "-drive" ["file=" ^ t.target_file; "format=" ^ t.target_format;
+       let target_file =
+         match t.target_file with
+         | TargetFile s -> s
+         | TargetURI _ -> assert false in
+       commas "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
                         "if=" ^ if_name; "index=" ^ string_of_int i;
                         "media=disk"]
 
@@ -118,7 +123,11 @@ object
     | BusSlotEmpty -> ()
 
     | BusSlotTarget t ->
-       commas "-drive" ["file=" ^ t.target_file; "format=" ^ t.target_format;
+       let target_file =
+         match t.target_file with
+         | TargetFile s -> s
+         | TargetURI _ -> assert false in
+       commas "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
                         "if=scsi"; "bus=0"; "unit=" ^ string_of_int i;
                         "media=disk"]
 