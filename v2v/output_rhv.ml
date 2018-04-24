libguestfs/virt-v2v

v2v: ovf: Create OVF more aligned with the standard
For historical reasons the OVF used in RHV export domain contains some
deviations from the OVF standard. The format used in -o rhv has to
remain fixed but for -o vdsm and we could produce much nicer OVF. This
patch serves as a preparatory step to this.

The main reason for creating different OVF is that it can be used to
create VM by oVirt REST API. The RHV export domain flavor cannot be used
that way.

For now the virt-v2v behavior is unchanged. The modified output will be
enabled in some later patch.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

v2v/output_rhv.ml
@@ -275,7 +275,8 @@ object
 
     (* Create the metadata. *)
     let ovf = Create_ovf.create_ovf source targets guestcaps inspect
-      output_alloc esd_uuid image_uuids vol_uuids vm_uuid in
+      output_alloc esd_uuid image_uuids vol_uuids vm_uuid
+      Create_ovf.RHVExportStorageDomain in
 
     (* Write it to the metadata file. *)
     let dir = esd_mp // esd_uuid // "master" // "vms" // vm_uuid in

v2v/output_vdsm.ml
@@ -175,7 +175,8 @@ object
       output_alloc dd_uuid
       vdsm_params.image_uuids
       vdsm_params.vol_uuids
-      vdsm_params.vm_uuid in
+      vdsm_params.vm_uuid
+      Create_ovf.RHVExportStorageDomain in
 
     (* Write it to the metadata file. *)
     let file = vdsm_params.ovf_output // vdsm_params.vm_uuid ^ ".ovf" in

libguestfs/libguestfs

Cherry picked from commit a52ed4b4454396eb13d2cdf5762292bff3104f66.
For RHEL 7.5-rhvpreview: Rebased the v2v/OVF.ml file so it closely
matches upstream, but fixed so it compiles against libguestfs 1.36
(eg. without functions such as List.push_back).

v2v/output_rhv.ml
@@ -269,7 +269,8 @@ object
 
     (* Create the metadata. *)
     let ovf = OVF.create_ovf source targets guestcaps inspect
-      output_alloc esd_uuid image_uuids vol_uuids vm_uuid in
+      output_alloc esd_uuid image_uuids vol_uuids vm_uuid
+      OVF.RHVExportStorageDomain in
 
     (* Write it to the metadata file. *)
     let dir = esd_mp // esd_uuid // "master" // "vms" // vm_uuid in

v2v/output_vdsm.ml
@@ -172,7 +172,8 @@ object
       output_alloc dd_uuid
       vdsm_params.image_uuids
       vdsm_params.vol_uuids
-      vdsm_params.vm_uuid in
+      vdsm_params.vm_uuid
+      OVF.RHVExportStorageDomain in
 
     (* Write it to the metadata file. *)
     let file = vdsm_params.ovf_output // vdsm_params.vm_uuid ^ ".ovf" in