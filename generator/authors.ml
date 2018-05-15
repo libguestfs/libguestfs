140a25b51143acbcefc6251e65e81066301d15a0
Richard W.M. Jones <rjones@redhat.com>
2018-05-15 14:03:19 +0100

    authors: Add Nikolay Ivanets (alternate form).

--- generator/authors.ml
+++ generator/authors.ml
@@ -95,7 +95,7 @@ let authors = [
 "Mike Kelly", [], [ Development ];
 "Mike Latimer", [], [ V2V_and_P2V ];
 "Ming Xie", [], [ Quality_assurance; V2V_and_P2V ];
-"Mykola Ivanets", [], [ Development ];
+"Mykola Ivanets", [], [ Development ]; (* alias: "Nikolay Ivanets" *)
 "Nicholas Strugnell", [], [ Development ];
 "Nikita A Menkovich", [], [ Development ];
 "Nikita Menkovich", [], [ Development ];
