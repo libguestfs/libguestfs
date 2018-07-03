8fdc1d4852bfe5067b35cb8dca387d56b3836645
Richard W.M. Jones <rjones@redhat.com>
2018-07-03 19:34:20 +0100

    v2v: types: Fix comment for #create_metadata method.

--- v2v/types.mli
+++ v2v/types.mli
@@ -390,7 +390,7 @@ class virtual output : object
   (** Called before conversion.  Can be used to check there is enough space
       on the target, using the [target.target_estimated_size] field. *)
   method virtual create_metadata : source -> target list -> target_buses -> guestcaps -> inspect -> target_firmware -> unit
-  (** Called after conversion to finish off and create metadata. *)
+  (** Called after conversion and copying to finish off and create metadata. *)
   method disk_create : ?backingfile:string -> ?backingformat:string -> ?preallocation:string -> ?compat:string -> ?clustersize:int -> string -> string -> int64 -> unit
   (** Called in order to create disks on the target.  The method has the
       same signature as Guestfs#disk_create. *)
