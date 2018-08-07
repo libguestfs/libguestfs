11ee3aaa4f73e9de23cd989ece373057f6e5f00c
Tomáš Golembiovský <tgolembi@redhat.com>
2018-08-07 14:03:07 +0200

    v2v: Add --mac option to machine-readable
    Patch fe1a8866 added --mac option that allows mapping bridges/networks
    based on the MAC address. It would be nice to have that listed in
    machine-readable output
    
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>
    (cherry picked from commit 71b588bab0f52d9701ba97aa7bc76217796fe556)

--- v2v/cmdline.ml
+++ v2v/cmdline.ml
@@ -341,6 +341,7 @@ read the man page virt-v2v(1).
     printf "colours-option\n";
     printf "vdsm-compat-option\n";
     printf "io/oo\n";
+    printf "mac-option\n";
     List.iter (printf "input:%s\n") (Modules_list.input_modules ());
     List.iter (printf "output:%s\n") (Modules_list.output_modules ());
     List.iter (printf "convert:%s\n") (Modules_list.convert_modules ());
