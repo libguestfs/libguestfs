libguestfs/virt-v2v

v2v: vdsm: add --vdsm-fixed-ovf option
Add option for -o vdsm that enables output of the modified OVF. oVirt
engine should already be able to consume the OVF, but let's not take any
chances and enable it only by command line argument. It can be made
default later when it receives proper testing.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

v2v/output_vdsm.ml
@@ -32,6 +32,7 @@ type vdsm_params = {
   vm_uuid : string;
   ovf_output : string;
   compat : string;
+  ovf_flavour : Create_ovf.ovf_flavour;
 }
 
 class output_vdsm os vdsm_params output_alloc =
@@ -39,7 +40,7 @@ object
   inherit output
 
   method as_options =
-    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s" os
+    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s%s" os
       (String.concat ""
          (List.map (sprintf " --vdsm-image-uuid %s") vdsm_params.image_uuids))
       (String.concat ""
@@ -49,6 +50,10 @@ object
       (match vdsm_params.compat with
        | "0.10" -> "" (* currently this is the default, so don't print it *)
        | s -> sprintf " --vdsm-compat=%s" s)
+      (match vdsm_params.ovf_flavour with
+       | Create_ovf.OVirt -> "--vdsm-ovf-flavour=ovf"
+       (* currently this is the default, so don't print it *)
+       | Create_ovf.RHVExportStorageDomain -> "")
 
   method supported_firmware = [ TargetBIOS ]
 
@@ -176,7 +181,7 @@ object
       vdsm_params.image_uuids
       vdsm_params.vol_uuids
       vdsm_params.vm_uuid
-      Create_ovf.RHVExportStorageDomain in
+      vdsm_params.ovf_flavour in
 
     (* Write it to the metadata file. *)
     let file = vdsm_params.ovf_output // vdsm_params.vm_uuid ^ ".ovf" in

libguestfs/libguestfs

(cherry picked from commit 285014b290507865fd2020e44ea453af0262b624)

v2v/output_vdsm.ml
@@ -32,6 +32,7 @@ type vdsm_options = {
   vm_uuid : string;
   ovf_output : string;
   compat : string;
+  ovf_flavour : Create_ovf.ovf_flavour;
 }
 
 class output_vdsm os vdsm_options output_alloc =
@@ -39,7 +40,7 @@ object
   inherit output
 
   method as_options =
-    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s" os
+    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s%s" os
       (String.concat ""
          (List.map (sprintf " --vdsm-image-uuid %s") vdsm_options.image_uuids))
       (String.concat ""
@@ -49,6 +50,10 @@ object
       (match vdsm_options.compat with
        | "0.10" -> "" (* currently this is the default, so don't print it *)
        | s -> sprintf " --vdsm-compat=%s" s)
+      (match vdsm_options.ovf_flavour with
+       | Create_ovf.OVirt -> "--vdsm-ovf-flavour=ovf"
+       (* currently this is the default, so don't print it *)
+       | Create_ovf.RHVExportStorageDomain -> "")
 
   method supported_firmware = [ TargetBIOS ]
 
@@ -176,7 +181,7 @@ object
       vdsm_options.image_uuids
       vdsm_options.vol_uuids
       vdsm_options.vm_uuid
-      Create_ovf.RHVExportStorageDomain in
+      vdsm_options.ovf_flavour in
 
     (* Write it to the metadata file. *)
     let file = vdsm_options.ovf_output // vdsm_options.vm_uuid ^ ".ovf" in