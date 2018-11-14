325fffd361c26fc8934f3f5480acb9bd6b0d63af
Richard W.M. Jones <rjones@redhat.com>
2018-11-14 17:07:49 +0000

    v2v: Reduce the amount of appliance memory back to around 2GB.
    Commit ba82fb023de8ff0d271b4b82e3ef12a8d169728e changed the default
    libguestfs appliance memory from 500 -> 768MB.
    
    Virt-v2v used a formula to calculate how much memory to use which is
    based on this which was designed to result in around 2GB of memory:
    
      500 * 20 / 5 = 2000
    
    However after the commit the same formula now produces:
    
      768 * 20 / 5 = 3072
    
    This commit reduces the numerator in the formula to yield (about) 2GB
    again:
    
      768 * 14 / 5 = 2150

--- v2v/v2v.ml
+++ v2v/v2v.ml
@@ -93,7 +93,7 @@ let rec main () =
   );
 
   let g = open_guestfs ~identifier:"v2v" () in
-  g#set_memsize (g#get_memsize () * 20 / 5);
+  g#set_memsize (g#get_memsize () * 14 / 5);
   (* The network is only used by the unconfigure_vmware () function. *)
   g#set_network true;
   (match conversion_mode with
