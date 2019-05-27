c2918b8b74506523a723b804d452816a059c5e50
Martin Kletzander <mkletzan@redhat.com>
2019-05-27 13:30:05 +0200

    Use proper label for nbdkit sockets
    While svirt_t can be used for sockets it does not always guarantee that it will
    be accessible from a virtual machine.  The VM might be running under svirt_tcg_t
    context which will need a svirt_tcg_t label on the socket in order to access it.
    
    There is, however, another label, svirt_socket_t, which is accessible from
    virt_domain:
    
      # sesearch -A -s svirt_t -c unix_stream_socket -p connectto
      ...
      allow virt_domain svirt_socket_t:unix_stream_socket { ... connectto ... };
      ...
    
    And virt_domain is a type attribute of both svirt_t and svirt_tcg_t:
    
      # seinfo -x -a virt_domain
      Type Attributes: 1
         attribute virt_domain;
              svirt_t
              svirt_tcg_t
    
    Resolves: https://bugzilla.redhat.com/1698437
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- v2v/output_rhv_upload.ml
+++ v2v/output_rhv_upload.ml
@@ -213,7 +213,7 @@ See also the virt-v2v-output-rhv(1) manual.")
     let args =
       (* label the socket so qemu can open it *)
       if have_selinux then
-        args @ ["--selinux-label"; "system_u:object_r:svirt_t:s0"]
+        args @ ["--selinux-label"; "system_u:object_r:svirt_socket_t:s0"]
       else args in
     args in
 
