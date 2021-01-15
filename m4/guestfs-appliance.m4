libguestfs/libguestfs

commit 94ff24b8800ebc79f19bd686ec2dda393906c454

appliance: Set default guestfs appliance path to $libdir/guestfs.
For unclear reasons this broke recently, although the breakage is only
reproducible in Fedora Koji.  It appears to be caused by the
calculation of the internal autoconf variable $acl_libdirstem
changing.  In the reproducer system:

checking for the common suffixes of directories in the library search path... lib,lib,lib64

On my local system:

checking for the common suffixes of directories in the library search path... lib64,lib64

My local system would be the correct one.  The actual code that
calculates this does some crazy stuff with ‘gcc -print-search-dirs’
which would hint that this output has changed in some way that subtly
breaks the generated configure script.  However even with match GCC
versions on my local system I could still not reproduce the issue.

None of this matters, as the easiest fix here is simply to stop using
internal autoconf variables at all.  I also added some AC_MSG_*
statements so we can more easily see what's going on in the configure
output.

Fixes commit 0f79400c7f3d69e1a5d67f7f56c9bd7e3584aae9.

m4/guestfs-appliance.m4
@@ -126,11 +126,13 @@ AC_ARG_WITH([extra-packages],
 AC_SUBST([EXTRA_PACKAGES])
 
 dnl Set guestfs default path
+AC_MSG_CHECKING([for guestfs path])
 AC_ARG_WITH([guestfs-path],
     [AS_HELP_STRING([--with-guestfs-path=PATH],
                    [specify guestfs path (default=$libdir/guestfs)])],
     [GUESTFS_DEFAULT_PATH="$withval"],
-    [GUESTFS_DEFAULT_PATH="${acl_final_exec_prefix}/${acl_libdirstem}/guestfs"])
+    [GUESTFS_DEFAULT_PATH="$libdir/guestfs"])
+AC_MSG_RESULT([$GUESTFS_DEFAULT_PATH])
 AC_SUBST([GUESTFS_DEFAULT_PATH])
 
 AC_DEFINE_UNQUOTED([GUESTFS_DEFAULT_PATH], ["$GUESTFS_DEFAULT_PATH"],

commit fb7b683a2988a7880b9d991f4e325aaa15fd2d42

appliance: Avoid having ${exec_prefix} in guestfs appliance path.
Normally packagers should try to use ./configure --with-guestfs-path=...
However if they do not, then a regression in 1.44 caused the path to
constain the literal string "${exec_prefix}".  This was because of
autoconf stupidity.

Try to resolve this by expanding ${exec_prefix} to something sensible.

The expansion will often be wrong (but at least it will now be a valid
path), hence the recommendation to always set --libdir and/or
--with-guestfs-path when configuring.

Fixes: commit 94ff24b8800ebc79f19bd686ec2dda393906c454
Fixes: https://bugzilla.redhat.com/1915646

m4/guestfs-appliance.m4
@@ -135,12 +135,17 @@ AC_ARG_WITH([extra-packages],
 AC_SUBST([EXTRA_PACKAGES])
 
 dnl Set guestfs default path
+dnl Expand $libdir to the actual path to avoid autoconf stupidity.
+real_libdir="$(
+        if test "$exec_prefix" = NONE ; then exec_prefix=/usr/local; fi
+        echo "$libdir" | sed "s|\${exec_prefix}|$exec_prefix|"
+        )"
 AC_MSG_CHECKING([for guestfs path])
 AC_ARG_WITH([guestfs-path],
     [AS_HELP_STRING([--with-guestfs-path=PATH],
-                   [specify guestfs path (default=$libdir/guestfs)])],
+                   [specify guestfs path (default=$real_libdir/guestfs)])],
     [GUESTFS_DEFAULT_PATH="$withval"],
-    [GUESTFS_DEFAULT_PATH="$libdir/guestfs"])
+    [GUESTFS_DEFAULT_PATH="$real_libdir/guestfs"])
 AC_MSG_RESULT([$GUESTFS_DEFAULT_PATH])
 AC_SUBST([GUESTFS_DEFAULT_PATH])
 