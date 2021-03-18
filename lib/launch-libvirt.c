libguestfs/libguestfs

commit 30f74f38bd6e42e783ba80895f4d6826abddd417

appliance: Use -cpu max.
QEMU has a newish feature (from about 2017 / qemu 2.9) called -cpu max
which is supposed to select the best CPU, ideal for libguestfs.

After this change, on x86-64:

               KVM                          TCG

Direct         -cpu max                     -cpu max
(non-libvirt)

Libvirt   <cpu mode="host-passthrough">     <cpu mode="host-model">
            <model fallback="allow"/>         <model fallback="allow"/>
          </cpu>                            </cpu>

Thanks: Daniel Berrangé

lib/launch-libvirt.c
@@ -1169,6 +1169,15 @@ construct_libvirt_xml_cpu (guestfs_h *g,
           attribute ("fallback", "allow");
         } end_element ();
       }
+      else if (STREQ (cpu_model, "max")) {
+        if (params->data->is_kvm)
+          attribute ("mode", "host-passthrough");
+        else
+          attribute ("mode", "host-model");
+        start_element ("model") {
+          attribute ("fallback", "allow");
+        } end_element ();
+      }
       else
         single_element ("model", cpu_model);
     } end_element ();

lib/appliance-cpu.c
@@ -38,6 +38,11 @@
  *
  * The literal string C<"host"> means use C<-cpu host>.
  *
+ * =item C<"max">
+ *
+ * The literal string C<"max"> means use C<-cpu max> (the best
+ * possible).  This requires awkward translation for libvirt.
+ *
  * =item some string
  *
  * Some string such as C<"cortex-a57"> means use C<-cpu cortex-a57>.
@@ -80,14 +85,9 @@ guestfs_int_get_cpu_model (int kvm)
   /* See discussion in https://bugzilla.redhat.com/show_bug.cgi?id=1605071 */
   return NULL;
 #else
-  /* On most architectures, it is faster to pass the CPU host model to
-   * the appliance, allowing maximum speed for things like checksums
-   * and encryption.  Only do this with KVM.  It is broken in subtle
-   * ways on TCG, and fairly pointless when you're emulating anyway.
+  /* On most architectures we can use "max" to get the best possible CPU.
+   * For recent qemu this should work even on TCG.
    */
-  if (kvm)
-    return "host";
-  else
-    return NULL;
+  return "max";
 #endif
 }

commit 13ceb6a87b2869909a6a0e3c8caa962b72e4cb0e

appliance: Use <cpu mode="maximum"/> for -cpu max on libvirt.
Note this requires libvirt >= 7.1.0 which was only released in March 2021.

With an older libvirt you will see this error:

  Original error from libvirt: unsupported configuration: Invalid mode attribute 'maximum' [code=67 int1=-1]

In theory we could check if this is supported by looking at the
libvirt capabilities and fall back, but this commit does not do that,
in the expectation that most people will be using the default backend
(direct) and on Fedora/RHEL we will add an explicit minimum version
dependency to the package.

qemu support has been around quite a bit longer (at least since 2017).

Fixes: commit 30f74f38bd6e42e783ba80895f4d6826abddd417

lib/launch-libvirt.c
@@ -1183,13 +1183,8 @@ construct_libvirt_xml_cpu (guestfs_h *g,
         } end_element ();
       }
       else if (STREQ (cpu_model, "max")) {
-        if (params->data->is_kvm)
-          attribute ("mode", "host-passthrough");
-        else
-          attribute ("mode", "host-model");
-        start_element ("model") {
-          attribute ("fallback", "allow");
-        } end_element ();
+        /* https://bugzilla.redhat.com/show_bug.cgi?id=1935572#c11 */
+        attribute ("mode", "maximum");
       }
       else
         single_element ("model", cpu_model);