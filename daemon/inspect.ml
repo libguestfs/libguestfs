libguestfs/libguestfs

commit 394d11be49121884295e61964ed47f5a8488c252

daemon: Implement inspection of Linux and other Unix-like operating systems.
This is essentially a line-for-line translation of the C inspection
code.

daemon/inspect.ml
@@ -1,0 +1,396 @@
+(* guestfs-inspection
+ * Copyright (C) 2009-2017 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Printf
+
+open Std_utils
+
+open Utils
+open Mountable
+open Inspect_types
+
+let re_primary_partition = PCRE.compile "^/dev/(?:h|s|v)d.[1234]$"
+
+let rec inspect_os () =
+  Mount.umount_all ();
+
+  (* Iterate over all detected filesystems.  Inspect each one in turn. *)
+  let fses = Listfs.list_filesystems () in
+
+  let fses =
+    filter_map (
+      fun (mountable, vfs_type) ->
+        Inspect_fs.check_for_filesystem_on mountable vfs_type
+  ) fses in
+  if verbose () then (
+    eprintf "inspect_os: fses:\n";
+    List.iter (fun fs -> eprintf "%s" (string_of_fs fs)) fses;
+    flush stderr
+  );
+
+  (* The OS inspection information for CoreOS are gathered by inspecting
+   * multiple filesystems. Gather all the inspected information in the
+   * inspect_fs struct of the root filesystem.
+   *)
+  let fses = collect_coreos_inspection_info fses in
+
+  (* Check if the same filesystem was listed twice as root in fses.
+   * This may happen for the *BSD root partition where an MBR partition
+   * is a shadow of the real root partition probably /dev/sda5
+   *)
+  let fses = check_for_duplicated_bsd_root fses in
+
+  (* For Linux guests with a separate /usr filesystem, merge some of the
+   * inspected information in that partition to the inspect_fs struct
+   * of the root filesystem.
+   *)
+  let fses = collect_linux_inspection_info fses in
+
+  (* Save what we found in a global variable. *)
+  Inspect_types.inspect_fses := fses;
+
+  (* At this point we have, in the handle, a list of all filesystems
+   * found and data about each one.  Now we assemble the list of
+   * filesystems which are root devices.
+   *
+   * Fall through to inspect_get_roots to do that.
+   *)
+  inspect_get_roots ()
+
+(* Traverse through the filesystem list and find out if it contains
+ * the [/] and [/usr] filesystems of a CoreOS image. If this is the
+ * case, sum up all the collected information on the root fs.
+ *)
+and collect_coreos_inspection_info fses =
+  (* Split the list into CoreOS root(s), CoreOS usr(s), and
+   * everything else.
+   *)
+  let rec loop roots usrs others = function
+    | [] -> roots, usrs, others
+    | ({ role = RoleRoot { distro = Some DISTRO_COREOS } } as r) :: rest ->
+       loop (r::roots) usrs others rest
+    | ({ role = RoleUsr { distro = Some DISTRO_COREOS } } as u) :: rest ->
+       loop roots (u::usrs) others rest
+    | o :: rest ->
+       loop roots usrs (o::others) rest
+  in
+  let roots, usrs, others = loop [] [] [] fses in
+
+  match roots with
+  (* If there are no CoreOS roots, then there's nothing to do. *)
+  | [] -> fses
+  (* If there are more than one CoreOS roots, we cannot inspect the guest. *)
+  | _::_::_ -> failwith "multiple CoreOS root filesystems found"
+  | [root] ->
+     match usrs with
+     (* If there are no CoreOS usr partitions, nothing to do. *)
+     | [] -> fses
+     | usrs ->
+        (* CoreOS is designed to contain 2 /usr partitions (USR-A, USR-B):
+         * https://coreos.com/docs/sdk-distributors/sdk/disk-partitions/
+         * One is active and one passive. During the initial boot, the
+         * passive partition is empty and it gets filled up when an
+         * update is performed.  Then, when the system reboots, the
+         * boot loader is instructed to boot from the passive partition.
+         * If both partitions are valid, we cannot determine which the
+         * active and which the passive is, unless we peep into the
+         * boot loader. As a workaround, we check the OS versions and
+         * pick the one with the higher version as active.
+         *)
+        let compare_versions u1 u2 =
+          let v1 =
+            match u1 with
+            | { role = RoleUsr { version = Some v } } -> v
+            | _ -> (0, 0) in
+          let v2 =
+            match u2 with
+            | { role = RoleUsr { version = Some v } } -> v
+            | _ -> (0, 0) in
+          compare v2 v1 (* reverse order *)
+        in
+        let usrs = List.sort compare_versions usrs in
+        let usr = List.hd usrs in
+
+        merge usr root;
+        root :: others
+
+(* On *BSD systems, sometimes [/dev/sda[1234]] is a shadow of the
+ * real root filesystem that is probably [/dev/sda5] (see:
+ * [http://www.freebsd.org/doc/handbook/disk-organization.html])
+ *)
+and check_for_duplicated_bsd_root fses =
+  try
+    let is_primary_partition = function
+      | { m_type = (MountablePath | MountableBtrfsVol _) } -> false
+      | { m_type = MountableDevice; m_device = d } ->
+         PCRE.matches re_primary_partition d
+    in
+
+    (* Try to find a "BSD primary", if there is one. *)
+    let bsd_primary =
+      List.find (
+        function
+        | { fs_location = { mountable = mountable };
+            role = RoleRoot { os_type = Some t } } ->
+           (t = OS_TYPE_FREEBSD || t = OS_TYPE_NETBSD || t = OS_TYPE_OPENBSD)
+           && is_primary_partition mountable
+        | _ -> false
+      ) fses in
+
+    let bsd_primary_os_type =
+      match bsd_primary with
+      | { role = RoleRoot { os_type = Some t } } -> t
+      | _ -> assert false in
+
+    (* Try to find a shadow of the primary, and if it is found the
+     * primary is removed.
+     *)
+    let fses_without_bsd_primary = List.filter ((!=) bsd_primary) fses in
+    let shadow_exists =
+      List.exists (
+        function
+        | { role = RoleRoot { os_type = Some t } } ->
+           t = bsd_primary_os_type
+        | _ -> false
+      ) fses_without_bsd_primary in
+    if shadow_exists then fses_without_bsd_primary else fses
+  with
+    Not_found -> fses
+
+(* Traverse through the filesystem list and find out if it contains
+ * the [/] and [/usr] filesystems of a Linux image (but not CoreOS,
+ * for which there is a separate [collect_coreos_inspection_info]).
+ *
+ * If this is the case, sum up all the collected information on each
+ * root fs from the respective [/usr] filesystems.
+ *)
+and collect_linux_inspection_info fses =
+  List.map (
+    function
+    | { role = RoleRoot { distro = Some d } } as root ->
+       if d <> DISTRO_COREOS then
+         collect_linux_inspection_info_for fses root
+       else
+         root
+    | fs -> fs
+  ) fses
+
+(* Traverse through the filesystems and find the /usr filesystem for
+ * the specified C<root>: if found, merge its basic inspection details
+ * to the root when they were set (i.e. because the /usr had os-release
+ * or other ways to identify the OS).
+ *)
+and collect_linux_inspection_info_for fses root =
+  let root_distro, root_fstab =
+    match root with
+    | { role = RoleRoot { distro = Some d; fstab = f } } -> d, f
+    | _ -> assert false in
+
+  try
+    let usr =
+      List.find (
+        function
+        | { role = RoleUsr { distro = d } }
+             when d = Some root_distro || d = None -> true
+        | _ -> false
+      ) fses in
+
+    let usr_mountable = usr.fs_location.mountable in
+
+    (* This checks that [usr] is found in the fstab of the root
+     * filesystem.  If not, [Not_found] is thrown.
+     *)
+    ignore (
+      List.find (fun (mountable, _) -> usr_mountable = mountable) root_fstab
+    );
+
+    merge usr root;
+    root
+  with
+    Not_found -> root
+
+and inspect_get_roots () =
+  let fses = !Inspect_types.inspect_fses in
+
+  let roots =
+    filter_map (
+      fun fs -> try Some (root_of_fs fs) with Invalid_argument _ -> None
+    ) fses in
+  if verbose () then (
+    eprintf "inspect_get_roots: roots:\n";
+    List.iter (fun root -> eprintf "%s" (string_of_root root)) roots;
+    flush stderr
+  );
+
+  (* Only return the list of mountables, since subsequent calls will
+   * be used to retrieve the other information.
+   *)
+  List.map (fun { root_location = { mountable = m } } -> m) roots
+
+and root_of_fs =
+  function
+  | { fs_location = location; role = RoleRoot data } ->
+     { root_location = location; inspection_data = data }
+  | { role = (RoleUsr _ | RoleSwap | RoleOther) } ->
+     invalid_arg "root_of_fs"
+
+and inspect_get_mountpoints root_mountable =
+  let root = search_for_root root_mountable in
+  let fstab = root.inspection_data.fstab in
+
+  (* If no fstab information (Windows) return just the root. *)
+  if fstab = [] then
+    [ "/", root_mountable ]
+  else (
+    filter_map (
+      fun (mountable, mp) ->
+        if String.length mp > 0 && mp.[0] = '/' then
+          Some (mp, mountable)
+        else
+          None
+    ) fstab
+  )
+
+and inspect_get_filesystems root_mountable =
+  let root = search_for_root root_mountable in
+  let fstab = root.inspection_data.fstab in
+
+  (* If no fstab information (Windows) return just the root. *)
+  if fstab = [] then
+    [ root_mountable ]
+  else
+    List.map fst fstab
+
+and inspect_get_format root = "installed"
+
+and inspect_get_type root =
+  let root = search_for_root root in
+  match root.inspection_data.os_type with
+  | Some v -> string_of_os_type v
+  | None -> "unknown"
+
+and inspect_get_distro root =
+  let root = search_for_root root in
+  match root.inspection_data.distro with
+  | Some v -> string_of_distro v
+  | None -> "unknown"
+
+and inspect_get_package_format root =
+  let root = search_for_root root in
+  match root.inspection_data.package_format with
+  | Some v -> string_of_package_format v
+  | None -> "unknown"
+
+and inspect_get_package_management root =
+  let root = search_for_root root in
+  match root.inspection_data.package_management with
+  | Some v -> string_of_package_management v
+  | None -> "unknown"
+
+and inspect_get_product_name root =
+  let root = search_for_root root in
+  match root.inspection_data.product_name with
+  | Some v -> v
+  | None -> "unknown"
+
+and inspect_get_product_variant root =
+  let root = search_for_root root in
+  match root.inspection_data.product_variant with
+  | Some v -> v
+  | None -> "unknown"
+
+and inspect_get_major_version root =
+  let root = search_for_root root in
+  match root.inspection_data.version with
+  | Some (major, _) -> major
+  | None -> 0
+
+and inspect_get_minor_version root =
+  let root = search_for_root root in
+  match root.inspection_data.version with
+  | Some (_, minor) -> minor
+  | None -> 0
+
+and inspect_get_arch root =
+  let root = search_for_root root in
+  match root.inspection_data.arch with
+  | Some v -> v
+  | None -> "unknown"
+
+and inspect_get_hostname root =
+  let root = search_for_root root in
+  match root.inspection_data.hostname with
+  | Some v -> v
+  | None -> "unknown"
+
+and inspect_get_windows_systemroot root =
+  let root = search_for_root root in
+  match root.inspection_data.windows_systemroot with
+  | Some v -> v
+  | None ->
+     failwith "not a Windows guest, or systemroot could not be determined"
+
+and inspect_get_windows_system_hive root =
+  let root = search_for_root root in
+  match root.inspection_data.windows_system_hive with
+  | Some v -> v
+  | None ->
+     failwith "not a Windows guest, or system hive not found"
+
+and inspect_get_windows_software_hive root =
+  let root = search_for_root root in
+  match root.inspection_data.windows_software_hive with
+  | Some v -> v
+  | None ->
+     failwith "not a Windows guest, or software hive not found"
+
+and inspect_get_windows_current_control_set root =
+  let root = search_for_root root in
+  match root.inspection_data.windows_current_control_set with
+  | Some v -> v
+  | None ->
+     failwith "not a Windows guest, or CurrentControlSet could not be determined"
+
+and inspect_is_live root = false
+
+and inspect_is_netinst root = false
+
+and inspect_is_multipart root = false
+
+and inspect_get_drive_mappings root =
+  let root = search_for_root root in
+  root.inspection_data.drive_mappings
+
+and search_for_root root =
+  let fses = !Inspect_types.inspect_fses in
+  if fses = [] then
+    failwith "no inspection data: call guestfs_inspect_os first";
+
+  let root =
+    try
+      List.find (
+        function
+        | { fs_location = { mountable = m }; role = RoleRoot _ } -> root = m
+        | _ -> false
+      ) fses
+    with
+      Not_found ->
+        failwithf "%s: root device not found: only call this function with a root device previously returned by guestfs_inspect_os"
+                  (Mountable.to_string root) in
+
+  root_of_fs root

daemon/inspect_fs.ml
@@ -1,0 +1,363 @@
+(* guestfs-inspection
+ * Copyright (C) 2009-2017 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Printf
+
+open Std_utils
+
+open Mountable
+open Inspect_types
+open Inspect_utils
+
+let rec check_for_filesystem_on mountable vfs_type =
+  if verbose () then
+    eprintf "check_for_filesystem_on: %s (%s)\n%!"
+            (Mountable.to_string mountable) vfs_type;
+
+  let role =
+    let is_swap = vfs_type = "swap" in
+    if is_swap then
+      Some RoleSwap
+    else (
+      (* Try mounting the device.  Ignore errors if we can't do this. *)
+      let mounted =
+        if vfs_type = "ufs" then ( (* Hack for the *BSDs. *)
+          (* FreeBSD fs is a variant of ufs called ufs2 ... *)
+          try
+            Mount.mount_vfs (Some "ro,ufstype=ufs2") (Some "ufs")
+                            mountable "/";
+            true
+          with _ ->
+            (* while NetBSD and OpenBSD use another variant labeled 44bsd *)
+            try
+              Mount.mount_vfs (Some "ro,ufstype=44bsd") (Some "ufs")
+                              mountable "/";
+              true
+            with _ -> false
+        ) else (
+          try Mount.mount_ro mountable "/";
+              true
+          with _ -> false
+        ) in
+      if not mounted then None
+      else (
+        let role = check_filesystem mountable in
+        Mount.umount_all ();
+        role
+      )
+    ) in
+
+  match role with
+  | None -> None
+  | Some role ->
+     Some { fs_location = { mountable = mountable; vfs_type = vfs_type };
+            role = role }
+
+(* When this function is called, the filesystem is mounted on sysroot (). *)
+and check_filesystem mountable =
+  let role = ref `Other in
+  (* The following struct is mutated in place by callees.  However we
+   * need to make a copy of the object here so we don't mutate the
+   * null_inspection_data struct!
+   *)
+  let data = null_inspection_data () in
+
+  let debug_matching what =
+    if verbose () then
+      eprintf "check_filesystem: %s matched %s\n%!"
+              (Mountable.to_string mountable) what
+  in
+
+  (* Grub /boot? *)
+  if Is.is_file "/grub/menu.lst" ||
+     Is.is_file "/grub/grub.conf" ||
+     Is.is_file "/grub2/grub.cfg" then (
+    debug_matching "Grub /boot";
+    ()
+  )
+  (* FreeBSD root? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_file "/etc/freebsd-update.conf" &&
+          Is.is_file "/etc/fstab" then (
+    debug_matching "FreeBSD root";
+    role := `Root;
+    Inspect_fs_unix.check_freebsd_root mountable data
+  )
+  (* NetBSD root? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_file "/netbsd" &&
+          Is.is_file "/etc/fstab" &&
+          Is.is_file "/etc/release" then (
+    debug_matching "NetBSD root";
+    role := `Root;
+    Inspect_fs_unix.check_netbsd_root mountable data;
+  )
+  (* OpenBSD root? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_file "/bsd" &&
+          Is.is_file "/etc/fstab" &&
+          Is.is_file "/etc/motd" then (
+    debug_matching "OpenBSD root";
+    role := `Root;
+    Inspect_fs_unix.check_openbsd_root mountable data;
+  )
+  (* Hurd root? *)
+  else if Is.is_file "/hurd/console" &&
+          Is.is_file "/hurd/hello" &&
+          Is.is_file "/hurd/null" then (
+    debug_matching "Hurd root";
+    role := `Root;
+    Inspect_fs_unix.check_hurd_root mountable data;
+  )
+  (* Minix root? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_file "/service/vm" &&
+          Is.is_file "/etc/fstab" &&
+          Is.is_file "/etc/version" then (
+    debug_matching "Minix root";
+    role := `Root;
+    Inspect_fs_unix.check_minix_root data;
+  )
+  (* Linux root? *)
+  else if Is.is_dir "/etc" &&
+          (Is.is_dir "/bin" ||
+           is_symlink_to "/bin" "usr/bin") &&
+          (Is.is_file "/etc/fstab" ||
+           Is.is_file "/etc/hosts") then (
+    debug_matching "Linux root";
+    role := `Root;
+    Inspect_fs_unix.check_linux_root mountable data;
+  )
+  (* CoreOS root? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/root" &&
+          Is.is_dir "/home" &&
+          Is.is_dir "/usr" &&
+          Is.is_file "/etc/coreos/update.conf" then (
+    debug_matching "CoreOS root";
+    role := `Root;
+    Inspect_fs_unix.check_coreos_root mountable data;
+  )
+  (* Linux /usr/local? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_dir "/share" &&
+          not (Is.is_dir "/local") &&
+          not (Is.is_file "/etc/fstab") then (
+    debug_matching "Linux /usr/local";
+    ()
+  )
+  (* Linux /usr? *)
+  else if Is.is_dir "/etc" &&
+          Is.is_dir "/bin" &&
+          Is.is_dir "/share" &&
+          Is.is_dir "/local" &&
+          not (Is.is_file "/etc/fstab") then (
+    debug_matching "Linux /usr";
+    role := `Usr;
+    Inspect_fs_unix.check_linux_usr data;
+  )
+  (* CoreOS /usr? *)
+  else if Is.is_dir "/bin" &&
+          Is.is_dir "/share" &&
+          Is.is_dir "/local" &&
+          Is.is_dir "/share/coreos" then (
+    debug_matching "CoreOS /usr";
+    role := `Usr;
+    Inspect_fs_unix.check_coreos_usr mountable data;
+  )
+  (* Linux /var? *)
+  else if Is.is_dir "/log" &&
+          Is.is_dir "/run" &&
+          Is.is_dir "/spool" then (
+    debug_matching "Linux /var";
+    ()
+  )
+  (* Windows volume with installed applications (but not root)? *)
+  else if is_dir_nocase "/System Volume Information" &&
+          is_dir_nocase "/Program Files" then (
+    debug_matching "Windows volume with installed applications";
+    ()
+  )
+  (* Windows volume (but not root)? *)
+  else if is_dir_nocase "/System Volume Information" then (
+    debug_matching "Windows volume without installed applications";
+    ()
+  )
+  (* FreeDOS? *)
+  else if is_dir_nocase "/FDOS" &&
+          is_file_nocase "/FDOS/FREEDOS.BSS" then (
+    debug_matching "FreeDOS";
+    role := `Root;
+    data.os_type <- Some OS_TYPE_DOS;
+    data.distro <- Some DISTRO_FREEDOS;
+    (* FreeDOS is a mix of 16 and 32 bit, but
+     * assume it requires a 32 bit i386 processor.
+     *)
+    data.arch <- Some "i386"
+  )
+  (* None of the above. *)
+  else (
+    debug_matching "no known OS partition"
+  );
+
+  (* The above code should have set [data.os_type] and [data.distro]
+   * fields, so we can now guess the package management system.
+   *)
+  data.package_format <- check_package_format data;
+  data.package_management <- check_package_management data;
+
+  match !role with
+  | `Root -> Some (RoleRoot data)
+  | `Usr -> Some (RoleUsr data)
+  | `Other -> Some RoleOther
+
+and is_symlink_to file wanted_target =
+  if not (Is.is_symlink file) then false
+  else Link.readlink file = wanted_target
+
+(* At the moment, package format and package management are just a
+ * simple function of the [distro] and [version[0]] fields, so these
+ * can never return an error.  We might be cleverer in future.
+ *)
+and check_package_format { distro = distro } =
+  match distro with
+  | None -> None
+  | Some DISTRO_FEDORA
+  | Some DISTRO_MEEGO
+  | Some DISTRO_REDHAT_BASED
+  | Some DISTRO_RHEL
+  | Some DISTRO_MAGEIA
+  | Some DISTRO_MANDRIVA
+  | Some DISTRO_SUSE_BASED
+  | Some DISTRO_OPENSUSE
+  | Some DISTRO_SLES
+  | Some DISTRO_CENTOS
+  | Some DISTRO_SCIENTIFIC_LINUX
+  | Some DISTRO_ORACLE_LINUX
+  | Some DISTRO_ALTLINUX ->
+     Some PACKAGE_FORMAT_RPM
+  | Some DISTRO_DEBIAN
+  | Some DISTRO_UBUNTU
+  | Some DISTRO_LINUX_MINT ->
+     Some PACKAGE_FORMAT_DEB
+  | Some DISTRO_ARCHLINUX ->
+     Some PACKAGE_FORMAT_PACMAN
+  | Some DISTRO_GENTOO ->
+     Some PACKAGE_FORMAT_EBUILD
+  | Some DISTRO_PARDUS ->
+     Some PACKAGE_FORMAT_PISI
+  | Some DISTRO_ALPINE_LINUX ->
+     Some PACKAGE_FORMAT_APK
+  | Some DISTRO_VOID_LINUX ->
+     Some PACKAGE_FORMAT_XBPS
+  | Some DISTRO_SLACKWARE
+  | Some DISTRO_TTYLINUX
+  | Some DISTRO_COREOS
+  | Some DISTRO_WINDOWS
+  | Some DISTRO_BUILDROOT
+  | Some DISTRO_CIRROS
+  | Some DISTRO_FREEDOS
+  | Some DISTRO_FREEBSD
+  | Some DISTRO_NETBSD
+  | Some DISTRO_OPENBSD
+  | Some DISTRO_FRUGALWARE
+  | Some DISTRO_PLD_LINUX ->
+     None
+
+and check_package_management { distro = distro; version = version } =
+  let major = match version with None -> 0 | Some (major, _) -> major in
+  match distro with
+  | None -> None
+
+  | Some DISTRO_MEEGO ->
+     Some PACKAGE_MANAGEMENT_YUM
+
+  | Some DISTRO_FEDORA ->
+    (* If Fedora >= 22 and dnf is installed, say "dnf". *)
+     if major >= 22 && Is.is_file ~followsymlinks:true "/usr/bin/dnf" then
+       Some PACKAGE_MANAGEMENT_DNF
+     else if major >= 1 then
+       Some PACKAGE_MANAGEMENT_YUM
+     else
+       (* Probably parsing the release file failed, see RHBZ#1332025. *)
+       None
+
+  | Some DISTRO_REDHAT_BASED
+  | Some DISTRO_RHEL
+  | Some DISTRO_CENTOS
+  | Some DISTRO_SCIENTIFIC_LINUX
+  | Some DISTRO_ORACLE_LINUX ->
+     if major >= 8 then
+       Some PACKAGE_MANAGEMENT_DNF
+     else if major >= 5 then
+       Some PACKAGE_MANAGEMENT_YUM
+     else if major >= 2 then
+       Some PACKAGE_MANAGEMENT_UP2DATE
+     else
+       (* Probably parsing the release file failed, see RHBZ#1332025. *)
+       None
+
+  | Some DISTRO_DEBIAN
+  | Some DISTRO_UBUNTU
+  | Some DISTRO_LINUX_MINT
+  | Some DISTRO_ALTLINUX ->
+     Some PACKAGE_MANAGEMENT_APT
+
+  | Some DISTRO_ARCHLINUX ->
+     Some PACKAGE_MANAGEMENT_PACMAN
+
+  | Some DISTRO_GENTOO ->
+     Some PACKAGE_MANAGEMENT_PORTAGE
+
+  | Some DISTRO_PARDUS ->
+     Some PACKAGE_MANAGEMENT_PISI
+
+  | Some DISTRO_MAGEIA
+  | Some DISTRO_MANDRIVA ->
+     Some PACKAGE_MANAGEMENT_URPMI
+
+  | Some DISTRO_SUSE_BASED
+  | Some DISTRO_OPENSUSE
+  | Some DISTRO_SLES ->
+     Some PACKAGE_MANAGEMENT_ZYPPER
+
+  | Some DISTRO_ALPINE_LINUX ->
+     Some PACKAGE_MANAGEMENT_APK
+
+  | Some DISTRO_VOID_LINUX ->
+     Some PACKAGE_MANAGEMENT_XBPS;
+
+  | Some DISTRO_SLACKWARE
+  | Some DISTRO_TTYLINUX
+  | Some DISTRO_COREOS
+  | Some DISTRO_WINDOWS
+  | Some DISTRO_BUILDROOT
+  | Some DISTRO_CIRROS
+  | Some DISTRO_FREEDOS
+  | Some DISTRO_FREEBSD
+  | Some DISTRO_NETBSD
+  | Some DISTRO_OPENBSD
+  | Some DISTRO_FRUGALWARE
+  | Some DISTRO_PLD_LINUX ->
+    None
+

daemon/Makefile.am
@@ -252,6 +252,10 @@ SOURCES_MLI = \
 	file.mli \
 	filearch.mli \
 	findfs.mli \
+	inspect.mli \
+	inspect_fs.mli \
+	inspect_fs_unix.mli \
+	inspect_fs_unix_fstab.mli \
 	inspect_types.mli \
 	inspect_utils.mli \
 	is.mli \
@@ -291,6 +295,10 @@ SOURCES_ML = \
 	realpath.ml \
 	inspect_types.ml \
 	inspect_utils.ml \
+	inspect_fs_unix_fstab.ml \
+	inspect_fs_unix.ml \
+	inspect_fs.ml \
+	inspect.ml \
 	callbacks.ml \
 	daemon.ml
 

commit 26427b9ecc64e7e5e53a1d577cef9dc080d08877

inspection: More reliable detection of Linux split /usr configurations
In RHEL 8+, /usr/etc no longer exists.  Since we were looking for this
directory in order to detect a separate /usr partition, those were no
longer detected, so the merging of /usr data into the root was not
being done.  The result was incomplete inspection data and failure of
virt-v2v.

All Linux systems since forever have had /usr/src but not /src, so
detect this instead.

Furthermore the merging code didn't work, because we expected that the
root filesystem had a distro assigned, but in this configuration we
may need to look for that information in /usr/lib/os-release (not on
the root filesystem).  This change makes the merging work even if we
have incomplete information about the root filesystem, so long as we
have an /etc/fstab entry pointing to the /usr mountpoint.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1949683
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1930133
Fixes: commit 394d11be49121884295e61964ed47f5a8488c252

daemon/inspect.ml
@@ -182,11 +182,9 @@ and check_for_duplicated_bsd_root fses =
 and collect_linux_inspection_info fses =
   List.map (
     function
-    | { role = RoleRoot { distro = Some d } } as root ->
-       if d <> DISTRO_COREOS then
-         collect_linux_inspection_info_for fses root
-       else
-         root
+    | { role = RoleRoot { distro = Some DISTRO_COREOS } } as root -> root
+    | { role = RoleRoot _ } as root ->
+       collect_linux_inspection_info_for fses root
     | fs -> fs
   ) fses
 
@@ -196,29 +194,28 @@ and collect_linux_inspection_info fses =
  * or other ways to identify the OS).
  *)
 and collect_linux_inspection_info_for fses root =
-  let root_distro, root_fstab =
+  let root_fstab =
     match root with
-    | { role = RoleRoot { distro = Some d; fstab = f } } -> d, f
+    | { role = RoleRoot { fstab = f } } -> f
     | _ -> assert false in
 
   try
     let usr =
       List.find (
         function
-        | { role = RoleUsr { distro = d } }
-             when d = Some root_distro || d = None -> true
+        | { role = RoleUsr _; fs_location = usr_mp } ->
+           (* This checks that this usr is found in the fstab of
+            * the root filesystem.
+            *)
+           List.exists (
+             fun (mountable, _) ->
+               usr_mp.mountable = mountable
+           ) root_fstab
         | _ -> false
       ) fses in
 
-    let usr_mountable = usr.fs_location.mountable in
-
-    (* This checks that [usr] is found in the fstab of the root
-     * filesystem.  If not, [Not_found] is thrown.
-     *)
-    ignore (
-      List.find (fun (mountable, _) -> usr_mountable = mountable) root_fstab
-    );
-
+    eprintf "collect_linux_inspection_info_for: merging:\n%sinto:\n%s"
+      (string_of_fs usr) (string_of_fs root);
     merge usr root;
     root
   with

daemon/inspect_fs.ml
@@ -164,10 +164,10 @@ and check_filesystem mountable =
     ()
   )
   (* Linux /usr? *)
-  else if Is.is_dir "/etc" &&
-          Is.is_dir "/bin" &&
-          Is.is_dir "/share" &&
+  else if Is.is_dir "/bin" &&
           Is.is_dir "/local" &&
+          Is.is_dir "/share" &&
+          Is.is_dir "/src" &&
           not (Is.is_file "/etc/fstab") then (
     debug_matching "Linux /usr";
     role := `Usr;