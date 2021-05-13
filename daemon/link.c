libguestfs/libguestfs

commit 0f54df53d26e4c293871fb30bce88511e1d61d6c

build: Remove gnulib.
As part of our efforts to clean up and simplify libguestfs, removing
gnulib deletes a large dependency that we mostly no longer use and
causes problems for new users trying to build the library from source.

A few modules from gnulib are still used (under a compatible license)
and these are copied into gnulib/lib/

daemon/link.c
@@ -24,8 +24,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <limits.h>
-
-#include "areadlink.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "daemon.h"
 #include "actions.h"
@@ -47,20 +47,36 @@ do_internal_readlinklist (const char *path, char *const *names)
   }
 
   for (i = 0; names[i] != NULL; ++i) {
+    CLEANUP_FREE char *link = NULL;
+    struct stat statbuf;
+    size_t n;
+    ssize_t r;
+
     /* Because of the way this function is intended to be used,
      * we actually expect to see errors here, and they are not fatal.
      */
-    CLEANUP_FREE char *link = areadlinkat (fd_cwd, names[i]);
 
-    if (link != NULL) {
-      if (add_string (&ret, link) == -1) {
-      add_string_failed:
+    if (fstatat (fd_cwd, names[i], &statbuf, AT_SYMLINK_NOFOLLOW) == -1) {
+    add_empty_string:
+      if (add_string (&ret, "") == -1) {
         close (fd_cwd);
         return NULL;
       }
-    } else {
-      if (add_string (&ret, "") == -1)
-        goto add_string_failed;
+    }
+    if (!S_ISLNK (statbuf.st_mode))
+      goto add_empty_string;
+    n = statbuf.st_size;
+    link = malloc (n+1);
+    if (link == NULL)
+      goto add_empty_string;
+    r = readlinkat (fd_cwd, names[i], link, n);
+    if (r == -1 || r != n)
+      goto add_empty_string;
+    link[n] = '\0';
+
+    if (add_string (&ret, link) == -1) {
+      close (fd_cwd);
+      return NULL;
     }
   }
 

.gitignore
@@ -205,8 +205,6 @@ Makefile.in
 /generator/stamp-generator
 /.gitattributes
 /.git-module-status
-/gnulib
-/GNUmakefile
 /gobject/bindtests.js
 /gobject/Guestfs-1.0.gir
 /gobject/Guestfs-1.0.typelib
@@ -266,7 +264,6 @@ Makefile.in
 /lua/guestfs.so
 /lua/lua-guestfs.c
 /m4/ChangeLog
-/m4/gnulib-cache.m4
 /m4/intmax.m4
 /m4/libtool.m4
 /m4/lt~obsolete.m4

.gitmodules
@@ -1,6 +1,3 @@
-[submodule "gnulib"]
-	path = .gnulib
-	url = https://git.savannah.gnu.org/git/gnulib.git
 [submodule "common"]
 	path = common
 	url = https://github.com/libguestfs/libguestfs-common

Makefile.am
@@ -26,13 +26,8 @@ SUBDIRS = common/mlstdutils generator
 # tests run.
 SUBDIRS += test-data
 
-# Gnulib - must be built and tested before the library.
-SUBDIRS += gnulib/lib
-if ENABLE_GNULIB_TESTS
-SUBDIRS += gnulib/tests
-endif
-
 # Basic source for the library.
+SUBDIRS += gnulib/lib
 SUBDIRS += common/errnostring common/protocol common/qemuopts
 SUBDIRS += common/utils
 SUBDIRS += common/structs
@@ -126,12 +121,9 @@ EXTRA_DIST = \
 	.gitignore \
 	.lvimrc \
 	.mailmap \
-	bootstrap \
 	bugs-in-changelog.sh \
-	autogen.sh \
 	bindtests \
 	build-aux/config.rpath \
-	cfg.mk \
 	check-mli.sh \
 	common/.gitignore \
 	common/README \
@@ -234,7 +226,7 @@ BUGS: configure.ac
 docs/C_SOURCE_FILES: configure.ac
 	rm -f $@ $@-t
 	find $(DIST_SUBDIRS) -name '*.[ch]' | \
-	grep -v -E '^(builder/index-parse\.|builder/index-scan\.|examples/|gnulib/|gobject/|java/com_redhat_et_libguestfs|perl/|php/extension/config\.h|ruby/ext/guestfs/extconf\.h|tests/|test-data/|bundled/)' | \
+	grep -v -E '^(builder/index-parse\.|builder/index-scan\.|examples/|gobject/|java/com_redhat_et_libguestfs|perl/|php/extension/config\.h|ruby/ext/guestfs/extconf\.h|tests/|test-data/|bundled/)' | \
 	grep -v -E '/(guestfs|rc)_protocol\.' | \
 	grep -v -E '.*/errnostring\.' | \
 	grep -v -E '.*-gperf\.' | \
@@ -247,7 +239,7 @@ po/POTFILES: configure.ac
 	rm -f $@ $@-t
 	cd $(srcdir); \
 	find $(DIST_SUBDIRS) -name '*.c' | \
-	grep -v -E '^(examples|gnulib|perl/(blib|examples)|po-docs|tests|test-data|bundled)/' | \
+	grep -v -E '^(examples|perl/(blib|examples)|po-docs|tests|test-data|bundled)/' | \
 	grep -v -E '/((guestfs|rc)_protocol\.c|dummy\.c)$$' | \
 	grep -v -E '^python/utils\.c$$' | \
 	grep -v -E '^perl/lib/Sys/Guestfs\.c$$' | \
@@ -490,7 +482,6 @@ maintainer-check-extra-dist:
 	  grep -v '^common/mlxml/' | \
 	  grep -v '^intltool-.*\.in' | \
 	  grep -v '^\.gitmodules' | \
-	  grep -v '^\.gnulib' | \
 	  sort > tmp/gitfiles
 	comm -13 tmp/tarfiles tmp/gitfiles > tmp/comm-out
 	@echo Checking for differences between EXTRA_DIST and git ...

autogen.sh
@@ -1,51 +1,0 @@
-#!/bin/bash -
-# libguestfs
-# Copyright (C) 2009 Red Hat Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Rebuild the autotools environment.
-
-set -e
-set -v
-
-# Ensure that whenever we pull in a gnulib update or otherwise change to a
-# different version (i.e., when switching branches), we also rerun ./bootstrap.
-curr_status=.git-module-status
-t=$(git submodule status | grep gnulib | sed 's/^[ +-]//;s/ .*//')
-if test "$t" = "$(cat $curr_status 2>/dev/null)"; then
-    : # good, it's up to date
-else
-    echo running bootstrap...
-    ./bootstrap && echo "$t" > $curr_status
-fi
-
-CONFIGUREDIR=.
-
-# Run configure in BUILDDIR if it's set
-if [ ! -z "$BUILDDIR" ]; then
-    mkdir -p $BUILDDIR
-    cd $BUILDDIR
-
-    CONFIGUREDIR=..
-fi
-
-# If no arguments were specified and configure has run before, use the previous
-# arguments
-if test $# -eq 0 && test -x ./config.status; then
-    ./config.status --recheck
-else
-    $CONFIGUREDIR/configure "$@"
-fi

bootstrap
@@ -1,174 +1,0 @@
-#!/bin/sh
-
-usage() {
-  echo >&2 "\
-Usage: $0 [OPTION]...
-Bootstrap this package from the checked-out sources.
-
-Options:
- --gnulib-srcdir=DIRNAME  specify the local directory where gnulib
-                          sources reside.  Use this if you already
-                          have gnulib sources on your machine, and
-                          do not want to waste your bandwidth downloading
-                          them again.  Defaults to \$GNULIB_SRCDIR
-"
-}
-
-for option
-do
-  case $option in
-  --help)
-    usage
-    exit;;
-  --gnulib-srcdir=*)
-    GNULIB_SRCDIR=${option#--gnulib-srcdir=};;
-  *)
-    echo >&2 "$0: $option: unknown option"
-    exit 1;;
-  esac
-done
-
-cleanup_gnulib() {
-  status=$?
-  rm -fr "$gnulib_path"
-  exit $status
-}
-
-git_modules_config () {
-  test -f .gitmodules && git config --file .gitmodules "$@"
-}
-
-gnulib_path=$(git_modules_config submodule.gnulib.path)
-test -z "$gnulib_path" && gnulib_path=gnulib
-
-# Get gnulib files.  Populate $GNULIB_SRCDIR, possibly updating a
-# submodule, for use in the rest of the script.
-
-case ${GNULIB_SRCDIR--} in
--)
-  if git_modules_config submodule.gnulib.url >/dev/null; then
-    echo "$0: getting gnulib files..."
-    git submodule init -- "$gnulib_path" || exit $?
-    git submodule update -- "$gnulib_path" || exit $?
-
-  elif [ ! -d "$gnulib_path" ]; then
-    echo "$0: getting gnulib files..."
-
-    trap cleanup_gnulib 1 2 13 15
-
-    shallow=
-    git clone -h 2>&1 | grep -- --depth > /dev/null && shallow='--depth 2'
-    git clone $shallow git://git.sv.gnu.org/gnulib "$gnulib_path" ||
-      cleanup_gnulib
-
-    trap - 1 2 13 15
-  fi
-  GNULIB_SRCDIR=$gnulib_path
-  ;;
-*)
-  # Use GNULIB_SRCDIR directly or as a reference.
-  if test -d "$GNULIB_SRCDIR"/.git && \
-        git_modules_config submodule.gnulib.url >/dev/null; then
-    echo "$0: getting gnulib files..."
-    if git submodule -h|grep -- --reference > /dev/null; then
-      # Prefer the one-liner available in git 1.6.4 or newer.
-      git submodule update --init --reference "$GNULIB_SRCDIR" \
-        "$gnulib_path" || exit $?
-    else
-      # This fallback allows at least git 1.5.5.
-      if test -f "$gnulib_path"/gnulib-tool; then
-        # Since file already exists, assume submodule init already complete.
-        git submodule update -- "$gnulib_path" || exit $?
-      else
-        # Older git can't clone into an empty directory.
-        rmdir "$gnulib_path" 2>/dev/null
-        git clone --reference "$GNULIB_SRCDIR" \
-          "$(git_modules_config submodule.gnulib.url)" "$gnulib_path" \
-          && git submodule init -- "$gnulib_path" \
-          && git submodule update -- "$gnulib_path" \
-          || exit $?
-      fi
-    fi
-    GNULIB_SRCDIR=$gnulib_path
-  fi
-  ;;
-esac
-
-# Autoreconf runs aclocal before libtoolize, which causes spurious
-# warnings if the initial aclocal is confused by the libtoolized
-# (or worse out-of-date) macro directory.
-libtoolize --copy --install
-
-gnulib_tool=$GNULIB_SRCDIR/gnulib-tool
-<$gnulib_tool || exit
-
-modules='
-accept4
-areadlinkat
-base64
-byteswap
-c-ctype
-cloexec
-closeout
-connect
-error
-fstatat
-full-read
-full-write
-futimens
-getline
-getprogname
-gitlog-to-changelog
-glob
-gnumakefile
-hash
-hash-pjw
-human
-ignore-value
-intprops
-lock
-maintainer-makefile
-manywarnings
-memmem
-mkdtemp
-mkstemps
-netdb
-nonblocking
-perror
-pipe2
-pread
-readlink
-select
-setenv
-sleep
-socket
-strchrnul
-strerror
-strndup
-sys_select
-sys_types
-sys_wait
-tls
-vasprintf
-vc-list-files
-warnings
-xstrtol
-xstrtoll
-xvasprintf
-'
-
-# If any tests fail, avoid including them by adding them to
-# this list.
-avoid="--avoid=dummy --avoid=getlogin_r-tests"
-
-$gnulib_tool			\
-  $avoid			\
-  --with-tests			\
-  --m4-base=m4			\
-  --source-base=gnulib/lib	\
-  --tests-base=gnulib/tests	\
-  --libtool			\
-  --import $modules
-
-# Disable autopoint and libtoolize, since they were already done above.
-AUTOPOINT=true LIBTOOLIZE=true autoreconf --verbose --install

cfg.mk
@@ -1,141 +1,0 @@
-# Customize Makefile.maint.                           -*- makefile -*-
-# Copyright (C) 2003-2012 Free Software Foundation, Inc.
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Use alpha.gnu.org for alpha and beta releases.
-# Use ftp.gnu.org for major releases.
-gnu_ftp_host-alpha = alpha.gnu.org
-gnu_ftp_host-beta = alpha.gnu.org
-gnu_ftp_host-major = ftp.gnu.org
-gnu_rel_host = $(gnu_ftp_host-$(RELEASE_TYPE))
-
-url_dir_list = \
-  ftp://$(gnu_rel_host)/gnu/coreutils
-
-# Exclude some filenames.
-exclude_file_name_regexp--sc_bindtextdomain = ^(daemon|erlang|examples|tests)/|/test-
-exclude_file_name_regexp--sc_error_message_period = ^(generator|php|po-docs)/
-exclude_file_name_regexp--sc_prohibit_always-defined_macros = ^examples/|^tests/xml/fake-libvirt-xml\.c|^daemon/guestfsd\.c
-exclude_file_name_regexp--sc_prohibit_doubled_word = ^po/
-exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = ^test-data/|\.patch$|\.png$
-exclude_file_name_regexp--sc_prohibit_magic_number_exit = ^(po|po-docs)/|\.pod$|^fuse/guestunmount\.c
-exclude_file_name_regexp--sc_prohibit_strcmp = ^(examples|po-docs)/|\.pod$
-exclude_file_name_regexp--sc_prohibit_strcmp_and_strncmp = ^(examples|po-docs)/|\.pod$
-exclude_file_name_regexp--sc_prohibit_strncpy = ^lib/launch-.*\.c$
-exclude_file_name_regexp--sc_require_config_h = ^examples/|^tests/c-api/test-just-header\.c$
-exclude_file_name_regexp--sc_require_config_h_first = ^examples/|^tests/c-api/test-just-header\.c$|^python/guestfs-py-byhand\.c$
-
-# Tests not to run as part of "make distcheck".
-local-checks-to-skip =			\
-  sc_po_check				\
-  sc_GPL_version			\
-  sc_error_exit_success			\
-  sc_file_system			\
-  sc_makefile_path_separator_check	\
-  sc_obsolete_symbols			\
-  sc_prohibit_atoi_atof			\
-  sc_prohibit_quote_without_use		\
-  sc_prohibit_quotearg_without_use	\
-  sc_prohibit_stat_st_blocks		\
-  sc_space_tab				\
-  sc_two_space_separator_in_usage	\
-  sc_error_message_uppercase		\
-  sc_program_name			\
-  $(disable_temporarily)		\
-  sc_useless_cpp_parens			\
-  sc_cast_of_argument_to_free
-
-disable_temporarily =			\
-  sc_makefile_check			\
-  sc_unmarked_diagnostics		\
-  sc_prohibit_ctype_h			\
-  sc_prohibit_asprintf			\
-  sc_avoid_write
-
-# Avoid uses of write(2).  Either switch to streams (fwrite), or use
-# the safewrite wrapper.
-sc_avoid_write:
-	@if $(VC_LIST_EXCEPT) | grep '\.c$$' > /dev/null; then		\
-	  grep '\<write *(' $$($(VC_LIST_EXCEPT) | grep '\.c$$') &&	\
-	    { echo "$(ME): the above files use write;"			\
-	      " consider using the safewrite wrapper instead"		\
-		  1>&2; exit 1; } || :;					\
-	else :;								\
-	fi
-
-# Use STREQ rather than comparing strcmp == 0, or != 0.
-# Similarly, use STREQLEN or STRPREFIX rather than strncmp.
-sc_prohibit_strcmp_and_strncmp:
-	@grep -nE '! *strn?cmp *\(|\<strn?cmp *\([^)]+\) *=='		\
-	    $$($(VC_LIST_EXCEPT))					\
-	  | grep -vE ':# *define STREQ(LEN)?\(' &&			\
-	  { echo '$(ME): use STREQ(LEN) in place of the above uses of strcmp(strncmp)' \
-		1>&2; exit 1; } || :
-
-# Use virAsprintf rather than a'sprintf since *strp is undefined on error.
-sc_prohibit_asprintf:
-	@re='\<[a]sprintf\>'						\
-	msg='use virAsprintf, not a'sprintf				\
-	  $(_prohibit_regexp)
-
-# Prohibit the inclusion of <ctype.h>.
-sc_prohibit_ctype_h:
-	@grep -E '^# *include  *<ctype\.h>' $$($(VC_LIST_EXCEPT)) &&	\
-	  { echo "$(ME): don't use ctype.h; instead, use c-ctype.h"	\
-		1>&2; exit 1; } || :
-
-ctype_re = isalnum|isalpha|isascii|isblank|iscntrl|isdigit|isgraph|islower\
-|isprint|ispunct|isspace|isupper|isxdigit|tolower|toupper
-
-sc_avoid_ctype_macros:
-	@grep -E '\b($(ctype_re)) *\(' /dev/null			\
-	     $$($(VC_LIST_EXCEPT)) &&					\
-	  { echo "$(ME): don't use ctype macros (use c-ctype.h)"	\
-		1>&2; exit 1; } || :
-
-sc_prohibit_virBufferAdd_with_string_literal:
-	@prohibit='\<virBufferAdd *\([^,]+, *"[^"]'			\
-	halt='use virBufferAddLit, not virBufferAdd, with a string literal' \
-	  $(_sc_search_regexp)
-
-# Not only do they fail to deal well with ipv6, but the gethostby*
-# functions are also not thread-safe.
-sc_prohibit_gethostby:
-	@prohibit='\<gethostby(addr|name2?) *\('			\
-	halt='use getaddrinfo, not gethostby*'				\
-	  $(_sc_search_regexp)
-
-# We don't use this feature of maint.mk.
-prev_version_file = /dev/null
-
-ifeq (0,$(MAKELEVEL))
-  _curr_status = .git-module-status
-  # The sed filter accommodates those who check out on a commit from which
-  # no tag is reachable.  In that case, git submodule status prints a "-"
-  # in column 1 and does not print a "git describe"-style string after the
-  # submodule name.  Contrast these:
-  # -b653eda3ac4864de205419d9f41eec267cb89eeb .gnulib
-  #  b653eda3ac4864de205419d9f41eec267cb89eeb .gnulib (v0.0-2286-gb653eda)
-  # $ cat .git-module-status
-  # b653eda3ac4864de205419d9f41eec267cb89eeb
-  _submodule_hash = sed 's/^[ +-]//;s/ .*//'
-  _update_required := $(shell						\
-      actual=$$(git submodule status | grep gnulib | $(_submodule_hash));		\
-      stamp="$$($(_submodule_hash) $(_curr_status) 2>/dev/null)";	\
-      test "$$stamp" = "$$actual"; echo $$?)
-  ifeq (1,$(_update_required))
-    $(error gnulib update required; run ./autogen.sh first)
-  endif
-endif

configure.ac
@@ -85,11 +85,6 @@ PACKAGE_VERSION_FULL="libguestfs_major.libguestfs_minor.libguestfs_release${libg
 AC_DEFINE_UNQUOTED([PACKAGE_VERSION_FULL],["$PACKAGE_VERSION_FULL"],[Full version string.])
 AC_SUBST([PACKAGE_VERSION_FULL])
 
-dnl Early gnulib initialization.
-HEADING([Configuring Gnulib])
-gl_EARLY
-gl_INIT
-
 dnl Check for external programs required to either build or run
 dnl libguestfs.
 HEADING([Checking for external programs])
@@ -242,7 +237,6 @@ AC_CONFIG_FILES([Makefile
                  fuse/Makefile
                  generator/Makefile
                  gnulib/lib/Makefile
-                 gnulib/tests/Makefile
                  gobject/libguestfs-gobject-1.0.pc
                  gobject/Makefile
                  golang/Makefile

commit 047cf7dcd26e649d45e7e21a3b679bad0bb6c312

daemon/link.c: Fix out of memory error when reading symlinks
Commit 0f54df53d2 ("build: Remove gnulib") introduced a bug when I
rewrote existing code that used gnulib areadlink().

A missing "continue" statement on the path where fstatat(2) failed
caused fall-through to the case where it tries to use malloc(3) on the
value from the uninitialized stat buf.  This caused a huge amount of
memory to be allocated, invoking the oom-killer inside the appliance.

Reported-by: Yongkui Guo
Fixes: commit 0f54df53d26e4c293871fb30bce88511e1d61d6c
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1960217

daemon/link.c
@@ -59,9 +59,11 @@ do_internal_readlinklist (const char *path, char *const *names)
     if (fstatat (fd_cwd, names[i], &statbuf, AT_SYMLINK_NOFOLLOW) == -1) {
     add_empty_string:
       if (add_string (&ret, "") == -1) {
+      add_string_failed:
         close (fd_cwd);
         return NULL;
       }
+      continue;
     }
     if (!S_ISLNK (statbuf.st_mode))
       goto add_empty_string;
@@ -74,10 +76,8 @@ do_internal_readlinklist (const char *path, char *const *names)
       goto add_empty_string;
     link[n] = '\0';
 
-    if (add_string (&ret, link) == -1) {
-      close (fd_cwd);
-      return NULL;
-    }
+    if (add_string (&ret, link) == -1)
+      goto add_string_failed;
   }
 
   close (fd_cwd);