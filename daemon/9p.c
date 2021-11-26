libguestfs/libguestfs

commit 5f10c3350338bbca735a74db26f98da968957bd9

New API: list-9p lists 9p filesystem mount tags (RHBZ#714981).

daemon/9p.c
@@ -1,0 +1,170 @@
+/* libguestfs - the guestfsd daemon
+ * Copyright (C) 2011 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+#include "daemon.h"
+#include "actions.h"
+
+#define BUS_PATH "/sys/bus/virtio/drivers/9pnet_virtio"
+
+static char *read_whole_file (const char *filename);
+
+/* https://bugzilla.redhat.com/show_bug.cgi?id=714981#c1 */
+char **
+do_list_9p (void)
+{
+  char **r = NULL;
+  int size = 0, alloc = 0;
+
+  DIR *dir;
+  int err = 0;
+
+  dir = opendir (BUS_PATH);
+  if (!dir) {
+    perror ("opendir: " BUS_PATH);
+    if (errno != ENOENT)
+      return NULL;
+
+    /* If this directory doesn't exist, it probably means that
+     * the virtio driver isn't loaded.  Don't return an error
+     * in this case, but return an empty list.
+     */
+    if (add_string (&r, &size, &alloc, NULL) == -1)
+      return NULL;
+
+    return r;
+  }
+
+  while (1) {
+    errno = 0;
+    struct dirent *d = readdir (dir);
+    if (d == NULL) break;
+
+    if (STRPREFIX (d->d_name, "virtio")) {
+      char mount_tag_path[256];
+      snprintf (mount_tag_path, sizeof mount_tag_path,
+                BUS_PATH "/%s/mount_tag", d->d_name);
+
+      /* A bit unclear, but it looks like the virtio transport allows
+       * the mount tag length to be unlimited (or up to 65536 bytes).
+       * See: linux/include/linux/virtio_9p.h
+       */
+      char *mount_tag = read_whole_file (mount_tag_path);
+      if (mount_tag == 0)
+        continue;
+
+      if (add_string (&r, &size, &alloc, mount_tag) == -1) {
+        free (mount_tag);
+        free_stringslen (r, size);
+        closedir (dir);
+        return NULL;
+      }
+
+      free (mount_tag);
+    }
+  }
+
+  /* Check readdir didn't fail */
+  if (errno != 0) {
+    reply_with_perror ("readdir: /sys/block");
+    free_stringslen (r, size);
+    closedir (dir);
+    return NULL;
+  }
+
+  /* Close the directory handle */
+  if (closedir (dir) == -1) {
+    reply_with_perror ("closedir: /sys/block");
+    free_stringslen (r, size);
+    return NULL;
+  }
+
+  /* Sort the tags.  Note that r might be NULL if there are no tags. */
+  if (r != NULL)
+    sort_strings (r, size);
+
+  /* NULL terminate the list */
+  if (add_string (&r, &size, &alloc, NULL) == -1)
+    return NULL;
+
+  return r;
+}
+
+/* Read whole file into dynamically allocated array.  If there is an
+ * error, DON'T call reply_with_perror, just return NULL.  Returns a
+ * \0-terminated string.
+ */
+static char *
+read_whole_file (const char *filename)
+{
+  char *r = NULL;
+  size_t alloc = 0, size = 0;
+  int fd;
+
+  fd = open (filename, O_RDONLY);
+  if (fd == -1) {
+    perror (filename);
+    return NULL;
+  }
+
+  while (1) {
+    alloc += 256;
+    char *r2 = realloc (r, alloc);
+    if (r2 == NULL) {
+      perror ("realloc");
+      free (r);
+      return NULL;
+    }
+    r = r2;
+
+    /* The '- 1' in the size calculation ensures there is space below
+     * to add \0 to the end of the input.
+     */
+    ssize_t n = read (fd, r + size, alloc - size - 1);
+    if (n == -1) {
+      perror (filename);
+      free (r);
+      return NULL;
+    }
+    if (n == 0)
+      break;
+    size += n;
+  }
+
+  if (close (fd) == -1) {
+    perror (filename);
+    free (r);
+    return NULL;
+  }
+
+  r[size] = '\0';
+
+  return r;
+}

daemon/Makefile.am
@@ -88,6 +88,7 @@ errnostring.h: $(libsrcdir)/errnostring.h
 
 noinst_PROGRAMS = guestfsd
 guestfsd_SOURCES = \
+	9p.c \
 	actions.h \
 	available.c \
 	augeas.c \

generator/generator_actions.ml
@@ -5960,6 +5960,13 @@ This returns true iff the device exists and contains all zero bytes.
 
 Note that for large devices this can take a long time to run.");
 
+  ("list_9p", (RStringList "mounttags", [], []), 285, [],
+   [],
+   "list 9p filesystems",
+   "\
+List all 9p filesystems attached to the guest.  A list of
+mount tags is returned.");
+
 ]
 
 let all_functions = non_daemon_functions @ daemon_functions

po/POTFILES.in
@@ -1,6 +1,7 @@
 cat/virt-cat.c
 cat/virt-filesystems.c
 cat/virt-ls.c
+daemon/9p.c
 daemon/augeas.c
 daemon/available.c
 daemon/base64.c

src/MAX_PROC_NR
@@ -1,1 +1,1 @@
-284
+285

commit e3671362afc6d4494753e39762925c2ad32df8a2

daemon/9p: fix wrong pathname in error message
The directory that readdir() and closedir() work on is BUS_PATH
("/sys/bus/virtio/drivers/9pnet_virtio"), not "/sys/block". Fix the error
messages that are sent when readdir() or closedir() fails.

(The invalid "sys/block" pathname could be a leftover from when the
directory reading logic was (perhaps) copied from "daemon/sync.c".)

Fixes: 5f10c3350338bbca735a74db26f98da968957bd9
Signed-off-by: Laszlo Ersek <lersek@redhat.com>
Acked-by: Richard W.M. Jones <rjones@redhat.com>
Message-Id: <20211125094954.9713-3-lersek@redhat.com>

daemon/9p.c
@@ -106,14 +106,14 @@ do_list_9p (void)
 
   /* Check readdir didn't fail */
   if (errno != 0) {
-    reply_with_perror ("readdir: /sys/block");
+    reply_with_perror ("readdir: " BUS_PATH);
     closedir (dir);
     return NULL;
   }
 
   /* Close the directory handle */
   if (closedir (dir) == -1) {
-    reply_with_perror ("closedir: /sys/block");
+    reply_with_perror ("closedir: " BUS_PATH);
     return NULL;
   }
 