libguestfs/libguestfs

commit c9ee831affed55abe0f928134cbbd2ed83b2f510

inspection: Fix inspection of recent RPM guests using non-BDB.
Recent RPM-based guests have switched from using Berkeley DB (BDB) to
sqlite.  In order to inspect these guests (and earlier ones) we need
to stop using the hokey parsing of the BDB and use librpm APIs
instead.

This commit adds a new internal API so we can call librpm from the
daemon, and changes the library part to use the new API for RPM-based
guests.

This change removes the requirement for BDB tools like db_dump.

See also:
http://lists.rpm.org/pipermail/rpm-ecosystem/2021-March/000751.html
http://lists.rpm.org/pipermail/rpm-ecosystem/2021-March/000754.html
https://blog.fpmurphy.com/2011/08/programmatically-retrieve-rpm-package-details.html

This breaks the virt-inspector test (now in the separate guestfs-tools
repository).  However this is not a bug in libguestfs, but a bug in
the phoney Fedora guest that we use for testing - we created a
BDB-style RPM database which was supposed to be just enough to make
the old code work.  The new code using real librpm needs
/usr/lib/rpm/rpmrc (not present in the phoney image) and also cannot
parse the phoney database, so we will need to separately rework that
test.

Thanks: Panu Matilainen
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1766487
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1409024

daemon/rpm-c.c
@@ -1,0 +1,161 @@
+/* libguestfs - the guestfsd daemon
+ * Copyright (C) 2021 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <caml/alloc.h>
+#include <caml/fail.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
+
+#ifdef HAVE_LIBRPM
+#include <rpm/rpmlib.h>
+#include <rpm/header.h>
+#include <rpm/rpmts.h>
+#include <rpm/rpmdb.h>
+#endif
+
+#include "daemon.h"
+#include "actions.h"
+
+/* Very lightweight OCaml bindings for librpm. */
+
+#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
+#pragma GCC diagnostic ignored "-Wmissing-prototypes"
+
+#ifndef HAVE_LIBRPM
+
+value __attribute__((noreturn))
+guestfs_int_daemon_rpm_init (value unitv)
+{
+  CAMLparam1 (unitv);
+  caml_failwith ("no support for RPM guests because "
+                 "librpm was missing at compile time");
+}
+
+value __attribute__((noreturn))
+guestfs_int_daemon_rpm_start_iterator (value unitv)
+{
+  guestfs_int_daemon_rpm_init (unitv);
+}
+
+value __attribute__((noreturn))
+guestfs_int_daemon_rpm_next_application (value unitv)
+{
+  guestfs_int_daemon_rpm_init (unitv);
+}
+
+value __attribute__((noreturn))
+guestfs_int_daemon_rpm_end_iterator (value unitv)
+{
+  guestfs_int_daemon_rpm_init (unitv);
+}
+
+#else /* HAVE_LIBRPM */
+
+value
+guestfs_int_daemon_rpm_init (value unitv)
+{
+  CAMLparam1 (unitv);
+  rpmReadConfigFiles (NULL, NULL);
+  CAMLreturn (Val_unit);
+}
+
+static rpmts ts;
+static rpmdbMatchIterator iter;
+
+value
+guestfs_int_daemon_rpm_start_iterator (value unitv)
+{
+  CAMLparam1 (unitv);
+  ts = rpmtsCreate ();
+  iter = rpmtsInitIterator (ts, RPMDBI_PACKAGES, NULL, 0);
+  CAMLreturn (Val_unit);
+}
+
+value
+guestfs_int_daemon_rpm_next_application (value unitv)
+{
+  CAMLparam1 (unitv);
+  CAMLlocal2 (rv, sv);
+  Header h;
+  guestfs_int_application2 app = { 0 };
+
+  h = rpmdbNextIterator (iter);
+  if (h == NULL) caml_raise_not_found ();
+
+  h = headerLink (h);
+  app.app2_name = headerFormat (h, "%{NAME}", NULL);
+  // XXXapp.app2_epoch = headerFormat (h, "%{NAME}", NULL);
+  app.app2_version = headerFormat (h, "%{VERSION}", NULL);
+  app.app2_release = headerFormat (h, "%{RELEASE}", NULL);
+  app.app2_arch = headerFormat (h, "%{ARCH}", NULL);
+  app.app2_url = headerFormat (h, "%{URL}", NULL);
+  app.app2_summary = headerFormat (h, "%{SUMMARY}", NULL);
+  app.app2_description = headerFormat (h, "%{DESCRIPTION}", NULL);
+  headerFree (h);
+
+  /* Convert this to an OCaml struct.  Any NULL fields must be turned
+   * into empty string.
+   */
+  rv = caml_alloc (17, 0);
+#define TO_CAML_STRING(i, name)                     \
+  sv = caml_copy_string (app.name ? app.name : ""); \
+  Store_field (rv, i, sv);                          \
+  free (app.name)
+
+  TO_CAML_STRING (0, app2_name);
+  TO_CAML_STRING (1, app2_display_name);
+  sv = caml_copy_int32 (app.app2_epoch);
+  Store_field (rv, 2, sv);
+  TO_CAML_STRING (3, app2_version);
+  TO_CAML_STRING (4, app2_release);
+  TO_CAML_STRING (5, app2_arch);
+  TO_CAML_STRING (6, app2_install_path);
+  TO_CAML_STRING (7, app2_trans_path);
+  TO_CAML_STRING (8, app2_publisher);
+  TO_CAML_STRING (9, app2_url);
+  TO_CAML_STRING (10, app2_source_package);
+  TO_CAML_STRING (11, app2_summary);
+  TO_CAML_STRING (12, app2_description);
+  TO_CAML_STRING (13, app2_spare1);
+  TO_CAML_STRING (14, app2_spare2);
+  TO_CAML_STRING (15, app2_spare3);
+  TO_CAML_STRING (16, app2_spare4);
+#undef TO_CAML_STRING
+
+  CAMLreturn (rv);
+}
+
+value
+guestfs_int_daemon_rpm_end_iterator (value unitv)
+{
+  CAMLparam1 (unitv);
+  rpmdbFreeIterator (iter);
+  rpmtsFree (ts);
+  CAMLreturn (Val_unit);
+}
+
+#endif /* HAVE_LIBRPM */

.gitignore
@@ -107,6 +107,7 @@ Makefile.in
 /daemon/optgroups.mli
 /daemon/parted.mli
 /daemon/realpath.mli
+/daemon/rpm.mli
 /daemon/stamp-guestfsd.pod
 /daemon/statvfs.mli
 /daemon/structs-cleanups.c

appliance/packagelist.in
@@ -41,6 +41,7 @@ ifelse(REDHAT,1,
   openssh-clients
   policycoreutils
   reiserfs-utils
+  rpm-libs
   syslinux-extlinux
   systemd              dnl for /sbin/reboot and udevd
   vim-minimal
@@ -71,6 +72,7 @@ dnl iproute has been renamed to iproute2
   isc-dhcp-client
   ldmtool
   libc-bin
+  librpm9
   linux-image
   dnl syslinux 'suggests' mtools, but in reality it's a hard dependency:
   mtools

daemon/Makefile.am
@@ -57,6 +57,7 @@ generator_built = \
 	optgroups.mli \
 	parted.mli \
 	realpath.mli \
+	rpm.mli \
 	statvfs.mli \
 	structs.ml \
 	structs.mli
@@ -168,6 +169,7 @@ guestfsd_SOURCES = \
 	proto.c \
 	readdir.c \
 	rename.c \
+	rpm-c.c \
 	rsync.c \
 	scrub.c \
 	selinux.c \
@@ -232,6 +234,7 @@ guestfsd_LDADD = \
 	$(LIB_CLOCK_GETTIME) \
 	$(LIBINTL) \
 	$(PCRE2_LIBS) \
+	$(LIBRPM_LIBS) \
 	$(TSK_LIBS) \
 	$(RPC_LIBS) \
 	$(YARA_LIBS) \
@@ -262,7 +265,8 @@ guestfsd_CFLAGS = \
 	$(HIVEX_CFLAGS) \
 	$(SD_JOURNAL_CFLAGS) \
 	$(JANSSON_CFLAGS) \
-	$(PCRE2_CFLAGS)
+	$(PCRE2_CFLAGS) \
+	$(LIBRPM_CFLAGS)
 
 # Parts of the daemon are written in OCaml.  These are linked into a
 # library and then linked to the daemon.  See
@@ -300,6 +304,7 @@ SOURCES_MLI = \
 	optgroups.mli \
 	parted.mli \
 	realpath.mli \
+	rpm.mli \
 	statvfs.mli \
 	structs.mli \
 	sysroot.mli \
@@ -340,6 +345,7 @@ SOURCES_ML = \
 	inspect_fs_windows.ml \
 	inspect_fs.ml \
 	inspect.ml \
+	rpm.ml \
 	callbacks.ml \
 	daemon.ml
 

daemon/rpm.ml
@@ -1,0 +1,58 @@
+(* guestfs-inspection
+ * Copyright (C) 2009-2021 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Printf
+
+open Std_utils
+
+external rpm_init : unit -> unit = "guestfs_int_daemon_rpm_init"
+external rpm_start_iterator : unit -> unit = "guestfs_int_daemon_rpm_start_iterator"
+external rpm_next_application : unit -> Structs.application2 = "guestfs_int_daemon_rpm_next_application"
+external rpm_end_iterator : unit -> unit = "guestfs_int_daemon_rpm_end_iterator"
+
+(* librpm is troublesome when run from the main process.  In
+ * particular it holds open some glibc NSS files.  Therefore we fork
+ * before doing the chroot and any librpm operations.
+ *
+ * We could also consider in future limiting the time taken to run the
+ * subprocess since it's unclear that parsing RPM config files from
+ * the guest in particular is safe.
+ *)
+let rec internal_list_rpm_applications () =
+  let chroot = Chroot.create ~name:"librpm" () in
+  let apps = Chroot.f chroot list_rpm_applications () in
+  eprintf "librpm returned %d installed packages\n%!" (List.length apps);
+  apps
+
+and list_rpm_applications () =
+  rpm_init ();
+  rpm_start_iterator ();
+  let ret = ref [] in
+  let rec loop () =
+    try
+      let app = rpm_next_application () in
+      List.push_front app ret;
+      loop ()
+    with Not_found -> ()
+  in
+  loop ();
+  rpm_end_iterator ();
+  List.sort
+    (fun { Structs.app2_name = n1 } { Structs.app2_name = n2 } ->
+      compare n1 n2)
+    !ret

docs/C_SOURCE_FILES
@@ -130,6 +130,7 @@ daemon/pingdaemon.c
 daemon/proto.c
 daemon/readdir.c
 daemon/rename.c
+daemon/rpm-c.c
 daemon/rsync.c
 daemon/scrub.c
 daemon/selinux-relabel.c
@@ -266,7 +267,6 @@ lib/command.c
 lib/conn-socket.c
 lib/copy-in-out.c
 lib/create.c
-lib/dbdump.c
 lib/drives.c
 lib/errors.c
 lib/event-string.c

docs/guestfs-building.pod
@@ -210,11 +210,6 @@ eg. F</etc/libguestfs-tools.conf>.
 Optional.  Used by the L<libvirt backend|guestfs(3)/BACKEND> to
 securely confine the appliance (sVirt).
 
-=item Berkeley DB utils (db_dump, db_load, etc)
-
-Optional.  Usually found in a package called C<db-utils>,
-C<db4-utils>, C<db4.X-utils> etc.
-
 =item systemtap
 
 Optional.  For userspace probes.
@@ -251,6 +246,10 @@ Optional.  Render icons from guests.
 =item icoutils
 
 Optional.  Render icons from Windows guests.
+
+=item librpm
+
+Optional.  To parse the list of applications from RPM-based guests.
 
 =item Perl C<Expect>
 

generator/actions_inspection.ml
@@ -607,6 +607,16 @@ Please read L<guestfs(3)/INSPECTION> for more details.
 See also C<guestfs_inspect_get_mountpoints>,
 C<guestfs_inspect_get_filesystems>." };
 
+  { defaults with
+    name = "internal_list_rpm_applications"; added = (1, 45, 3);
+    style = RStructList ("applications2", "application2"), [], [];
+    visibility = VInternal;
+    impl = OCaml "Rpm.internal_list_rpm_applications";
+    shortdesc = "get applications from RPM guest";
+    longdesc = "\
+This internal function is used by C<guestfs_inspect_list_applications2>
+to list the applications for RPM guests."};
+
 ]
 
 let non_daemon_functions = [

commit 488245ed6c0c5db282ec7fed646e8bc00ce0d487

daemon: rpm: Check return values from librpm calls
We previously didn't bother to check the return values from any librpm
calls.  In some cases where possibly the RPM database is faulty, this
caused us to return a zero-length list of installed applications (but
no error indication).

One way to reproduce this is given below.  Note this reproducer will
only work when run on a RHEL 8 host (or more specifically, with
rpm <= 4.16):

$ virt-builder fedora-28
$ guestfish -a fedora-28.img -i rm /var/lib/rpm/Packages
$ guestfish --ro -a fedora-28.img -i inspect-list-applications /dev/sda4 -vx
...
chroot: /sysroot: running 'librpm'
error: cannot open Packages index using db5 - Read-only file system (30)
error: cannot open Packages database in
error: cannot open Packages index using db5 - Read-only file system (30)
error: cannot open Packages database in
librpm returned 0 installed packages
...

With this commit we get an error instead:

...
chroot: /sysroot: running 'librpm'
error: cannot open Packages index using db5 - Read-only file system (30)
error: cannot open Packages database in
ocaml_exn: 'internal_list_rpm_applications' raised 'Failure' exception
guestfsd: error: rpmtsInitIterator
guestfsd: => internal_list_rpm_applications (0x1fe) took 0.01 secs
libguestfs: trace: internal_list_rpm_applications = NULL (error)
libguestfs: error: internal_list_rpm_applications: rpmtsInitIterator
libguestfs: trace: inspect_list_applications2 = NULL (error)
libguestfs: trace: inspect_list_applications = NULL (error)
...

Not in this case, but in some cases of corrupt RPM databases it is
possible to recover them by running "rpmdb --rebuilddb" as a guest
command (ie. with guestfs_sh).

See-also: https://bugzilla.redhat.com/show_bug.cgi?id=2089623#c12
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2089623
Fixes: commit c9ee831affed55abe0f928134cbbd2ed83b2f510
Reported-by: Xiaodai Wang
Reported-by: Ming Xie
Acked-by: Laszlo Ersek <lersek@redhat.com>

daemon/rpm-c.c
@@ -24,6 +24,7 @@
 #include <inttypes.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <caml/alloc.h>
 #include <caml/fail.h>
@@ -79,7 +80,14 @@ value
 guestfs_int_daemon_rpm_init (value unitv)
 {
   CAMLparam1 (unitv);
-  rpmReadConfigFiles (NULL, NULL);
+
+  /* Nothing in actual RPM C code bothers to check if this call
+   * succeeds, so using that as an example, just print a debug message
+   * if it failed, but continue.  (The librpm Python bindings do check)
+   */
+  if (rpmReadConfigFiles (NULL, NULL) == -1)
+    fprintf (stderr, "rpmReadConfigFiles: failed, errno=%d\n", errno);
+
   CAMLreturn (Val_unit);
 }
 
@@ -92,6 +100,8 @@ guestfs_int_daemon_rpm_start_iterator (value unitv)
   CAMLparam1 (unitv);
 
   ts = rpmtsCreate ();
+  if (ts == NULL)
+    caml_failwith ("rpmtsCreate");
 
 #ifdef RPMVSF_MASK_NOSIGNATURES
   /* Disable signature checking (RHBZ#2064182). */
@@ -99,6 +109,14 @@ guestfs_int_daemon_rpm_start_iterator (value unitv)
 #endif
 
   iter = rpmtsInitIterator (ts, RPMDBI_PACKAGES, NULL, 0);
+  /* This could return NULL in theory if there are no packages, but
+   * that could not happen in a real guest.  However it also returns
+   * NULL when unable to open the database (RHBZ#2089623) which is
+   * something we do need to detect.
+   */
+  if (iter == NULL)
+    caml_failwith ("rpmtsInitIterator");
+
   CAMLreturn (Val_unit);
 }
 