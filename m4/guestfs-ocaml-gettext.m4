libguestfs/libguestfs

commit 733d2182b64df7abc5c5cd7d78177baa6079628c

Remove the tools.
These have now moved to a new repository.  Provisionally it is here:

https://github.com/rwmjones/guestfs-tools/

but this is not the final location, as it will eventually be hosted on
gitlab.com.

The tarballs are here:

https://download.libguestfs.org/guestfs-tools/

Makefile.am
@@ -47,6 +47,7 @@ SUBDIRS += include lib docs examples
 SUBDIRS += common/mlutils
 SUBDIRS += bundled/ocaml-augeas
 SUBDIRS += common/mlpcre
+SUBDIRS += common/mlgettext
 if ENABLE_DAEMON
 SUBDIRS += daemon
 SUBDIRS += tests/daemon
@@ -102,14 +103,8 @@ SUBDIRS += common/progress
 SUBDIRS += common/visit
 SUBDIRS += common/windows
 
-# libguestfs-test-tool
-SUBDIRS += test-tool
-
-# Guestfish.
-SUBDIRS += fish
-
-# virt-tools in C.
-SUBDIRS += align cat diff df edit format inspector make-fs rescue
+# Small tools written in C.
+SUBDIRS += test-tool fish rescue
 
 # bash-completion
 SUBDIRS += bash
@@ -154,31 +149,6 @@ endif
 
 # Unconditional because nothing is built yet.
 SUBDIRS += csharp
-
-# OCaml tools.  Note 'common/ml*' and 'customize' contain shared code
-# used by other OCaml tools, so these must come first.
-if HAVE_OCAML
-SUBDIRS += common/mlgettext
-SUBDIRS += common/mlprogress
-SUBDIRS += common/mlvisit
-SUBDIRS += common/mlxml
-SUBDIRS += common/mltools
-SUBDIRS += common/mlcustomize
-SUBDIRS += customize
-SUBDIRS += builder builder/templates
-SUBDIRS += get-kernel
-SUBDIRS += resize
-SUBDIRS += sparsify
-SUBDIRS += sysprep
-if HAVE_FUSE
-SUBDIRS += dib
-endif
-endif
-
-# Perl tools.
-if HAVE_TOOLS
-SUBDIRS += tools
-endif
 
 # guestmount
 if HAVE_FUSE
@@ -292,8 +262,6 @@ maintainer-upload-website:
 #  docs/C_SOURCE_FILES
 #                 - source files scanned for internal documentation
 #  po/POTFILES    - files with ordinary extensions, but not OCaml files
-#  po/POTFILES-pl - Perl files that don't end in *.pl, which need a
-#                   special xgettext option [not generated here]
 #  po/POTFILES-ml - OCaml files, which need a special tool to translate
 
 dist-hook: BUGS ChangeLog docs/C_SOURCE_FILES po/POTFILES po/POTFILES-ml
@@ -335,16 +303,6 @@ po/POTFILES: configure.ac
 	grep -v -E '^perl/lib/Sys/Guestfs\.c$$' | \
 	grep -v -E '.*-(tests|gperf)\.c$$' | \
 	LC_ALL=C sort -u > $@-t
-	mv $@-t $@
-
-po/POTFILES-ml: configure.ac
-	rm -f $@ $@-t
-	cd $(srcdir); \
-	find builder common/ml* customize dib get-kernel resize sparsify sysprep -name '*.ml' | \
-	grep -v '^builder/templates/' | \
-	grep -v '^common/mlv2v/' | \
-	grep -v -E '.*_tests\.ml$$' | \
-	LC_ALL=C sort > $@-t
 	mv $@-t $@
 
 # Try to stop people using 'make install' without 'DESTDIR'.
@@ -584,7 +542,12 @@ maintainer-check-extra-dist:
 	( git ls-files ; \
 	  cd common; git ls-files | sed 's,^,common/,' ) | \
 	  grep -v '^common$$' | \
+	  grep -v '^common/mlcustomize/' | \
+	  grep -v '^common/mlprogress/' | \
+	  grep -v '^common/mltools/' | \
 	  grep -v '^common/mlv2v/' | \
+	  grep -v '^common/mlvisit/' | \
+	  grep -v '^common/mlxml/' | \
 	  grep -v '^intltool-.*\.in' | \
 	  grep -v '^\.gitmodules' | \
 	  grep -v '^\.gnulib' | \
@@ -594,7 +557,7 @@ maintainer-check-extra-dist:
 	cat tmp/comm-out
 	[ ! -s tmp/comm-out ]
 	@echo Checking for generated files missing from the tarball ...
-	@for f in `cat generator/files-generated.txt | grep -v '^common/mlv2v/'`; do \
+	@for f in `cat generator/files-generated.txt | grep -v '^common/mlcustomize' | grep -v '^common/mlv2v/'`; do \
 	  if ! grep -sq "^$$f\$$" tmp/tarfiles; then \
 	    echo generated file missing from tarball: $$f; \
 	    exit 1; \

configure.ac
@@ -213,30 +213,16 @@ AC_CONFIG_FILES([run],
                 [chmod +x,-w run])
 
 AC_CONFIG_FILES([Makefile
-                 align/Makefile
                  appliance/Makefile
                  bash/Makefile
-                 builder/Makefile
-                 builder/libguestfs.conf
-                 builder/opensuse.conf
-                 builder/templates/Makefile
-                 builder/test-config/virt-builder/repos.d/test-index.conf
-                 builder/test-simplestreams/virt-builder/repos.d/cirros.conf
-                 builder/test-website/virt-builder/repos.d/libguestfs.conf
                  bundled/ocaml-augeas/Makefile
-                 cat/Makefile
                  common/errnostring/Makefile
                  common/edit/Makefile
-                 common/mlcustomize/Makefile
                  common/mlgettext/Makefile
                  common/mlpcre/Makefile
-                 common/mlprogress/Makefile
                  common/mlstdutils/Makefile
                  common/mlstdutils/guestfs_config.ml
-                 common/mltools/Makefile
                  common/mlutils/Makefile
-                 common/mlvisit/Makefile
-                 common/mlxml/Makefile
                  common/options/Makefile
                  common/parallel/Makefile
                  common/progress/Makefile
@@ -248,22 +234,15 @@ AC_CONFIG_FILES([Makefile
                  common/windows/Makefile
                  config.sh
                  csharp/Makefile
-                 customize/Makefile
                  daemon/daemon_config.ml
                  daemon/Makefile
-                 df/Makefile
-                 dib/Makefile
-                 diff/Makefile
                  docs/Makefile
-                 edit/Makefile
                  erlang/Makefile
                  erlang/examples/Makefile
                  examples/Makefile
                  fish/Makefile
-                 format/Makefile
                  fuse/Makefile
                  generator/Makefile
-                 get-kernel/Makefile
                  gnulib/lib/Makefile
                  gnulib/tests/Makefile
                  gobject/libguestfs-gobject-1.0.pc
@@ -272,7 +251,6 @@ AC_CONFIG_FILES([Makefile
                  golang/examples/Makefile
                  haskell/Makefile
                  include/Makefile
-                 inspector/Makefile
                  java/Makefile
                  java/examples/Makefile
                  lib/Makefile
@@ -280,7 +258,6 @@ AC_CONFIG_FILES([Makefile
                  lib/local/libguestfs.pc
                  lua/Makefile
                  lua/examples/Makefile
-                 make-fs/Makefile
                  ocaml/META
                  ocaml/Makefile
                  ocaml/examples/Makefile
@@ -297,15 +274,12 @@ AC_CONFIG_FILES([Makefile
                  python/setup.py
                  python/t/tests_helper.py
                  rescue/Makefile
-                 resize/Makefile
                  ruby/Makefile
                  ruby/Rakefile
                  ruby/examples/Makefile
                  ruby/ext/guestfs/extconf.rb
                  rust/Makefile
                  rust/Cargo.toml
-                 sparsify/Makefile
-                 sysprep/Makefile
                  test-data/Makefile
                  test-data/binaries/Makefile
                  test-data/blank-disks/Makefile
@@ -352,7 +326,6 @@ AC_CONFIG_FILES([Makefile
                  tests/xfs/Makefile
                  tests/xml/Makefile
                  tests/yara/Makefile
-                 tools/Makefile
                  website/index.html])
 
 AC_OUTPUT
@@ -368,19 +341,15 @@ echo
 echo       "Daemon .............................. $enable_daemon"
 echo       "Appliance ........................... $ENABLE_APPLIANCE"
 echo       "QEMU ................................ $QEMU"
-echo       "guestfish and C-based virt tools .... yes"
+echo       "guestfish ........................... yes"
 echo       "FUSE filesystem ..................... $enable_fuse"
 echo       "Default backend ..................... $DEFAULT_BACKEND"
 AS_ECHO_N(["GNU gettext for i18n ................ "])
 if test "x$HAVE_GNU_GETTEXT_TRUE" = "x"; then echo "yes"; else echo "no"; fi
 AS_ECHO_N(["OCaml bindings ...................... "])
 if test "x$HAVE_OCAML_TRUE" = "x"; then echo "yes"; else echo "no"; fi
-AS_ECHO_N(["OCaml-based virt tools .............. "])
-if test "x$HAVE_OCAML_TRUE" = "x"; then echo "yes"; else echo "no"; fi
 AS_ECHO_N(["Perl bindings ....................... "])
 if test "x$HAVE_PERL_TRUE" = "x"; then echo "yes"; else echo "no"; fi
-AS_ECHO_N(["Perl-based virt tools ............... "])
-if test "x$HAVE_TOOLS_TRUE" = "x"; then echo "yes"; else echo "no"; fi
 AS_ECHO_N(["Python bindings ..................... "])
 if test "x$HAVE_PYTHON_TRUE" = "x"; then echo "yes"; else echo "no"; fi
 AS_ECHO_N(["Ruby bindings ....................... "])

po/Makefile.am
@@ -26,14 +26,13 @@ MSGID_BUGS_ADDRESS = https://bugzilla.redhat.com/enter_bug.cgi?component=libgues
 linguas     := $(shell cat $(srcdir)/LINGUAS)
 
 POTFILES    := $(shell $(SED) 's,^,$(top_srcdir)/,' $(srcdir)/POTFILES)
-POTFILES_PL := $(shell $(SED) 's,^,$(top_srcdir)/,' $(srcdir)/POTFILES-pl)
 POTFILES_ML := $(shell $(SED) 's,^,$(top_srcdir)/,' $(srcdir)/POTFILES-ml)
 POFILES     := $(linguas:%=%.po)
 GMOFILES    := $(linguas:%=%.gmo)
 
 EXTRA_DIST = \
 	LINGUAS \
-	POTFILES POTFILES-pl POTFILES-ml \
+	POTFILES POTFILES-ml \
 	$(DOMAIN).pot \
 	$(POFILES) \
 	$(GMOFILES)
@@ -61,7 +60,7 @@ XGETTEXT_ARGS = \
 FIX_CHARSET = \
 	$(SED) -i 's|text/plain; charset=CHARSET|text/plain; charset=utf-8|g'
 
-$(DOMAIN).pot: Makefile POTFILES $(POTFILES) POTFILES-pl $(POTFILES_PL) POTFILES-ml $(POTFILES_ML)
+$(DOMAIN).pot: Makefile POTFILES $(POTFILES) POTFILES-ml $(POTFILES_ML)
 	rm -f $@-t
 	touch $@-t
 if HAVE_OCAML_GETTEXT
@@ -72,8 +71,6 @@ endif
 	$(XGETTEXT) -j -o $@-t $(XGETTEXT_ARGS) \
 	  --files-from=$(abs_srcdir)/POTFILES
 	$(FIX_CHARSET) $@-t
-	$(XGETTEXT) -j -o $@-t $(XGETTEXT_ARGS) --language=Perl \
-	  --files-from=$(abs_srcdir)/POTFILES-pl
 	mv $@-t $@
 
 .po.gmo:

po/POTFILES-ml
@@ -1,121 +1,6 @@
-builder/builder.ml
-builder/cache.ml
-builder/cmdline.ml
-builder/downloader.ml
-builder/index.ml
-builder/index_parser.ml
-builder/ini_reader.ml
-builder/languages.ml
-builder/list_entries.ml
-builder/osinfo.ml
-builder/osinfo_config.ml
-builder/paths.ml
-builder/pxzcat.ml
-builder/repository_main.ml
-builder/setlocale.ml
-builder/sigchecker.ml
-builder/simplestreams_parser.ml
-builder/sources.ml
-builder/utils.ml
-common/mlcustomize/SELinux_relabel.ml
-common/mlcustomize/customize_cmdline.ml
-common/mlcustomize/firstboot.ml
 common/mlgettext/common_gettext.ml
 common/mlpcre/PCRE.ml
-common/mlprogress/progress.ml
 common/mlstdutils/guestfs_config.ml
 common/mlstdutils/std_utils.ml
 common/mlstdutils/stringMap.ml
 common/mlstdutils/stringSet.ml
-common/mltools/JSON.ml
-common/mltools/JSON_parser.ml
-common/mltools/URI.ml
-common/mltools/checksums.ml
-common/mltools/curl.ml
-common/mltools/getopt.ml
-common/mltools/planner.ml
-common/mltools/regedit.ml
-common/mltools/registry.ml
-common/mltools/tools_utils.ml
-common/mltools/urandom.ml
-common/mltools/xpath_helpers.ml
-common/mlutils/c_utils.ml
-common/mlutils/unix_utils.ml
-common/mlvisit/visit.ml
-common/mlxml/xml.ml
-customize/append_line.ml
-customize/crypt.ml
-customize/customize_cmdline.ml
-customize/customize_main.ml
-customize/customize_run.ml
-customize/hostname.ml
-customize/password.ml
-customize/perl_edit.ml
-customize/random_seed.ml
-customize/ssh_key.ml
-customize/subscription_manager.ml
-customize/timezone.ml
-dib/cmdline.ml
-dib/dib.ml
-dib/elements.ml
-dib/output_format.ml
-dib/output_format_docker.ml
-dib/output_format_qcow2.ml
-dib/output_format_raw.ml
-dib/output_format_squashfs.ml
-dib/output_format_tar.ml
-dib/output_format_tgz.ml
-dib/output_format_vhd.ml
-dib/utils.ml
-get-kernel/get_kernel.ml
-resize/resize.ml
-sparsify/cmdline.ml
-sparsify/copying.ml
-sparsify/in_place.ml
-sparsify/sparsify.ml
-sparsify/utils.ml
-sysprep/main.ml
-sysprep/sysprep_operation.ml
-sysprep/sysprep_operation_abrt_data.ml
-sysprep/sysprep_operation_backup_files.ml
-sysprep/sysprep_operation_bash_history.ml
-sysprep/sysprep_operation_blkid_tab.ml
-sysprep/sysprep_operation_ca_certificates.ml
-sysprep/sysprep_operation_crash_data.ml
-sysprep/sysprep_operation_cron_spool.ml
-sysprep/sysprep_operation_customize.ml
-sysprep/sysprep_operation_dhcp_client_state.ml
-sysprep/sysprep_operation_dhcp_server_state.ml
-sysprep/sysprep_operation_dovecot_data.ml
-sysprep/sysprep_operation_firewall_rules.ml
-sysprep/sysprep_operation_flag_reconfiguration.ml
-sysprep/sysprep_operation_fs_uuids.ml
-sysprep/sysprep_operation_ipa_client.ml
-sysprep/sysprep_operation_kerberos_data.ml
-sysprep/sysprep_operation_kerberos_hostkeytab.ml
-sysprep/sysprep_operation_logfiles.ml
-sysprep/sysprep_operation_lvm_uuids.ml
-sysprep/sysprep_operation_machine_id.ml
-sysprep/sysprep_operation_mail_spool.ml
-sysprep/sysprep_operation_net_hostname.ml
-sysprep/sysprep_operation_net_hwaddr.ml
-sysprep/sysprep_operation_pacct_log.ml
-sysprep/sysprep_operation_package_manager_cache.ml
-sysprep/sysprep_operation_pam_data.ml
-sysprep/sysprep_operation_passwd_backups.ml
-sysprep/sysprep_operation_puppet_data_log.ml
-sysprep/sysprep_operation_rh_subscription_manager.ml
-sysprep/sysprep_operation_rhn_systemid.ml
-sysprep/sysprep_operation_rpm_db.ml
-sysprep/sysprep_operation_samba_db_log.ml
-sysprep/sysprep_operation_script.ml
-sysprep/sysprep_operation_smolt_uuid.ml
-sysprep/sysprep_operation_ssh_hostkeys.ml
-sysprep/sysprep_operation_ssh_userdir.ml
-sysprep/sysprep_operation_sssd_db_log.ml
-sysprep/sysprep_operation_tmp_files.ml
-sysprep/sysprep_operation_udev_persistent_net.ml
-sysprep/sysprep_operation_user_account.ml
-sysprep/sysprep_operation_utmp.ml
-sysprep/sysprep_operation_yum_uuid.ml
-sysprep/utils.ml

align/Makefile.am
@@ -1,94 +1,0 @@
-# libguestfs virt alignment tools
-# Copyright (C) 2011 Red Hat Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-include $(top_srcdir)/subdir-rules.mk
-
-EXTRA_DIST = \
-	test-virt-alignment-scan.sh \
-	test-virt-alignment-scan-docs.sh \
-	test-virt-alignment-scan-guests.sh \
-	virt-alignment-scan.pod
-
-bin_PROGRAMS = virt-alignment-scan
-
-virt_alignment_scan_SOURCES = \
-	scan.c
-
-virt_alignment_scan_CPPFLAGS = \
-	-DGUESTFS_NO_DEPRECATED=1 \
-	-I$(top_srcdir)/common/utils -I$(top_builddir)/common/utils \
-	-I$(top_srcdir)/common/structs -I$(top_builddir)/common/structs \
-	-I$(top_srcdir)/lib -I$(top_builddir)/lib \
-	-I$(top_srcdir)/include \
-	-I$(top_srcdir)/common/options -I$(top_builddir)/common/options \
-	-I$(top_srcdir)/common/parallel -I$(top_builddir)/common/parallel \
-	-I$(srcdir)/../gnulib/lib -I../gnulib/lib \
-	-DLOCALEBASEDIR=\""$(datadir)/locale"\"
-
-virt_alignment_scan_CFLAGS = \
-	-pthread \
-	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS) \
-	$(LIBVIRT_CFLAGS)
-
-virt_alignment_scan_LDADD = \
-	$(top_builddir)/common/options/liboptions.la \
-	$(top_builddir)/common/parallel/libparallel.la \
-	$(top_builddir)/common/structs/libstructs.la \
-	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
-	$(LIBXML2_LIBS) \
-	$(LIBVIRT_LIBS) \
-	$(LTLIBINTL) \
-	../gnulib/lib/libgnu.la \
-	-lm
-
-# Manual pages and HTML files for the website.
-man_MANS = virt-alignment-scan.1
-noinst_DATA = $(top_builddir)/website/virt-alignment-scan.1.html
-
-virt-alignment-scan.1 $(top_builddir)/website/virt-alignment-scan.1.html: stamp-virt-alignment-scan.pod
-
-stamp-virt-alignment-scan.pod: virt-alignment-scan.pod
-	$(PODWRAPPER) \
-	  --man virt-alignment-scan.1 \
-	  --html $(top_builddir)/website/virt-alignment-scan.1.html \
-	  --path $(top_srcdir)/common/options \
-	  --license GPLv2+ \
-	  --warning safe \
-	  $<
-	touch $@
-
-# Tests.
-
-TESTS_ENVIRONMENT = $(top_builddir)/run --test
-
-TESTS = \
-	test-virt-alignment-scan-docs.sh
-
-if ENABLE_APPLIANCE
-TESTS += \
-	test-virt-alignment-scan.sh
-
-if HAVE_LIBVIRT
-TESTS += \
-	test-virt-alignment-scan-guests.sh
-endif
-endif
-
-check-valgrind:
-	$(MAKE) VG="@VG@" check

commit adfaf25a12b97b18f8a22d96803cd0920bfd25f7

m4: Fix ./configure --disable-ocaml
OCaml is required to compile libguestfs, however we should still be
able to disable the OCaml bindings.  This didn't work because using
--disable-ocaml caused various configure tests to be skipped which are
required to compile the daemon.  In particular the check for
caml_alloc_initialized_string, resulting in this error:

pcre-c.c:47:1: error: static declaration of ‘caml_alloc_initialized_string’ follows non-static declaration
 caml_alloc_initialized_string (mlsize_t len, const char *p)
 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Also OCaml gettext is not required by libguestfs.  There are no *.ml
files used by libguestfs which require translation.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2108425
Fixes: https://bugs.gentoo.org/820053
Fixes: commit 733d2182b64df7abc5c5cd7d78177baa6079628c

Makefile.am
@@ -37,7 +37,6 @@ SUBDIRS += include lib docs examples
 SUBDIRS += common/mlutils
 SUBDIRS += bundled/ocaml-augeas
 SUBDIRS += common/mlpcre
-SUBDIRS += common/mlgettext
 if ENABLE_DAEMON
 SUBDIRS += daemon
 endif
@@ -208,9 +207,8 @@ maintainer-upload-website:
 #  docs/C_SOURCE_FILES
 #                 - source files scanned for internal documentation
 #  po/POTFILES    - files with ordinary extensions, but not OCaml files
-#  po/POTFILES-ml - OCaml files, which need a special tool to translate
 
-dist-hook: docs/C_SOURCE_FILES po/POTFILES po/POTFILES-ml
+dist-hook: docs/C_SOURCE_FILES po/POTFILES
 
 # This has to be in the top-level Makefile.am so that we have access
 # to DIST_SUBDIRS.
@@ -446,6 +444,7 @@ maintainer-check-extra-dist:
 	  cd common; git ls-files | sed 's,^,common/,' ) | \
 	  grep -v '^common$$' | \
 	  grep -v '^common/mlcustomize/' | \
+	  grep -v '^common/mlgettext/' | \
 	  grep -v '^common/mlprogress/' | \
 	  grep -v '^common/mltools/' | \
 	  grep -v '^common/mlv2v/' | \

configure.ac
@@ -211,7 +211,6 @@ AC_CONFIG_FILES([Makefile
                  bundled/ocaml-augeas/Makefile
                  common/errnostring/Makefile
                  common/edit/Makefile
-                 common/mlgettext/Makefile
                  common/mlpcre/Makefile
                  common/mlstdutils/Makefile
                  common/mlstdutils/guestfs_config.ml

docs/guestfs-building.pod
@@ -560,8 +560,7 @@ Note that disabling OCaml (bindings) or Perl will have the knock-on
 effect of disabling parts of the test suite and some tools.
 
 OCaml is required to build libguestfs and this requirement cannot be
-removed.  Using I<--disable-ocaml> only disables the bindings and
-OCaml tools.
+removed.  Using I<--disable-ocaml> only disables the bindings.
 
 =item B<--disable-fuse>
 

m4/guestfs-ocaml-gettext.m4
@@ -1,64 +1,0 @@
-# libguestfs
-# Copyright (C) 2009-2020 Red Hat Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-dnl Write a common gettext module used by each OCaml tool.
-dnl If OCaml gettext is not available then we write dummy functions.
-
-AC_DEFUN([GUESTFS_CREATE_COMMON_GETTEXT_ML],[
-    dnl Check for ocaml-gettext package to translate OCaml tools.
-    AC_CHECK_OCAML_PKG(gettext)
-
-    AC_MSG_NOTICE([creating $1])
-    rm -f $1
-
-    cat <<EOF > $1
-(* This file is generated automatically by ./configure. *)
-
-EOF
-
-    if test "x$OCAML_PKG_gettext" != "xno"; then
-        # ocaml-gettext available: real module.
-        cat <<EOF >>$1
-module Gettext = Gettext.Program (
-  struct
-    let textdomain = "$PACKAGE_NAME"
-    let codeset = None
-    let dir = None
-    let dependencies = [[]]
-  end
-) (GettextStub.Native)
-EOF
-    else
-        # No gettext: module containing dummy gettext functions.
-        cat <<EOF >>$1
-module Gettext = struct
-  external s_ : string -> string = "%identity"
-  external f_ : ('a, 'b, 'c, 'd, 'e, 'f) format6
-    -> ('a, 'b, 'c, 'd, 'e, 'f) format6
-    = "%identity"
-  let sn_ : string -> string -> int -> string
-    = fun s p n -> if n = 1 then s else p
-  let fn_ : ('a, 'b, 'c, 'd, 'e, 'f) format6
-    -> ('a, 'b, 'c, 'd, 'e, 'f) format6
-    -> int -> ('a, 'b, 'c, 'd, 'e, 'f) format6
-    = fun s p n -> if n = 1 then s else p
-end
-EOF
-    fi
-
-    chmod -w $1
-])

m4/guestfs-ocaml.m4
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-dnl Check for OCaml (required, for OCaml bindings and OCaml tools).
+dnl Check for OCaml (required)
 
 dnl OCAMLC and OCAMLFIND have to be unset first, otherwise
 dnl AC_CHECK_TOOL (inside AC_PROG_OCAML) will not look.
@@ -32,9 +32,9 @@ AS_IF([test "x$OCAMLFIND" = "xno"],[
     AC_MSG_ERROR([OCaml findlib is required])
 ])
 
-dnl --disable-ocaml only disables OCaml bindings and OCaml virt tools.
+dnl --disable-ocaml only disables OCaml bindings.
 AC_ARG_ENABLE([ocaml],
-    AS_HELP_STRING([--disable-ocaml], [disable OCaml language bindings and tools]),
+    AS_HELP_STRING([--disable-ocaml], [disable OCaml language bindings]),
     [],
     [enable_ocaml=yes])
 
@@ -132,51 +132,29 @@ AS_IF([test "x$have_Hivex_OPEN_UNSAFE" = "xno"],[
 ])
 AC_SUBST([HIVEX_OPEN_UNSAFE_FLAG])
 
-OCAML_PKG_gettext=no
-OCAML_PKG_ounit2=no
-ounit_is_v2=no
-AS_IF([test "x$OCAMLC" != "xno"],[
-    # Create common/mlgettext/common_gettext.ml gettext functions or stubs.
-
-    # If we're building in a different directory, then common/mlgettext
-    # might not exist yet, so create it:
-    mkdir -p common/mlgettext
-
-    GUESTFS_CREATE_COMMON_GETTEXT_ML([common/mlgettext/common_gettext.ml])
-
-    AC_CHECK_OCAML_PKG(ounit2)
-
-    # oUnit >= 2 is required, so check that it has OUnit2.
-    if test "x$OCAML_PKG_ounit2" != "xno"; then
-        AC_CHECK_OCAML_MODULE(ounit_is_v2,[OUnit.OUnit2],OUnit2,[+ounit2])
-    fi
-])
-AM_CONDITIONAL([HAVE_OCAML_PKG_GETTEXT],
-               [test "x$OCAML_PKG_gettext" != "xno"])
+# oUnit is optional, used by some tests in common/mlstdutils (that we
+# should replace with regular tests one day).  If used, oUnit >= 2 is
+# required.
+if test "x$OCAML_PKG_ounit2" != "xno"; then
+    AC_CHECK_OCAML_MODULE(ounit_is_v2,[OUnit.OUnit2],OUnit2,[+ounit2])
+fi
 AM_CONDITIONAL([HAVE_OCAML_PKG_OUNIT],
                [test "x$OCAML_PKG_ounit2" != "xno" && test "x$ounit_is_v2" != "xno"])
 
-AC_CHECK_PROG([OCAML_GETTEXT],[ocaml-gettext],[ocaml-gettext],[no])
-AM_CONDITIONAL([HAVE_OCAML_GETTEXT],
-               [test "x$OCAML_PKG_gettext" != "xno" && test "x$OCAML_GETTEXT" != "xno"])
-
 dnl Check if OCaml has caml_alloc_initialized_string (added 2017).
-AS_IF([test "x$OCAMLC" != "xno" && test "x$OCAMLFIND" != "xno" && \
-       test "x$enable_ocaml" = "xyes"],[
-    AC_MSG_CHECKING([for caml_alloc_initialized_string])
-    cat >conftest.c <<'EOF'
+AC_MSG_CHECKING([for caml_alloc_initialized_string])
+cat >conftest.c <<'EOF'
 #include <caml/alloc.h>
 int main () { char *p = (void *) caml_alloc_initialized_string; return 0; }
 EOF
-    AS_IF([$OCAMLC conftest.c >&AS_MESSAGE_LOG_FD 2>&1],[
-        AC_MSG_RESULT([yes])
-        AC_DEFINE([HAVE_CAML_ALLOC_INITIALIZED_STRING],[1],
-                  [caml_alloc_initialized_string found at compile time.])
-    ],[
-        AC_MSG_RESULT([no])
-    ])
-    rm -f conftest.c conftest.o
+AS_IF([$OCAMLC conftest.c >&AS_MESSAGE_LOG_FD 2>&1],[
+    AC_MSG_RESULT([yes])
+    AC_DEFINE([HAVE_CAML_ALLOC_INITIALIZED_STRING],[1],
+              [caml_alloc_initialized_string found at compile time.])
+],[
+    AC_MSG_RESULT([no])
 ])
+rm -f conftest.c conftest.o
 
 dnl Flags we want to pass to every OCaml compiler call.
 OCAML_WARN_ERROR="-warn-error +C+D+E+F+L+M+P+S+U+V+Y+Z+X+52-3-6 -w -6"

po/Makefile.am
@@ -26,13 +26,12 @@ MSGID_BUGS_ADDRESS = https://bugzilla.redhat.com/enter_bug.cgi?component=libgues
 linguas     := $(shell cat $(srcdir)/LINGUAS)
 
 POTFILES    := $(shell $(SED) 's,^,$(top_srcdir)/,' $(srcdir)/POTFILES)
-POTFILES_ML := $(shell $(SED) 's,^,$(top_srcdir)/,' $(srcdir)/POTFILES-ml)
 POFILES     := $(linguas:%=%.po)
 GMOFILES    := $(linguas:%=%.gmo)
 
 EXTRA_DIST = \
 	LINGUAS \
-	POTFILES POTFILES-ml \
+	POTFILES \
 	$(DOMAIN).pot \
 	$(POFILES) \
 	$(GMOFILES)
@@ -60,14 +59,9 @@ XGETTEXT_ARGS = \
 FIX_CHARSET = \
 	$(SED) -i 's|text/plain; charset=CHARSET|text/plain; charset=utf-8|g'
 
-$(DOMAIN).pot: Makefile POTFILES $(POTFILES) POTFILES-ml $(POTFILES_ML)
+$(DOMAIN).pot: Makefile POTFILES $(POTFILES)
 	rm -f $@-t
 	touch $@-t
-if HAVE_OCAML_GETTEXT
-	cd $(top_srcdir) && $(OCAML_GETTEXT) --action extract --extract-pot $(abs_srcdir)/$@-t $(shell cat $(abs_srcdir)/POTFILES-ml)
-	 \
-	$(FIX_CHARSET) $@-t
-endif
 	$(XGETTEXT) -j -o $@-t $(XGETTEXT_ARGS) \
 	  --files-from=$(abs_srcdir)/POTFILES
 	$(FIX_CHARSET) $@-t

po/POTFILES-ml
@@ -1,6 +1,0 @@
-common/mlgettext/common_gettext.ml
-common/mlpcre/PCRE.ml
-common/mlstdutils/guestfs_config.ml
-common/mlstdutils/std_utils.ml
-common/mlstdutils/stringMap.ml
-common/mlstdutils/stringSet.ml