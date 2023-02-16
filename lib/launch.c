ecd0395a7c1b942a63211e4b457152f74623354d
Richard W.M. Jones <rjones@redhat.com>
2023-02-16 11:24:37 +0000

    lib: Get rid of minimum libvirt version check
    We require libvirt >= 0.10.2, and we included code to check this at
    configure-, compile- and run-time.  Remove the checks at compile and
    run time (keep the ./configure check).  Libvirt 0.10.2 was released
    over 10 years ago so it's safe to assume that everyone has it by now.
    
    (cherry picked from commit b9ccfe3e039b1962d8a0d65ffa7b275dbc913eb4)

--- lib/launch.c
+++ lib/launch.c
@@ -420,7 +420,7 @@ guestfs_int_set_backend (guestfs_h *g, const char *method)
 void *
 guestfs_int_force_load_backends[] = {
   guestfs_int_init_direct_backend,
-#ifdef HAVE_LIBVIRT_BACKEND
+#ifdef HAVE_LIBVIRT
   guestfs_int_init_libvirt_backend,
 #endif
 };
