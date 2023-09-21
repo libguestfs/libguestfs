libguestfs/libguestfs

commit 23986d3c4f4d1f9cbac44cc743d3e6af721e4237

file: Use -S option with -z
The file(1) manual suggests using -S (disable seccomp) with -z since
the set of system calls provided by the seccomp policy does not allow
the subprocess to run.  This is obvious when you use file -z on a
compressed file on a Linux distro that enables file's seccomp policy
(Arch does this, Fedora does not):

  $ file -zbsL lib-i586.so.zst
  Bad system call

I also fixed some incorrect text in the manual.

Thanks: Toolybird for pointing to this fix
Reported-by: David Runge
Fixes: https://github.com/libguestfs/libguestfs/issues/100

daemon/file.ml
@@ -43,7 +43,7 @@ let file path =
     | S_SOCK -> "socket"
     | S_REG ->
        (* Regular file, so now run [file] on it. *)
-       let out = command "file" ["-zb"; Sysroot.sysroot_path path] in
+       let out = command "file" ["-zSb"; Sysroot.sysroot_path path] in
 
        (*  We need to remove the trailing \n from output of file(1).
         *
@@ -54,6 +54,6 @@ let file path =
        String.trimr out
   )
   else (* it's a device *) (
-    let out = command "file" ["-zbsL"; path] in
+    let out = command "file" ["-zSbsL"; path] in
     String.trimr out
   )

daemon/filearch.ml
@@ -128,7 +128,7 @@ and cpio_arch magic orig_path path =
         | bin :: bins ->
            let bin_path = tmpdir // bin in
            if is_regular_file bin_path then (
-             let out = command "file" ["-zb"; bin_path] in
+             let out = command "file" ["-zSb"; bin_path] in
              file_architecture_of_magic out orig_path bin_path
            )
            else

generator/actions_core.ml
@@ -2204,9 +2204,8 @@ the type or contents of the file.
 This call will also transparently look inside various types
 of compressed file.
 
-The exact command which runs is C<file -zb path>.  Note in
-particular that the filename is not prepended to the output
-(the I<-b> option).
+The filename is not prepended to the output
+(like the file command I<-b> option).
 
 The output depends on the output of the underlying L<file(1)>
 command and it can change in future in ways beyond our control.

commit c95d8c4cf64142bb707b42c32cf3e1ba3c4a5eb1

daemon: Omit 'file -S' option on older distros that lack support
OpenSUSE LEAP 15 lacks support for this option, so test for it before
using it.

See also:
https://listman.redhat.com/archives/libguestfs/2023-September/032613.html

Reported-by: Olaf Hering
Fixes: commit 23986d3c4f4d1f9cbac44cc743d3e6af721e4237
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

daemon/Makefile.am
@@ -280,6 +280,7 @@ SOURCES_MLI = \
 	devsparts.mli \
 	file.mli \
 	filearch.mli \
+	file_helper.mli \
 	findfs.mli \
 	inspect.mli \
 	inspect_fs.mli \
@@ -321,6 +322,7 @@ SOURCES_ML = \
 	btrfs.ml \
 	cryptsetup.ml \
 	devsparts.ml \
+	file_helper.ml \
 	file.ml \
 	filearch.ml \
 	isoinfo.ml \

daemon/file.ml
@@ -43,7 +43,10 @@ let file path =
     | S_SOCK -> "socket"
     | S_REG ->
        (* Regular file, so now run [file] on it. *)
-       let out = command "file" ["-zSb"; Sysroot.sysroot_path path] in
+       let file_options =
+         sprintf "-z%sb"
+           (if File_helper.file_has_S_option () then "S" else "") in
+       let out = command "file" [file_options; Sysroot.sysroot_path path] in
 
        (*  We need to remove the trailing \n from output of file(1).
         *
@@ -54,6 +57,9 @@ let file path =
        String.trimr out
   )
   else (* it's a device *) (
-    let out = command "file" ["-zSbsL"; path] in
+    let file_options =
+      sprintf "-z%sbsL"
+        (if File_helper.file_has_S_option () then "S" else "") in
+    let out = command "file" [file_options; path] in
     String.trimr out
   )

daemon/file_helper.ml
@@ -1,0 +1,28 @@
+(* libguestfs daemon
+ * Copyright (C) 2009-2023 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Std_utils
+
+(* Does [file] support the [-S] / [--no-sandbox] option
+ * (not on OpenSUSE LEAP 15).
+ *)
+let test_option = lazy (
+  let out = Utils.command "file" ["--help"] in
+  String.find out "--no-sandbox" >= 0
+)
+let file_has_S_option () = Lazy.force test_option

daemon/file_helper.mli
@@ -1,0 +1,19 @@
+(* libguestfs daemon
+ * Copyright (C) 2009-2023 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+val file_has_S_option : unit -> bool

daemon/filearch.ml
@@ -128,7 +128,10 @@ and cpio_arch magic orig_path path =
         | bin :: bins ->
            let bin_path = tmpdir // bin in
            if is_regular_file bin_path then (
-             let out = command "file" ["-zSb"; bin_path] in
+             let file_options =
+               sprintf "-z%sb"
+                 (if File_helper.file_has_S_option () then "S" else "") in
+             let out = command "file" [file_options; bin_path] in
              file_architecture_of_magic out orig_path bin_path
            )
            else