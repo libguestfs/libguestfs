fbe7e96cee460e26a0ff6a9c293c155a78bfdfde
Richard W.M. Jones <rjones@redhat.com>
2023-09-21 15:20:55 +0100

    ruby: Get rid of old Test::Unit compatibility
    See this commit in hivex:
    https://github.com/libguestfs/hivex/commit/6dbbc474d3df5cdfd21ed5e692b3a58136fffc42
    
    (cherry picked from commit ecf361d7237d38b2418ddecb1b70e3b722509c12)

--- ruby/t/tc_020_create.rb
+++ ruby/t/tc_020_create.rb
@@ -15,7 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-require File::join(File::dirname(__FILE__), 'test_helper')
+require 'minitest/autorun'
+require 'guestfs'
 
 class Test020Create < Minitest::Test
   def test_020_create
