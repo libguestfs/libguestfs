27645254de11a50778161e1d864998323e28bb9e
Richard W.M. Jones <rjones@redhat.com>
2024-05-10 13:45:59 +0100

    daemon/parted: Assume sfdisk --part-type exists
    This "new" parameter was added in 2014:
    
      commit 8eab3194ce1737a167812d5e84d83b0dfc253fac
      Author: Karel Zak <kzak@redhat.com>
      Date:   Mon Sep 15 12:37:52 2014 +0200
    
        sfdisk: add --parttype
    
        The patch also makes --{id,change-id,print-id} deprecated in favour
        of --parttype. The original --id is too generic option name and the
        --print-id and --change-id are unnecessary and inconsistent with
        another sfdisk options (e.g. we don't have --change-bootable)
    
    Also remove an extraneous / incorrect comment about parted.  As
    history has played out, sfdisk proves to be the better tool and parted
    is a PITA.
    
    (cherry picked from commit 857615d6d258d5bb3badf4caebd4bc8e69f1a33a)

--- daemon/parted.ml
+++ daemon/parted.ml
@@ -25,33 +25,13 @@ open Utils
 
 include Structs
 
-(* Test if [sfdisk] is recent enough to have [--part-type], to be used
- * instead of [--print-id] and [--change-id].
- *)
-let test_sfdisk_has_part_type = lazy (
-  let out = command "sfdisk" ["--help"] in
-  String.find out "--part-type" >= 0
-)
-
-(* Currently we use sfdisk for getting and setting the ID byte.  In
- * future, extend parted to provide this functionality.  As a result
- * of using sfdisk, this won't work for non-MBR-style partitions, but
- * that limitation is noted in the documentation and we can extend it
- * later without breaking the ABI.
- *)
 let part_get_mbr_id device partnum =
   if partnum <= 0 then
     failwith "partition number must be >= 1";
 
-  let param =
-    if Lazy.force test_sfdisk_has_part_type then
-      "--part-type"
-    else
-      "--print-id" in
-
   udev_settle ();
   let out =
-    command "sfdisk" [param; device; string_of_int partnum] in
+    command "sfdisk" ["--part-type"; device; string_of_int partnum] in
   udev_settle ();
 
   (* It's printed in hex, possibly with a leading space. *)
