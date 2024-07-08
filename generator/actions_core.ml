libguestfs/libguestfs

commit 1816651f3c138600ad2e5ba0d6437b4753333818

New APIs: findfs_partuuid and findfs_partlabel
These search for partitions by UUID or label (name).  They only work
for GPT.

generator/actions_core.ml
@@ -5724,6 +5724,30 @@ filesystem can be found.
 To find the label of a filesystem, use C<guestfs_vfs_label>." };
 
   { defaults with
+    name = "findfs_partuuid"; added = (1, 5, 3);
+    style = RString (RDevice, "device"), [String (PlainString, "uuid")], [];
+    impl = OCaml "Findfs.findfs_partuuid";
+    shortdesc = "find a partition by UUID";
+    longdesc = "\
+This command searches the partitions and returns the one
+which has the given partition UUID.  An error is returned if no such
+partition can be found.
+
+To find the UUID of a partition, use C<guestfs_blkid> (C<PART_ENTRY_UUID>)." };
+
+  { defaults with
+    name = "findfs_partlabel"; added = (1, 5, 3);
+    style = RString (RDevice, "device"), [String (PlainString, "label")], [];
+    impl = OCaml "Findfs.findfs_partlabel";
+    shortdesc = "find a partition by label";
+    longdesc = "\
+This command searches the partitions and returns the one
+which has the given label.  An error is returned if no such
+partition can be found.
+
+To find the label of a partition, use C<guestfs_blkid> (C<PART_ENTRY_NAME>)." };
+
+  { defaults with
     name = "is_chardev"; added = (1, 5, 10);
     style = RBool "flag", [String (Pathname, "path")], [OBool "followsymlinks"];
     once_had_no_optargs = true;

daemon/findfs.ml
@@ -27,6 +27,10 @@ let rec findfs_uuid uuid =
   findfs "UUID" uuid
 and findfs_label label =
   findfs "LABEL" label
+and findfs_partuuid uuid =
+  findfs "PARTUUID" uuid
+and findfs_partlabel label =
+  findfs "PARTLABEL" label
 
 and findfs tag str =
   (* Kill the cache file, forcing blkid to reread values from the

generator/proc_nr.ml
@@ -516,6 +516,8 @@ let proc_nr = [
 511, "internal_readdir";
 512, "clevis_luks_unlock";
 513, "inspect_get_build_id";
+514, "findfs_partuuid";
+515, "findfs_partlabel";
 ]
 
 (* End of list.  If adding a new entry, add it at the end of the list

lib/MAX_PROC_NR
@@ -1,1 +1,1 @@
-513
+515

commit 6bf22df62a42c2c7b99216a185cce680f2e1a6db

generator/actions_core.ml: Fix version field for new APIs
Fixes: commit 1816651f3c138600ad2e5ba0d6437b4753333818

generator/actions_core.ml
@@ -5724,7 +5724,7 @@ filesystem can be found.
 To find the label of a filesystem, use C<guestfs_vfs_label>." };
 
   { defaults with
-    name = "findfs_partuuid"; added = (1, 5, 3);
+    name = "findfs_partuuid"; added = (1, 53, 5);
     style = RString (RDevice, "device"), [String (PlainString, "uuid")], [];
     impl = OCaml "Findfs.findfs_partuuid";
     shortdesc = "find a partition by UUID";
@@ -5736,7 +5736,7 @@ partition can be found.
 To find the UUID of a partition, use C<guestfs_blkid> (C<PART_ENTRY_UUID>)." };
 
   { defaults with
-    name = "findfs_partlabel"; added = (1, 5, 3);
+    name = "findfs_partlabel"; added = (1, 53, 5);
     style = RString (RDevice, "device"), [String (PlainString, "label")], [];
     impl = OCaml "Findfs.findfs_partlabel";
     shortdesc = "find a partition by label";