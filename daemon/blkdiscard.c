9492fb945812a657215082400948fda5fab41464
Susant Sahani <ssahani@redhat.com>
2025-11-26 12:34:22 +0530

    refactor: do_blkdiscardzeroes: add CLEANUP_CLOSE
    Signed-off-by: Susant Sahani <ssahani@redhat.com>

--- daemon/blkdiscard.c
+++ daemon/blkdiscard.c
@@ -30,6 +30,7 @@
 #include <linux/fs.h>
 #endif
 
+#include "cleanups.h"
 #include "daemon.h"
 #include "actions.h"
 #include "optgroups.h"
@@ -103,7 +104,7 @@ optgroup_blkdiscardzeroes_available (void)
 int
 do_blkdiscardzeroes (const char *device)
 {
-  int fd;
+  CLEANUP_CLOSE int fd = -1;
   unsigned int arg;
 
   fd = open (device, O_RDONLY|O_CLOEXEC);
@@ -114,11 +115,8 @@ do_blkdiscardzeroes (const char *device)
 
   if (ioctl (fd, BLKDISCARDZEROES, &arg) == -1) {
     reply_with_perror ("ioctl: %s: BLKDISCARDZEROES", device);
-    close (fd);
     return -1;
   }
-
-  close (fd);
 
   return arg != 0;
 }
