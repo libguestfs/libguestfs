fcc511920866437fbe71739f0c170e377e18f73e
Richard W.M. Jones <rjones@redhat.com>
2026-04-28 09:57:27 +0100

    daemon: Rewrite some luks_* APIs in OCaml
    No change.  Just rewriting these to so that all functions that handle
    cryptsetup Key parameters are written in OCaml, so we can use a common
    function in future for handling base64.
    
    Note that the previous versions of these functions ignored the case of
    cryptsetup exiting with a non-zero error code, as does existing
    Cryptsetup.cryptsetup_open.  Unclear if this is right or not, but this
    change keeps the same behaviour.
    
    (cherry picked from commit ec3548af1e6c75e021e49887fd27efa9262be98b)

--- daemon/luks.ml
+++ daemon/luks.ml
@@ -23,6 +23,65 @@ open Std_utils
 
 open Utils
 
+let write_key_to_tmp_file key =
+  let filename, chan = Filename.open_temp_file "luks" ".out" in
+  output_string chan key;
+  close_out chan;
+  filename
+
+let rec luks_format device key keyslot =
+  _luks_format device key keyslot
+
+and luks_format_cipher device key keyslot cipher =
+  _luks_format ~cipher device key keyslot
+
+and _luks_format ?cipher device key keyslot =
+  let tmp = write_key_to_tmp_file key in
+  Fun.protect ~finally:(fun () -> unlink tmp) (
+    fun () ->
+      let args = ref [] in
+      List.push_back args "-q";
+      Option.iter (fun s -> List.push_back_list args ["--cipher"; s]) cipher;
+      List.push_back args "--key-slot";
+      List.push_back args (string_of_int keyslot);
+      List.push_back args "luksFormat";
+      List.push_back args device;
+      List.push_back args tmp;
+      ignore (command "cryptsetup" !args)
+  );
+  udev_settle ()
+
+let luks_add_key device key newkey keyslot =
+  let keyfile = write_key_to_tmp_file key
+  and newkeyfile = write_key_to_tmp_file newkey in
+  Fun.protect ~finally:(fun () -> unlink keyfile; unlink newkeyfile) (
+    fun () ->
+      let args = ref [] in
+      List.push_back args "-q";
+      List.push_back args "-d";
+      List.push_back args keyfile;
+      List.push_back args "--key-slot";
+      List.push_back args (string_of_int keyslot);
+      List.push_back args "luksAddKey";
+      List.push_back args device;
+      List.push_back args newkeyfile;
+      ignore (command "cryptsetup" !args)
+  )
+
+let luks_kill_slot device key keyslot =
+  let tmp = write_key_to_tmp_file key in
+  Fun.protect ~finally:(fun () -> unlink tmp) (
+    fun () ->
+      let args = ref [] in
+      List.push_back args "-q";
+      List.push_back args "-d";
+      List.push_back args tmp;
+      List.push_back args "luksKillSlot";
+      List.push_back args device;
+      List.push_back args (string_of_int keyslot);
+      ignore (command "cryptsetup" !args)
+  )
+
 (* Deprecated APIs for backwards compatibility. *)
 let luks_open device key mapname =
   Cryptsetup.cryptsetup_open ~crypttype:"luks" device key mapname
