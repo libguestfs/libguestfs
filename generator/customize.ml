1f00e533610dc9291337c81669997ebebae23719
Richard W.M. Jones <rjones@redhat.com>
2026-05-05 12:22:18 +0100

    generator: Refer to new virt-customize(1)/FIRSTBOOT section
    Documentation about firstboot scripts was consolidated and reworked
    into a new section of the virt-customize(1) manual.  Change the
    existing references.
    
    Update common submodule:
    
      Richard W.M. Jones (1):
            mlcustomize: Update generated files
    
      Srihari Parimi (1):
            mlcustomize: firstboot.bat must not reboot VM on error code 250

--- generator/customize.ml
+++ generator/customize.ml
@@ -224,10 +224,7 @@ conveniently wraps the command up in a single line script for you.
 You can have multiple I<--firstboot> options.  They run in the same
 order that they appear on the command line.
 
-Please take a look at L<virt-builder(1)/FIRST BOOT SCRIPTS> for more
-information and caveats about the first boot scripts.
-
-See also I<--run>.|};
+See also: L<virt-customize(1)/FIRSTBOOT>, I<--run>.|};
   };
 
   { op_name = "firstboot-command";
@@ -241,10 +238,7 @@ boots up (as root, late in the boot process).
 You can have multiple I<--firstboot> options.  They run in the same
 order that they appear on the command line.
 
-Please take a look at L<virt-builder(1)/FIRST BOOT SCRIPTS> for more
-information and caveats about the first boot scripts.
-
-See also I<--run>.|};
+See also: L<virt-customize(1)/FIRSTBOOT>, I<--run>.|};
   };
 
   { op_name = "firstboot-install";
